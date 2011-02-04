34493262fd39eb1568be70b49220699c2734a03f
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-02-03 23:18:54 -0400

    Removed setup top menu

--- apps/main/__init__.py
+++ apps/main/__init__.py
@@ -6,8 +6,8 @@ from common.api import register_menu
 register_menu([
     {'text':_(u'home'), 'view':'home', 'famfam':'house', 'position':0},
 
-    {'text':_(u'setup'), 'view':'', 'links': [
-    ],'famfam':'cog', 'name':'setup','position':7},
+    #{'text':_(u'setup'), 'view':'', 'links': [
+    #],'famfam':'cog', 'name':'setup','position':7},
 
     {'text':_(u'about'), 'view':'about', 'position':8},
 ])
