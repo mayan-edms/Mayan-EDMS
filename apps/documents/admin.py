b723dc3c42954286379b3fed08a362b97a46409b
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-02-04 01:12:46 -0400

    Improved administration views

--- apps/documents/admin.py
+++ apps/documents/admin.py
@@ -3,9 +3,35 @@ from django.contrib import admin
 from models import MetadataType, DocumentType, Document, \
     DocumentTypeMetadataType, DocumentMetadata
 
-admin.site.register(MetadataType)
-admin.site.register(DocumentType)
-admin.site.register(Document)
-admin.site.register(DocumentTypeMetadataType)
-admin.site.register(DocumentMetadata)
-                         
+
+class MetadataTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'default', 'lookup')
+        
+    
+class DocumentTypeMetadataTypeInline(admin.StackedInline):
+    model = DocumentTypeMetadataType
+    extra = 1
+    classes = ('collapse-open',)
+    allow_add = True
+
+
+class DocumentTypeAdmin(admin.ModelAdmin):
+    inlines = [DocumentTypeMetadataTypeInline]
+
+
+class DocumentMetadataInline(admin.StackedInline):
+    model = DocumentMetadata
+    extra = 1
+    classes = ('collapse-open',)
+    allow_add = True    
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    inlines = [DocumentMetadataInline,]
+    list_display = ('uuid', 'file_filename', 'file_extension', 'file_mimetype')
+
+
+admin.site.register(MetadataType, MetadataTypeAdmin)
+admin.site.register(DocumentType, DocumentTypeAdmin)
+admin.site.register(Document, DocumentAdmin)
+                
