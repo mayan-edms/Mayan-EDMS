9a518f6c63199cfaeb5ab545bec6c98def414a6d
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-02-05 02:18:44 -0400

    Improved link navigation and added document details links

--- apps/documents/__init__.py
+++ apps/documents/__init__.py
@@ -4,14 +4,15 @@ from common.api import register_links, register_menu
 
 from models import Document
 
-document_list = {'text':_(u'documents'), 'view':'document_list', 'famfam':'page'}
+document_list = {'text':_(u'documents list'), 'view':'document_list', 'famfam':'page'}
 document_create = {'text':_('upload a document'), 'view':'document_create', 'famfam':'page_add'}
 document_create_multiple = {'text':_('upload multiple document'), 'view':'document_create_multiple', 'famfam':'page_add'}
+document_view = {'text':_('details'), 'view':'document_view', 'args':'object.id', 'famfam':'page'}
 document_delete = {'text':_('delete'), 'view':'document_delete', 'args':'object.id', 'famfam':'page_delete'}
 
-register_links(Document, [document_delete])
-register_links(['document_list', 'document_create', 'upload_document_with_type'], [document_create, document_create_multiple], menu_name='sidebar')
-
+register_links(Document, [document_view, document_delete])
+register_links(Document, [document_list, document_create, document_create_multiple], menu_name='sidebar')
+register_links(['document_list', 'document_create', 'document_create_multiple', 'upload_document_with_type', 'upload_multiple_documents_with_type'], [document_list, document_create, document_create_multiple], menu_name='sidebar')
 
 
 register_menu([
