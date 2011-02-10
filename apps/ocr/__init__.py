9374448eb05dde2d9ca7a9516869fcf0c78bd9f2
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-02-10 00:15:00 -0400

    Improved the text of the link

--- apps/ocr/__init__.py
+++ apps/ocr/__init__.py
@@ -5,7 +5,7 @@ from common.api import register_links, register_menu
 from documents.models import Document
 
 
-submit_document = {'text':_('submit to ocr'), 'view':'submit_document', 'args':'object.id', 'famfam':'page_lightning'}
+submit_document = {'text':_('submit to OCR queue'), 'view':'submit_document', 'args':'object.id', 'famfam':'page_lightning'}
 
 register_links(Document, [submit_document], menu_name='sidebar')
 
