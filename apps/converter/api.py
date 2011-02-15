mayan-edms/Mayan-EDMS

commit c8d5e26b4c36f94d169a779dca37cd029f67e598

Fix error accidental delete of staging file on preview

apps/converter/api.py
@@ -115,7 +115,7 @@ def in_image_cache(input_filepath, size, page=0, format='jpg', quality=QUALITY_D
         return None
     
     
-def convert(input_filepath, size, quality=QUALITY_DEFAULT, cache=True, page=0, format='jpg', extra_options='', mimetype=None, extension=None):
+def convert(input_filepath, size, quality=QUALITY_DEFAULT, cache=True, page=0, format='jpg', extra_options='', mimetype=None, extension=None, cleanup=True):
     unoconv_output = None
     output_filepath = create_image_cache_filename(input_filepath, size=size, page=page, format=format, quality=quality, extra_options=extra_options)
     if os.path.exists(output_filepath):
@@ -140,7 +140,8 @@ def convert(input_filepath, size, quality=QUALITY_DEFAULT, cache=True, page=0, f
             errors = get_errors(error_string)
             raise ConvertError(status, errors)
     finally:
-        cleanup(input_filepath)
+        if cleanup:
+            cleanup(input_filepath)
         if unoconv_output:
             cleanup(unoconv_output)
         return output_filepath

apps/documents/views.py
@@ -521,7 +521,7 @@ def document_download(request, document_id):
 def staging_file_preview(request, staging_file_id):
     try:
         filepath = StagingFile.get(staging_file_id).filepath
-        output_file = convert(filepath, STAGING_FILES_PREVIEW_SIZE)
+        output_file = convert(filepath, STAGING_FILES_PREVIEW_SIZE, cleanup=False)
         return serve_file(request, File(file=open(output_file, 'r')))
     except Exception, e:
         return serve_file(request, File(file=open('%simages/1297211435_error.png' % settings.MEDIA_ROOT, 'r')))        

commit 0eb97746184a2e3777f46e2c45fb1d4d98c34b5e

Fixed regression caused by c8d5e26b4c36f94d169a779dca37cd029f67e598

apps/converter/api.py
@@ -115,7 +115,7 @@ def in_image_cache(input_filepath, size, page=0, format='jpg', quality=QUALITY_D
         return None
     
     
-def convert(input_filepath, size, quality=QUALITY_DEFAULT, cache=True, page=0, format='jpg', extra_options='', mimetype=None, extension=None, cleanup=True):
+def convert(input_filepath, size, quality=QUALITY_DEFAULT, cache=True, page=0, format='jpg', extra_options='', mimetype=None, extension=None, cleanup_files=True):
     unoconv_output = None
     output_filepath = create_image_cache_filename(input_filepath, size=size, page=page, format=format, quality=quality, extra_options=extra_options)
     if os.path.exists(output_filepath):
@@ -140,7 +140,7 @@ def convert(input_filepath, size, quality=QUALITY_DEFAULT, cache=True, page=0, f
             errors = get_errors(error_string)
             raise ConvertError(status, errors)
     finally:
-        if cleanup:
+        if cleanup_files:
             cleanup(input_filepath)
         if unoconv_output:
             cleanup(unoconv_output)

apps/documents/views.py
@@ -536,7 +536,7 @@ def staging_file_preview(request, staging_file_id):
      
     try:
         filepath = StagingFile.get(staging_file_id).filepath
-        output_file = convert(filepath, size=STAGING_FILES_PREVIEW_SIZE, extra_options=tranformation_string, cleanup=False)
+        output_file = convert(filepath, size=STAGING_FILES_PREVIEW_SIZE, extra_options=tranformation_string, cleanup_files=False)
         return serve_file(request, File(file=open(output_file, 'r')))
     except Exception, e:
         return serve_file(request, File(file=open('%simages/1297211435_error.png' % settings.MEDIA_ROOT, 'r')))        