c5af3bb6cbfbcfe17c17a18abb40d3286ae7f21c
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-02-19 02:29:51 -0400

    Added favicon

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
+from django.conf import settings
 
 
 urlpatterns = patterns('common.views',
@@ -18,6 +19,8 @@ urlpatterns += patterns('',
     url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', { 'template_name' : 'password_reset_confirm.html', 'post_reset_redirect' : '/password/reset/complete/'}, name='password_reset_confirm_view'),
     url(r'^password/reset/complete/$', 'django.contrib.auth.views.password_reset_complete', { 'template_name' : 'password_reset_complete.html' }, name='password_reset_complete_view'),
     url(r'^password/reset/done/$', 'django.contrib.auth.views.password_reset_done', { 'template_name' : 'password_reset_done.html'}, name='password_reset_done_view'),
+
+    (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '%s%s' %(settings.MEDIA_URL, 'images/favicon.ico')}),
 )
 
 urlpatterns += patterns('',
