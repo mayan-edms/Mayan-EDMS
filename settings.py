9f3e595eeec90ac2608645a520d219fcdd76bc34
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2011-03-05 20:31:30 -0400

    Added locale middleware, menu bar language switching works now

--- settings.py
+++ settings.py
@@ -90,6 +90,7 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
