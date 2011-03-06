e1bff0c25abab78dd10762947423b2f6e9d1ed1d
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-03-06 02:22:38 -0400

    Added middleware to strip html spaces

---
+++ apps/common/middleware/strip_spaces_widdleware.py
@@ -0,0 +1,8 @@
+# Aliasing it for the sake of page size.
+from django.utils.html import strip_spaces_between_tags as short
+
+class SpacelessMiddleware(object):
+    def process_response(self, request, response):
+        if 'text/html' in response['Content-Type']:
+            response.content = short(response.content)
+        return response
