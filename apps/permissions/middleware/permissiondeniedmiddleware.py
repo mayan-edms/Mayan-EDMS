1fa80944443d504c875719513fa8cf1625d5ed08
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-03-11 02:59:23 -0400

    Dont't raise PermissionDenied exception, even while debugging

--- apps/permissions/middleware/permissiondeniedmiddleware.py
+++ apps/permissions/middleware/permissiondeniedmiddleware.py
@@ -8,36 +8,33 @@ from django.utils.importlib import import_module
 class PermissionDeniedMiddleware(object):
     def process_exception(self, request, exception):
         if isinstance(exception, PermissionDenied):
-            if settings.DEBUG:
-                raise PermissionDenied
-            else:
+            try:
+                # Handle import error but allow any type error from view
+                callback = getattr(import_module(settings.ROOT_URLCONF),'handler403')
+                return callback(request,exception)
+            except (ImportError,AttributeError):
+                # Try to get a 403 template
                 try:
-                    # Handle import error but allow any type error from view
-                    callback = getattr(import_module(settings.ROOT_URLCONF),'handler403')
-                    return callback(request,exception)
-                except (ImportError,AttributeError):
-                    # Try to get a 403 template
-                    try:
-                        # First look for a user-defined template named "403.html"
-                        t = loader.get_template('403.html')
-                    except TemplateDoesNotExist:
-                        # If a template doesn't exist in the projct, use the following hardcoded template
-                        t = Template("""{% load i18n %}
-                         <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-                                "http://www.w3.org/TR/html4/strict.dtd">
-                         <html>
-                         <head>
-                             <title>{% trans "403 ERROR: Access denied" %}</title>
-                         </head>
-                         <body>
-                             <h1>{% trans "Access Denied (403)" %}</h1>
-                             {% trans "We're sorry, but you are not authorized to view this page." %}
-                         </body>
-                         </html>""")
+                    # First look for a user-defined template named "403.html"
+                    t = loader.get_template('403.html')
+                except TemplateDoesNotExist:
+                    # If a template doesn't exist in the projct, use the following hardcoded template
+                    t = Template("""{% load i18n %}
+                     <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+                            "http://www.w3.org/TR/html4/strict.dtd">
+                     <html>
+                     <head>
+                         <title>{% trans "403 ERROR: Access denied" %}</title>
+                     </head>
+                     <body>
+                         <h1>{% trans "Access Denied (403)" %}</h1>
+                         {% trans "We're sorry, but you are not authorized to view this page." %}
+                     </body>
+                     </html>""")
 
-                    # Now use context and render template
-                    c = RequestContext(request, {
-                          'message': exception.message
-                     })
+                # Now use context and render template
+                c = RequestContext(request, {
+                      'message': exception.message
+                 })
 
-                    return HttpResponseForbidden(t.render(c))
+                return HttpResponseForbidden(t.render(c))
