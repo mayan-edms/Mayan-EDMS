6b5a17af39388dfadd47da91179076f6b4e4c3ec
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-04-13 03:25:45 -0400

    Made English the default language for Tesseract if none is specified

--- apps/ocr/conf/settings.py
+++ apps/ocr/conf/settings.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 
 TESSERACT_PATH = getattr(settings, 'OCR_TESSERACT_PATH', u'/usr/bin/tesseract')
-TESSERACT_LANGUAGE = getattr(settings, 'OCR_TESSERACT_LANGUAGE', None)
+TESSERACT_LANGUAGE = getattr(settings, 'OCR_TESSERACT_LANGUAGE', u'eng')
 REPLICATION_DELAY = getattr(settings, 'OCR_REPLICATION_DELAY', 10)  # In seconds
 NODE_CONCURRENT_EXECUTION = getattr(settings, 'OCR_NODE_CONCURRENT_EXECUTION', 1)
 AUTOMATIC_OCR = getattr(settings, 'OCR_AUTOMATIC_OCR', False)
