e8b23b48a2f1a42875360ded73f03706f38f0434
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-04-15 00:04:06 -0400

    Fixed diagnostics url resolution

--- apps/main/api.py
+++ apps/main/api.py
@@ -1,8 +1,11 @@
+from django.core.urlresolvers import reverse
+
 diagnostics = {}
 
 
 def register_diagnostic(namespace, title, link):
     namespace_dict = diagnostics.get(namespace, {'title': None, 'links': []})
     namespace_dict['title'] = title
+    link['url'] = link.get('url', reverse(link['view']))
     namespace_dict['links'].append(link)
     diagnostics[namespace] = namespace_dict
