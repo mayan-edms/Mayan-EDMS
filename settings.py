488bb7bd027096d13d16e3d801200ed573c6c175
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-04-30 04:47:58 -0400

    Initial commit to support document comments

--- settings.py
+++ settings.py
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'django.contrib.comments',
     'navigation',
     'web_theme',
     'main',
