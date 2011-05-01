b66e996b39562e68edf0cc8282cbbd2f4581e741
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-05-01 15:32:19 -0400

    Show tagget document link in the tags subtemplate of documents

--- apps/tags/utils.py
+++ apps/tags/utils.py
@@ -1,6 +1,6 @@
 from django.utils.translation import ugettext_lazy as _
 
-from tags import tag_document_remove
+from tags import tag_document_remove, tag_tagged_item_list
 
 
 def get_tags_subtemplate(obj):
@@ -14,6 +14,6 @@ def get_tags_subtemplate(obj):
             'title': _(u'tags'),
             'object_list': obj.tags.all(),
             'hide_link': True,
-            'navigation_object_links': [tag_document_remove],
+            'navigation_object_links': [tag_tagged_item_list, tag_document_remove],
         }
     }
