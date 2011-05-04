df35de27596a605cb9487dc7f1a461654b2c9344
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-05-04 02:11:50 -0400

    Added four new highlight color: khaki, coral, orange and lightgrey

--- apps/tags/models.py
+++ apps/tags/models.py
@@ -9,13 +9,21 @@ COLOR_MAGENTA = u'mag'
 COLOR_CYAN = u'cya'
 COLOR_YELLOW = u'yel'
 COLOR_GREENYELLOW = u'gry'
+COLOR_CORAL = u'crl'
+COLOR_KHAKI = u'kki'
+COLOR_LIGHTGREY = u'lig'
+COLOR_ORANGE = u'org'
 
 COLOR_CHOICES = (
     (COLOR_BLUE, _(u'Blue')),
     (COLOR_CYAN, _(u'Cyan')),
+    (COLOR_CORAL, _(u'Coral')),
     (COLOR_GREENYELLOW, _(u'Green-Yellow')),
+    (COLOR_KHAKI, _(u'Khaki')),
+    (COLOR_LIGHTGREY, _(u'LightGrey')),
     (COLOR_MAGENTA, _(u'Magenta')),
     (COLOR_RED, _(u'Red')),
+    (COLOR_ORANGE, _(u'Orange')),
     (COLOR_YELLOW, _(u'Yellow'))
 )
 
@@ -26,6 +34,10 @@ COLOR_CODES = (
     (COLOR_CYAN, u'cyan'),
     (COLOR_YELLOW, u'yellow'),
     (COLOR_GREENYELLOW, u'greenyellow '),
+    (COLOR_CORAL, u'coral'),
+    (COLOR_KHAKI, u'khaki'),
+    (COLOR_ORANGE, u'orange'),
+    (COLOR_LIGHTGREY, u'lightgrey'),
 )
 
 
