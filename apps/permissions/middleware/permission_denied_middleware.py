30a32c0affc88a6bea5ed6ff713134e1267f9771
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-05-08 01:58:33 -0400

    Updated permission denied middleware to comform to coding style

--- apps/permissions/middleware/permission_denied_middleware.py
+++ apps/permissions/middleware/permission_denied_middleware.py
@@ -11,16 +11,16 @@ class PermissionDeniedMiddleware(object):
         if isinstance(exception, PermissionDenied):
             try:
                 # Handle import error but allow any type error from view
-                callback = getattr(import_module(settings.ROOT_URLCONF), 'handler403')
+                callback = getattr(import_module(settings.ROOT_URLCONF), u'handler403')
                 return callback(request, exception)
             except (ImportError, AttributeError):
                 # Try to get a 403 template
                 try:
                     # First look for a user-defined template named "403.html"
-                    t = loader.get_template('403.html')
+                    t = loader.get_template(u'403.html')
                 except TemplateDoesNotExist:
                     # If a template doesn't exist in the projct, use the following hardcoded template
-                    t = Template("""{% load i18n %}
+                    t = Template(u'''{% load i18n %}
                      <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
                             "http://www.w3.org/TR/html4/strict.dtd">
                      <html>
@@ -31,7 +31,7 @@ class PermissionDeniedMiddleware(object):
                          <h1>{% trans "Access Denied (403)" %}</h1>
                          {% trans "We're sorry, but you are not authorized to view this page." %}
                      </body>
-                     </html>""")
+                     </html>''')
 
                 # Now use context and render template
                 c = RequestContext(request)
