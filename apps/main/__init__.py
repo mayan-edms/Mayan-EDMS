e39f162a5ef042f6bd73bc4a8b7e0b696970d7a4
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-06-19 00:51:01 -0400

    Improved menu highlighting for metadata setup and document metadata view

--- apps/main/__init__.py
+++ apps/main/__init__.py
@@ -41,7 +41,7 @@ if not SIDE_BAR_SEARCH:
     register_top_menu('search', link={'text': _(u'search'), 'view': 'search', 'famfam': 'zoom'})
 register_top_menu('tools', link=tools_menu, children_views=['statistics', 'history_list', 'formats_list'])
 #register_top_menu('setup_menu', link={'text': _(u'setup'), 'view': 'setting_list', 'famfam': 'cog'}, children=setup_views)
-register_top_menu('setup_menu', link={'text': _(u'setup'), 'view': 'setting_list', 'famfam': 'cog'}, children_path_regex=[r'^settings/', r'^user_management/', r'^permissions', r'^documents/type'])
+register_top_menu('setup_menu', link={'text': _(u'setup'), 'view': 'setting_list', 'famfam': 'cog'}, children_path_regex=[r'^settings/', r'^user_management/', r'^permissions', r'^documents/type', r'^metadata/setup'])
 register_top_menu('about', link={'text': _(u'about'), 'view': 'about', 'famfam': 'information'})
 
 register_links(['tools_menu', 'statistics', 'history_list', 'history_view', 'formats_list'], [tools_menu, statistics, history_list, formats_list, sentry], menu_name='secondary_menu')
