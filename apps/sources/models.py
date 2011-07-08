aae3f3c147e99cc72cfa0f0dcd1aed14bd52a41b
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-07-08 03:35:22 -0400

    Added help texts to the source models and added 2 new icons

--- apps/sources/models.py
+++ apps/sources/models.py
@@ -10,14 +10,14 @@ SOURCE_UNCOMPRESS_CHOICE_N = 'n'
 SOURCE_UNCOMPRESS_CHOICE_ASK = 'a'
 
 SOURCE_UNCOMPRESS_CHOICES = (
-    (SOURCE_UNCOMPRESS_CHOICE_Y, _(u'Yes')),
-    (SOURCE_UNCOMPRESS_CHOICE_N, _(u'No')),
+    (SOURCE_UNCOMPRESS_CHOICE_Y, _(u'Always')),
+    (SOURCE_UNCOMPRESS_CHOICE_N, _(u'Never')),
 )
 
 SOURCE_INTERACTIVE_UNCOMPRESS_CHOICES = (
-    (SOURCE_UNCOMPRESS_CHOICE_Y, _(u'Yes')),
-    (SOURCE_UNCOMPRESS_CHOICE_N, _(u'No')),
-    (SOURCE_UNCOMPRESS_CHOICE_ASK, _(u'Ask'))
+    (SOURCE_UNCOMPRESS_CHOICE_Y, _(u'Always')),
+    (SOURCE_UNCOMPRESS_CHOICE_N, _(u'Never')),
+    (SOURCE_UNCOMPRESS_CHOICE_ASK, _(u'Ask user'))
 )
 
 SOURCE_ICON_DISK = 'disk'
@@ -28,16 +28,20 @@ SOURCE_ICON_DRIVE_USER = 'drive_user'
 SOURCE_ICON_EMAIL = 'email'
 SOURCE_ICON_FOLDER = 'folder'
 SOURCE_ICON_WORLD = 'world'
+SOURCE_ICON_PRINTER = 'printer'
+SOURCE_ICON_PRINTER_EMPTY = 'printer_empty'
 
 SOURCE_ICON_CHOICES = (
-    (SOURCE_ICON_DISK, _(u'disk')),
-    (SOURCE_ICON_DATABASE, _(u'database')),
-    (SOURCE_ICON_DRIVE, _(u'drive')),
-    (SOURCE_ICON_DRIVE_NETWORK, _(u'network drive')),
-    (SOURCE_ICON_DRIVE_USER, _(u'user drive')),
-    (SOURCE_ICON_EMAIL, _(u'envelope')),
-    (SOURCE_ICON_FOLDER, _(u'folder')),
-    (SOURCE_ICON_WORLD, _(u'world'))
+    (SOURCE_ICON_DISK, _(u'Disk')),
+    (SOURCE_ICON_DATABASE, _(u'Database')),
+    (SOURCE_ICON_DRIVE, _(u'Drive')),
+    (SOURCE_ICON_DRIVE_NETWORK, _(u'Network drive')),
+    (SOURCE_ICON_DRIVE_USER, _(u'User drive')),
+    (SOURCE_ICON_EMAIL, _(u'Envelope')),
+    (SOURCE_ICON_FOLDER, _(u'Folder')),
+    (SOURCE_ICON_WORLD, _(u'World')),
+    (SOURCE_ICON_PRINTER, _(u'Printer')),
+    (SOURCE_ICON_PRINTER_EMPTY, _(u'Empty printer')),
 )
 
 SOURCE_CHOICE_WEB_FORM = 'webform'
@@ -54,7 +58,7 @@ class BaseModel(models.Model):
     enabled = models.BooleanField(default=True, verbose_name=_(u'enabled'))
     whitelist = models.TextField(blank=True, verbose_name=_(u'whitelist'))
     blacklist = models.TextField(blank=True, verbose_name=_(u'blacklist'))
-    document_type = models.ForeignKey(DocumentType, blank=True, null=True, verbose_name=_(u'document type'))
+    document_type = models.ForeignKey(DocumentType, blank=True, null=True, verbose_name=_(u'document type'), help_text=(u'Optional document type to be applied to documents uploaded from this source.'))
 
     def __unicode__(self):
         return u'%s' % self.title
@@ -65,7 +69,7 @@ class BaseModel(models.Model):
 
 
 class InteractiveBaseModel(BaseModel):
-    icon = models.CharField(blank=True, null=True, max_length=24, choices=SOURCE_ICON_CHOICES, verbose_name=_(u'icon'))
+    icon = models.CharField(blank=True, null=True, max_length=24, choices=SOURCE_ICON_CHOICES, verbose_name=_(u'icon'), help_text=_(u'An icon to visually distinguish this source.'))
 
     def save(self, *args, **kwargs):
         if not self.icon:
@@ -81,11 +85,11 @@ class StagingFolder(InteractiveBaseModel):
     source_type = SOURCE_CHOICE_STAGING
     default_icon = SOURCE_ICON_DRIVE
     
-    folder_path = models.CharField(max_length=255, verbose_name=_(u'folder path'))
-    preview_width = models.IntegerField(verbose_name=_(u'preview width'))
-    preview_height = models.IntegerField(blank=True, null=True, verbose_name=_(u'preview height'))
-    uncompress = models.CharField(max_length=1, choices=SOURCE_INTERACTIVE_UNCOMPRESS_CHOICES, verbose_name=_(u'uncompress'))
-    delete_after_upload = models.BooleanField(default=True, verbose_name=_(u'delete after upload'))
+    folder_path = models.CharField(max_length=255, verbose_name=_(u'folder path'), help_text=_(u'Server side filesystem path.'))
+    preview_width = models.IntegerField(blank=True, null=True, verbose_name=_(u'preview width'), help_text=_(u'Width value to be passed to the converter backend.'))
+    preview_height = models.IntegerField(blank=True, null=True, verbose_name=_(u'preview height'), help_text=_(u'Height value to be passed to the converter backend.'))
+    uncompress = models.CharField(max_length=1, choices=SOURCE_INTERACTIVE_UNCOMPRESS_CHOICES, verbose_name=_(u'uncompress'), help_text=_(u'Whether to expand or not compressed archives.'))
+    delete_after_upload = models.BooleanField(default=True, verbose_name=_(u'delete after upload'), help_text=_(u'Delete the file after is has been successfully uploaded.'))
 
     def get_preview_size(self):
         dimensions = []
@@ -118,7 +122,7 @@ class WebForm(InteractiveBaseModel):
     source_type = SOURCE_CHOICE_WEB_FORM
     default_icon = SOURCE_ICON_DISK
 
-    uncompress = models.CharField(max_length=1, choices=SOURCE_INTERACTIVE_UNCOMPRESS_CHOICES, verbose_name=_(u'uncompress'))
+    uncompress = models.CharField(max_length=1, choices=SOURCE_INTERACTIVE_UNCOMPRESS_CHOICES, verbose_name=_(u'uncompress'), help_text=_(u'Whether to expand or not compressed archives.'))
     #Default path
 
     class Meta(InteractiveBaseModel.Meta):
