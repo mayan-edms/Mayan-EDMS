a15d3afd075193877d88391de299dad618f574e8
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-07-08 03:35:54 -0400

    Added a new famfam graphics radioselect widget and applied it to the webform and stagingform edit and create forms

--- apps/sources/forms.py
+++ apps/sources/forms.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext
 from documents.forms import DocumentForm
 
 from sources.models import WebForm, StagingFolder
+from sources.widgets import FamFamRadioSelect
 
 
 class StagingDocumentForm(DocumentForm):
@@ -53,10 +54,24 @@ class WebFormForm(DocumentForm):
 
 
 class WebFormSetupForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(WebFormSetupForm, self).__init__(*args, **kwargs)    
+        self.fields['icon'].widget = FamFamRadioSelect(
+            attrs=self.fields['icon'].widget.attrs,
+            choices=self.fields['icon'].widget.choices,
+        )
+            
     class Meta:
         model = WebForm
         
         
 class StagingFolderSetupForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(StagingFolderSetupForm, self).__init__(*args, **kwargs)    
+        self.fields['icon'].widget = FamFamRadioSelect(
+            attrs=self.fields['icon'].widget.attrs,
+            choices=self.fields['icon'].widget.choices,
+        )
+    
     class Meta:
         model = StagingFolder
