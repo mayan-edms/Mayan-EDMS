fd5b93021ef52f3d3285e73a7b13a9e051082220
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-08-05 10:11:07 -0400

    Updated document indexing widget to show different icon for indexes or indexes that contain documents

--- apps/document_indexing/widgets.py
+++ apps/document_indexing/widgets.py
@@ -2,11 +2,18 @@ from django.utils.safestring import mark_safe
 
 from document_indexing.models import IndexInstance
 
-FOLDER_ICON = u'folder_page'
+FOLDER_W_DOCUMENTS = u'folder_page'
+FOLDER_ICON = u'folder'
 
 
 def index_instance_item_link(index_instance_item):
-    icon = FOLDER_ICON if isinstance(index_instance_item, IndexInstance) else u''
+    if isinstance(index_instance_item, IndexInstance):
+        if index_instance_item.index.link_documents:
+            icon = FOLDER_W_DOCUMENTS
+        else:
+            icon = FOLDER_ICON
+    else:
+        icon = u''
     icon_template = u'<span class="famfam active famfam-%s"></span>' % icon if icon else u''
     return mark_safe('%(icon_template)s<a href="%(url)s">%(text)s</a>' % {
         'url': index_instance_item.get_absolute_url(),
