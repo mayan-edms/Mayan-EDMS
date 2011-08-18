dc4142b575f1d1ba19d3a294c650a5a20273f782
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-08-18 10:12:06 -0400

    Improved new button based links appearance

--- apps/project_setup/widgets.py
+++ apps/project_setup/widgets.py
@@ -20,7 +20,7 @@ def setup_button_widget(request, setup_link):
 
  
 def render_widget(setup_link):
-    return mark_safe(u'<a style="margin-right: 10px;" href="%(url)s"><button style="vertical-align: top; padding: 1px; width: 110px; height: 100px; margin: 10px;"><img src="%(static_url)simages/icons/%(icon)s" alt="%(image_alt)s" /><p style="margin: 0px 0px 0px 0px;">%(string)s</p></button></a>' % {
+    return mark_safe(u'<a style="text-decoration:none; margin-right: 10px;" href="%(url)s"><button style="vertical-align: top; padding: 1px; width: 110px; height: 100px; margin: 10px;"><img src="%(static_url)simages/icons/%(icon)s" alt="%(image_alt)s" /><p style="margin: 0px 0px 0px 0px;">%(string)s</p></button></a>' % {
         'url': reverse(setup_link['view']) if 'view' in setup_link else setup_link['url'],
         'icon': setup_link.get('icon', 'link_button.png'),
         'static_url': settings.STATIC_URL,
