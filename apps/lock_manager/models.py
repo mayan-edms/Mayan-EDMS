9185ed335dc1d3daf0b4b4c774aab1077bb280a4
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2011-08-23 17:34:05 -0400

    lock manager app updates

--- apps/lock_manager/models.py
+++ apps/lock_manager/models.py
@@ -2,42 +2,14 @@ import datetime
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.db.utils import IntegrityError
-from django.db import transaction
 
+from lock_manager.managers import LockManager
+from lock_manager.conf.settings import DEFAULT_LOCK_TIMEOUT
 
-class LockError(Exception):
-    pass
-
-class LockManager(models.Manager):
-    @transaction.commit_manually
-    def acquire_lock(self, name):
-        lock = self.model(name=name)
-        try:
-            lock.save(force_insert=True)
-        except IntegrityError:
-            transaction.rollback()
-            # There is already an existing lock
-            # Check it's expiration date and if expired delete it and 
-            # re-create it
-            lock = Lock.objects.get(name=name)
-            if datetime.datetime.now() > lock.creation_datetime + datetime.timedelta(seconds=lock.expiration):
-                release_lock(self)
-                lock.save()
-            else:
-                raise LockError('Unable to acquire lock')
-        else:
-            transaction.commit()
-        
-    @transaction.commit_manually
-    def release_lock(self, name):
-        lock = Lock.objects.get(name=name)
-        lock.delete()
-        transaction.commit()
 
 class Lock(models.Model):
     creation_datetime = models.DateTimeField(verbose_name=_(u'creation datetime'))
-    expiration_time = models.IntegerField(default=3600, verbose_name=_(u'expiration time'))
+    timeout = models.IntegerField(default=DEFAULT_LOCK_TIMEOUT, verbose_name=_(u'timeout'))
     name = models.CharField(max_length=32, verbose_name=_(u'name'), unique=True)
     
     objects = LockManager()
