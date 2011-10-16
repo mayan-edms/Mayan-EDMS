b1f8a300a9d8a51a6a8f655f61447911b0c9d0e9
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-10-16 07:30:38 -0400

    Used new multi parameter passing method to improve the usability of the grant/revoke method

--- apps/permissions/__init__.py
+++ apps/permissions/__init__.py
@@ -3,7 +3,7 @@ from django.db.models.signals import post_save
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.translation import ugettext_lazy as _
 
-from navigation.api import register_links
+from navigation.api import register_links, register_multi_item_links
 from project_setup.api import register_setup
 
 from permissions.conf.settings import DEFAULT_ROLES
@@ -16,7 +16,6 @@ PERMISSION_ROLE_DELETE = {'namespace': 'permissions', 'name': 'role_delete', 'la
 PERMISSION_PERMISSION_GRANT = {'namespace': 'permissions', 'name': 'permission_grant', 'label': _(u'Grant permissions')}
 PERMISSION_PERMISSION_REVOKE = {'namespace': 'permissions', 'name': 'permission_revoke', 'label': _(u'Revoke permissions')}
 
-
 role_list = {'text': _(u'roles'), 'view': 'role_list', 'famfam': 'medal_gold_1', 'icon': 'medal_gold_1.png', 'permissions': [PERMISSION_ROLE_VIEW]}
 role_create = {'text': _(u'create new role'), 'view': 'role_create', 'famfam': 'medal_gold_add', 'permissions': [PERMISSION_ROLE_CREATE]}
 role_edit = {'text': _(u'edit'), 'view': 'role_edit', 'args': 'object.id', 'famfam': 'medal_gold_1', 'permissions': [PERMISSION_ROLE_EDIT]}
@@ -24,8 +23,12 @@ role_members = {'text': _(u'members'), 'view': 'role_members', 'args': 'object.i
 role_permissions = {'text': _(u'role permissions'), 'view': 'role_permissions', 'args': 'object.id', 'famfam': 'key_go', 'permissions': [PERMISSION_PERMISSION_GRANT, PERMISSION_PERMISSION_REVOKE]}
 role_delete = {'text': _(u'delete'), 'view': 'role_delete', 'args': 'object.id', 'famfam': 'medal_gold_delete', 'permissions': [PERMISSION_ROLE_DELETE]}
 
+permission_grant = {'text': _(u'grant'), 'view': 'permission_multiple_grant', 'famfam': 'key_add', 'permissions': [PERMISSION_PERMISSION_GRANT]}
+permission_revoke = {'text': _(u'revoke'), 'view': 'permission_multiple_revoke', 'famfam': 'key_delete', 'permissions': [PERMISSION_PERMISSION_REVOKE]}
+
 register_links(Role, [role_edit, role_delete, role_permissions, role_members])
 register_links(['role_members', 'role_list', 'role_view', 'role_create', 'role_edit', 'role_permissions', 'role_delete'], [role_list, role_create], menu_name='sidebar')
+register_multi_item_links(['role_permissions'], [permission_grant, permission_revoke])
 
 permission_views = ['role_list', 'role_create', 'role_edit', 'role_members', 'role_permissions', 'role_delete']
 
--- apps/permissions/urls.py
+++ apps/permissions/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('permissions.views',
     url(r'^role/(?P<role_id>\d+)/edit/$', 'role_edit', (), 'role_edit'),
     url(r'^role/(?P<role_id>\d+)/delete/$', 'role_delete', (), 'role_delete'),
     url(r'^role/(?P<role_id>\d+)/members/$', 'role_members', (), 'role_members'),
-
-    url(r'^permission/(?P<permission_id>\d+)/for/(?P<app_label>[\w\-]+)/(?P<module_name>[\w\-]+)/(?P<pk>\d+)/grant/$', 'permission_grant_revoke', {'action': 'grant'}, 'permission_grant'),
-    url(r'^permission/(?P<permission_id>\d+)/for/(?P<app_label>[\w\-]+)/(?P<module_name>[\w\-]+)/(?P<pk>\d+)/revoke/$', 'permission_grant_revoke', {'action': 'revoke'}, 'permission_revoke'),
+   
+    url(r'^permissions/multiple/grant/$', 'permission_grant', (), 'permission_multiple_grant'),
+    url(r'^permissions/multiple/revoke/$', 'permission_revoke', (), 'permission_multiple_revoke'),
 )
