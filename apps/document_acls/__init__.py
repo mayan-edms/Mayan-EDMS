6bbaca774554fe30945c7eb4cb6f68394e4a34ce
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-10-24 12:58:14 -0400

    Added a new docuent_acls app to tie in documents with ACLs

---
+++ apps/document_acls/__init__.py
@@ -0,0 +1,12 @@
+from django.utils.translation import ugettext_lazy as _
+
+from documents.models import Document
+from navigation.api import register_links, register_multi_item_links
+from project_setup.api import register_setup
+
+from acls import ACLS_VIEW_ACL
+
+
+acl_list = {'text': _(u'ACLs'), 'view': 'document_acl_list', 'args': 'object.pk', 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
+
+register_links(Document, [acl_list], menu_name='form_header')
