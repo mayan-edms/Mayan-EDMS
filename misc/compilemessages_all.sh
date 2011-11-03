b1854cb93bd0cfabcb3e1d45a671d6b2da3739f0
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-03 16:41:42 -0400

    Added Russian language to the compile messages script

--- misc/compilemessages_all.sh
+++ misc/compilemessages_all.sh
@@ -5,63 +5,84 @@ BASE=$PWD
 
 cd $BASE/apps/common
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/converter
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/documents
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/document_comments
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/document_indexing
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/dynamic_search
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/folders
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/history
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/grouping
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/main
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/metadata
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/navigation
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/ocr
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/permissions
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/project_setup
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/project_tools
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/smart_settings
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/sources
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/tags
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/user_management
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
 
 cd $BASE/apps/web_theme
 $COMPILEMESSAGES -l pt
+$COMPILEMESSAGES -l ru
