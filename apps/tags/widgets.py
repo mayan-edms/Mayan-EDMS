7121ad86d14b3c67423c99e7503cee6b647d1b6d
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-07 02:08:45 -0400

    Removed tag count from the widget to conserve vertical space

--- apps/tags/widgets.py
+++ apps/tags/widgets.py
@@ -10,8 +10,6 @@ def get_tags_inline_widget(document):
     tag_count = document.tags.count()
     if tag_count:
         tags_template.append(u'<div class="tc">')
-        tags_template.append(u'<div>%(tag_string)s: %(tag_count)s</div>' % {
-            'tag_string': _(u'Tags'), 'tag_count': tag_count})
 
         for tag in document.tags.all():
             tags_template.append(u'<ul class="tags"><li style="background: %s;">%s</li></ul>' % (tag.tagproperties_set.get().get_color_code(), tag.name))
