c65b6a0309acbbd431a91ae5ff93de89f2934c62
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-22 15:06:21 -0400

    Added aditional loggin to the locking app

--- apps/lock_manager/managers.py
+++ apps/lock_manager/managers.py
@@ -14,11 +14,11 @@ logger = logging.getLogger(__name__)
 class LockManager(models.Manager):
     @transaction.commit_on_success
     def acquire_lock(self, name, timeout=None):
-        logger.debug('DEBUG: trying to acquire lock: %s' % name)
+        logger.debug('trying to acquire lock: %s' % name)
         lock = self.model(name=name, timeout=timeout)
         try:
             lock.save(force_insert=True)
-            logger.debug('DEBUG: acquired lock: %s' % name)
+            logger.debug('acquired lock: %s' % name)
             return lock
         except IntegrityError:
             # There is already an existing lock
@@ -27,14 +27,15 @@ class LockManager(models.Manager):
                 lock = self.model.objects.get(name=name)
             except self.model.DoesNotExist:
                 # Table based locking
-                logger.debug('DEBUG: lock: %s does not exist' % name)
+                logger.debug('lock: %s does not exist' % name)
                 raise LockError('Unable to acquire lock')
 
             if datetime.datetime.now() > lock.creation_datetime + datetime.timedelta(seconds=lock.timeout):
-                logger.debug('DEBUG: reseting deleting stale lock: %s' % name)
+                logger.debug('reseting deleting stale lock: %s' % name)
                 lock.timeout=timeout
-                logger.debug('DEBUG: try to reacquire stale lock: %s' % name)
+                logger.debug('try to reacquire stale lock: %s' % name)
                 lock.save()
                 return lock
             else:
+                logger.debug('unable to acquire lock: %s' % name)
                 raise LockError('Unable to acquire lock')
