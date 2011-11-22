667af2a4420c7669d69da4c603463afd6e42408c
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-11-22 17:45:56 -0400

    Added multiple document OCR submit link

--- apps/ocr/__init__.py
+++ apps/ocr/__init__.py
@@ -47,6 +47,7 @@ register_permission(PERMISSION_OCR_QUEUE_EDIT)
 
 #Links
 submit_document = {'text': _('submit to OCR queue'), 'view': 'submit_document', 'args': 'object.id', 'famfam': 'hourglass_add', 'permissions': [PERMISSION_OCR_DOCUMENT]}
+submit_document_multiple = {'text': _('submit to OCR queue'), 'view': 'submit_document_multiple', 'famfam': 'hourglass_add', 'permissions': [PERMISSION_OCR_DOCUMENT]}
 re_queue_document = {'text': _('re-queue'), 'view': 're_queue_document', 'args': 'object.id', 'famfam': 'hourglass_add', 'permissions': [PERMISSION_OCR_DOCUMENT]}
 re_queue_multiple_document = {'text': _('re-queue'), 'view': 're_queue_multiple_document', 'famfam': 'hourglass_add', 'permissions': [PERMISSION_OCR_DOCUMENT]}
 queue_document_delete = {'text': _(u'delete'), 'view': 'queue_document_delete', 'args': 'object.id', 'famfam': 'hourglass_delete', 'permissions': [PERMISSION_OCR_DOCUMENT_DELETE]}
@@ -68,6 +69,8 @@ setup_queue_transformation_edit = {'text': _(u'edit'), 'view': 'setup_queue_tran
 setup_queue_transformation_delete = {'text': _(u'delete'), 'view': 'setup_queue_transformation_delete', 'args': 'transformation.pk', 'famfam': 'shape_square_delete'}
 
 register_links(Document, [submit_document])
+register_multi_item_links(['document_find_duplicates', 'folder_view', 'index_instance_list', 'document_type_document_list', 'search', 'results', 'document_group_view', 'document_list', 'document_list_recent'], [submit_document_multiple])
+
 register_links(DocumentQueue, [document_queue_disable, document_queue_enable, setup_queue_transformation_list])
 register_links(QueueTransformation, [setup_queue_transformation_edit, setup_queue_transformation_delete])
 
