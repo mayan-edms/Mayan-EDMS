cb8b4a4def127fc8b293dee140117f5b633873db
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-12-01 04:43:59 -0400

    Add more logging

--- apps/lock_manager/managers.py
+++ apps/lock_manager/managers.py
@@ -20,7 +20,8 @@ class LockManager(models.Manager):
             lock.save(force_insert=True)
             logger.debug('acquired lock: %s' % name)
             return lock
-        except IntegrityError:
+        except IntegrityError, msg:
+            logger.debug('IntegrityError: %s', msg)
             # There is already an existing lock
             # Check it's expiration date and if expired, reset it
             try:
@@ -32,9 +33,10 @@ class LockManager(models.Manager):
 
             if datetime.datetime.now() > lock.creation_datetime + datetime.timedelta(seconds=lock.timeout):
                 logger.debug('reseting deleting stale lock: %s' % name)
-                lock.timeout=timeout
-                logger.debug('try to reacquire stale lock: %s' % name)
+                lock.timeout = timeout
+                logger.debug('trying to reacquire stale lock: %s' % name)
                 lock.save()
+                logger.debug('reacquired stale lock: %s' % name)
                 return lock
             else:
                 logger.debug('unable to acquire lock: %s' % name)
