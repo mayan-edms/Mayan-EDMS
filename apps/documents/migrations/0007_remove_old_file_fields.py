e81a02b18d7896f04fc95adce8b5c55e58f38c30
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-12-03 23:29:52 -0400

    Improve document multi version migrations

--- apps/documents/migrations/0007_remove_old_file_fields.py
+++ apps/documents/migrations/0007_remove_old_file_fields.py
@@ -9,7 +9,7 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         
         # Changing field 'DocumentVersion.document'
-        db.alter_column('documents_documentversion', 'document_id', self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['documents.Document']))
+        db.alter_column('documents_documentversion', 'document_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['documents.Document']))
 
         # Deleting field 'Document.date_updated'
         db.delete_column('documents_document', 'date_updated')
@@ -33,7 +33,6 @@ class Migration(SchemaMigration):
         db.delete_column('documents_documentpage', 'document_id')
 
         # Changing field 'DocumentPage.document_version'
-        #db.alter_column('documents_documentpage', 'document_version_id', self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['documents.DocumentVersion']))
         db.alter_column('documents_documentpage', 'document_version_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['documents.DocumentVersion']))
 
 
