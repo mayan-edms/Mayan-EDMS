b2097eeb7566303ed6f6e62de9e2baa7cd57ce44
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-12-13 11:48:58 -0400

    Updated document_acl app

--- apps/document_acls/__init__.py
+++ apps/document_acls/__init__.py
@@ -3,8 +3,11 @@ from django.utils.translation import ugettext_lazy as _
 from documents.models import Document
 from navigation.api import register_links, register_multi_item_links
 from project_setup.api import register_setup
+from acls import ACLS_VIEW_ACL
 
-#from acls import ACLS_VIEW_ACL
-acl_list = {'text': _(u'ACLs'), 'view': 'document_acl_list', 'args': 'object.pk', 'famfam': 'lock'}#, 'permissions': [ACLS_VIEW_ACL]}
+acl_list = {'text': _(u'ACLs'), 'view': 'document_acl_list', 'args': 'object.pk', 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
+document_new_holder = {'text': _(u'New holder'), 'view': 'document_new_holder', 'args': 'object.pk', 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
 
 register_links(Document, [acl_list], menu_name='form_header')
+
+register_links(['document_acl_list', 'document_new_holder'], [document_new_holder], menu_name='sidebar')
--- apps/document_acls/views.py
+++ apps/document_acls/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.utils.translation import ugettext_lazy as _
 
 from documents.models import Document
-from acls.views import acl_list_for
+from acls.views import acl_list_for, acl_new_holder_for
 from acls.models import AccessEntry
 
 
 def document_acl_list(request, document_id):
 	document = get_object_or_404(Document, pk=document_id)
 	return acl_list_for(
+		request,
+		document,
+		extra_context={
+			'object': document,
+		}
+	)
+
+
+def document_new_holder(request, document_id):
+	document = get_object_or_404(Document, pk=document_id)
+	return acl_new_holder_for(
 		request,
 		document,
 		extra_context={
