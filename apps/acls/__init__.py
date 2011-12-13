c1fadd08403d1473598fa5f81725735de0fc8a12
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-12-13 10:58:49 -0400

    Converted the acls app to the new class based permission system

--- apps/acls/__init__.py
+++ apps/acls/__init__.py
@@ -1,17 +1,14 @@
 from django.utils.translation import ugettext_lazy as _
 
-from permissions.api import register_permission, set_namespace_title
 from navigation.api import register_links, register_multi_item_links
+from permissions.models import PermissionNamespace, Permission
 
 from acls.models import AccessHolder
 
+acls_namespace = PermissionNamespace('acls', _(u'Access control lists'))
 
-ACLS_EDIT_ACL = {'namespace': 'acls', 'name': 'acl_edit', 'label': _(u'Edit ACLs')}
-ACLS_VIEW_ACL = {'namespace': 'acls', 'name': 'acl_view', 'label': _(u'View ACLs')}
-
-set_namespace_title('acls', _(u'Access control lists'))
-register_permission(ACLS_EDIT_ACL)
-register_permission(ACLS_VIEW_ACL)
+ACLS_EDIT_ACL = Permission.objects.register(acls_namespace, 'acl_edit', _(u'Edit ACLs'))
+ACLS_VIEW_ACL = Permission.objects.register(acls_namespace, 'acl_view', _(u'View ACLs'))
 
 acl_list = {'text': _(u'ACLs'), 'view': 'acl_list', 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
 acl_detail = {'text': _(u'edit'), 'view': 'acl_detail', 'args': ['access_object.gid', 'object.gid'], 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
