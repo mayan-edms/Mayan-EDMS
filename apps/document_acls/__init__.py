175a06182cbce35af99a946b80474324de1a8e54
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-12-17 14:04:58 -0400

    Add the ACL_VIEW and ACL_EDIT permissions and document class permissions

--- apps/document_acls/__init__.py
+++ apps/document_acls/__init__.py
@@ -3,11 +3,17 @@ from django.utils.translation import ugettext_lazy as _
 from documents.models import Document
 from navigation.api import register_links, register_multi_item_links
 from project_setup.api import register_setup
-from acls import ACLS_VIEW_ACL
+from acls import ACLS_VIEW_ACL, ACLS_EDIT_ACL
+from acls.models import class_permissions
 
 acl_list = {'text': _(u'ACLs'), 'view': 'document_acl_list', 'args': 'object.pk', 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
-document_new_holder = {'text': _(u'New holder'), 'view': 'document_new_holder', 'args': 'object.pk', 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
+document_new_holder = {'text': _(u'New holder'), 'view': 'document_new_holder', 'args': 'object.pk', 'famfam': 'user', 'permissions': [ACLS_VIEW_ACL]}
 
 register_links(Document, [acl_list], menu_name='form_header')
 
 register_links(['document_acl_list', 'document_new_holder'], [document_new_holder], menu_name='sidebar')
+
+class_permissions(Document, [
+    ACLS_VIEW_ACL,
+    ACLS_EDIT_ACL
+])
