3de53dccfa196deaa6b1483fdff711c69df47737
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-01-01 20:10:52 -0400

    Added document_post_save signal to detect is a document has a signature, improve the pre_open_hook, and added logging

--- apps/document_signatures/__init__.py
+++ apps/document_signatures/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+import logging
 
 try:
     from cStringIO import StringIO
@@ -6,6 +7,7 @@ except ImportError:
     from StringIO import StringIO   
 
 from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_save
 
 from documents.models import Document, DocumentVersion
 from navigation.api import register_links, register_top_menu, \
@@ -13,7 +15,6 @@ from navigation.api import register_links, register_top_menu, \
     register_sidebar_template
 from django_gpg.runtime import gpg
 from django_gpg.exceptions import GPGDecryptionError    
-#from main.api import register_diagnostic, register_maintenance_links
 
 from .models import DocumentVersionSignature
 from .permissions import (
@@ -21,6 +22,8 @@ from .permissions import (
     PERMISSION_SIGNATURE_UPLOAD,
     PERMISSION_SIGNATURE_DOWNLOAD
     )
+
+logger = logging.getLogger(__name__)
 
 
 def has_embedded_signature(context):
@@ -31,12 +34,18 @@ def doesnt_have_detached_signature(context):
 
 def document_pre_open_hook(descriptor):
     try:
-        result = gpg.decrypt_file(descriptor)
+        result = gpg.decrypt_file(descriptor, close_descriptor=False)
         # gpg return a string, turn it into a file like object
-        return StringIO(result.data)
     except GPGDecryptionError:
         # At least return the original raw content
+        descriptor.seek(0)
         return descriptor
+    else:
+        return StringIO(result.data)
+
+def document_post_save(sender, instance, **kwargs):
+    if kwargs.get('created', False):
+        DocumentVersionSignature.objects.signature_state(instance.document)        
 
 document_signature_upload = {'text': _(u'upload signature'), 'view': 'document_signature_upload', 'args': 'object.pk', 'famfam': 'pencil_add', 'permissions': [PERMISSION_SIGNATURE_UPLOAD], 'conditional_disable': has_embedded_signature}
 document_signature_download = {'text': _(u'download signature'), 'view': 'document_signature_download', 'args': 'object.pk', 'famfam': 'disk', 'permissions': [PERMISSION_SIGNATURE_DOWNLOAD], 'conditional_disable': doesnt_have_detached_signature}
@@ -45,5 +54,6 @@ document_verify = {'text': _(u'signatures'), 'view': 'document_verify', 'args': 
 register_links(Document, [document_verify], menu_name='form_header')
 register_links(['document_verify', 'document_signature_upload', 'document_signature_download'], [document_signature_upload, document_signature_download], menu_name='sidebar')
 
-
 DocumentVersion.register_pre_open_hook(1, document_pre_open_hook)
+
+post_save.connect(document_post_save, sender=DocumentVersion)
