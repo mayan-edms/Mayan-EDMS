b82aa2ccfc4bf537009844061233867fb97ec083
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-01-02 05:39:11 -0400

    Update the acl app to use absolute imports, rename the acl_detail link text from edit to details, fix class default acl links permissions

--- apps/acls/__init__.py
+++ apps/acls/__init__.py
@@ -1,22 +1,17 @@
+from __future__ import absolute_import
+
 from django.utils.translation import ugettext_lazy as _
 
 from navigation.api import register_links, register_multi_item_links
-from permissions.models import PermissionNamespace, Permission
 from project_setup.api import register_setup
 
-from acls.models import AccessHolder, AccessObjectClass, ClassAccessHolder
+from .models import AccessHolder, AccessObjectClass, ClassAccessHolder
+from .permissions import (ACLS_EDIT_ACL, ACLS_VIEW_ACL, 
+    ACLS_CLASS_EDIT_ACL, ACLS_CLASS_VIEW_ACL)
 
-acls_namespace = PermissionNamespace('acls', _(u'Access control lists'))
-acls_setup_namespace = PermissionNamespace('acls_setup', _(u'Access control lists'))
-
-ACLS_EDIT_ACL = Permission.objects.register(acls_namespace, 'acl_edit', _(u'Edit ACLs'))
-ACLS_VIEW_ACL = Permission.objects.register(acls_namespace, 'acl_view', _(u'View ACLs'))
-
-ACLS_CLASS_EDIT_ACL = Permission.objects.register(acls_setup_namespace, 'acl_class_edit', _(u'Edit class default ACLs'))
-ACLS_CLASS_VIEW_ACL = Permission.objects.register(acls_setup_namespace, 'acl_class_view', _(u'View class default ACLs'))
 
 acl_list = {'text': _(u'ACLs'), 'view': 'acl_list', 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
-acl_detail = {'text': _(u'edit'), 'view': 'acl_detail', 'args': ['access_object.gid', 'object.gid'], 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
+acl_detail = {'text': _(u'details'), 'view': 'acl_detail', 'args': ['access_object.gid', 'object.gid'], 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
 acl_grant = {'text': _(u'grant'), 'view': 'acl_multiple_grant', 'famfam': 'key_add', 'permissions': [ACLS_EDIT_ACL]}
 acl_revoke = {'text': _(u'revoke'), 'view': 'acl_multiple_revoke', 'famfam': 'key_delete', 'permissions': [ACLS_EDIT_ACL]}
 
@@ -25,8 +20,8 @@ acl_class_list = {'text': _(u'List of classes'), 'view': 'acl_setup_valid_classe
 acl_class_acl_list = {'text': _(u'ACLs for class'), 'view': 'acl_class_acl_list', 'args': 'object.gid', 'famfam': 'lock'}#, 'permissions': [ACLS_VIEW_ACL]}
 acls_class_acl_detail = {'text': _(u'edit'), 'view': 'acls_class_acl_detail', 'args': ['access_object_class.gid', 'object.gid'], 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
 acl_class_new_holder_for = {'text': _(u'New holder'), 'view': 'acl_class_new_holder_for', 'args': 'object.gid', 'famfam': 'user'}#, 'permissions': [ACLS_VIEW_ACL]}
-acl_class_grant = {'text': _(u'grant'), 'view': 'acl_class_multiple_grant', 'famfam': 'key_add', 'permissions': [ACLS_EDIT_ACL]}
-acl_class_revoke = {'text': _(u'revoke'), 'view': 'acl_class_multiple_revoke', 'famfam': 'key_delete', 'permissions': [ACLS_EDIT_ACL]}
+acl_class_grant = {'text': _(u'grant'), 'view': 'acl_class_multiple_grant', 'famfam': 'key_add', 'permissions': [ACLS_CLASS_EDIT_ACL]}
+acl_class_revoke = {'text': _(u'revoke'), 'view': 'acl_class_multiple_revoke', 'famfam': 'key_delete', 'permissions': [ACLS_CLASS_EDIT_ACL]}
 
 register_links(AccessHolder, [acl_detail])
 register_multi_item_links(['acl_detail'], [acl_grant, acl_revoke])
