edd344b924da6dfebb7a102d846c9797ff70fd85
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-01-03 18:49:03 -0400

    Add ACL support to the document comments app

--- apps/document_comments/__init__.py
+++ apps/document_comments/__init__.py
@@ -8,20 +8,19 @@ from django.contrib.contenttypes import generic
 from navigation.api import register_links, register_model_list_columns
 from permissions.models import PermissionNamespace, Permission
 from common.utils import encapsulate
-
+from acls.api import class_permissions
 from documents.models import Document
 
 if 'django.contrib.comments' not in settings.INSTALLED_APPS:
     raise Exception('This app depends on the django.contrib.comments app.')
 
 from .permissions import (PERMISSION_COMMENT_CREATE,
-    PERMISSION_COMMENT_DELETE, PERMISSION_COMMENT_EDIT,
-    PERMISSION_COMMENT_VIEW)
+    PERMISSION_COMMENT_DELETE, PERMISSION_COMMENT_VIEW)
     
 comment_delete = {'text': _('delete'), 'view': 'comment_delete', 'args': 'object.pk', 'famfam': 'comment_delete', 'permissions': [PERMISSION_COMMENT_DELETE]}
 comment_multiple_delete = {'text': _('delete'), 'view': 'comment_multiple_delete', 'args': 'object.pk', 'famfam': 'comments_delete', 'permissions': [PERMISSION_COMMENT_DELETE]}
 comment_add = {'text': _('add comment'), 'view': 'comment_add', 'args': 'object.pk', 'famfam': 'comment_add', 'permissions': [PERMISSION_COMMENT_CREATE]}
-comments_for_object = {'text': _('comments'), 'view': 'comments_for_object', 'args': 'object.pk', 'famfam': 'comments', 'permissions': [PERMISSION_COMMENT_VIEW], 'children_view_regex': ['comment']}
+comments_for_document = {'text': _('comments'), 'view': 'comments_for_document', 'args': 'object.pk', 'famfam': 'comments', 'permissions': [PERMISSION_COMMENT_VIEW], 'children_view_regex': ['comment']}
 
 register_model_list_columns(Comment, [
     {
@@ -38,9 +37,9 @@ register_model_list_columns(Comment, [
     }
 ])
 
-register_links(['comments_for_object', 'comment_add', 'comment_delete', 'comment_multiple_delete'], [comment_add], menu_name='sidebar')
+register_links(['comments_for_document', 'comment_add', 'comment_delete', 'comment_multiple_delete'], [comment_add], menu_name='sidebar')
 register_links(Comment, [comment_delete])
-register_links(Document, [comments_for_object], menu_name='form_header')
+register_links(Document, [comments_for_document], menu_name='form_header')
 
 Document.add_to_class(
     'comments',
@@ -50,3 +49,9 @@ Document.add_to_class(
         object_id_field='object_pk'
     )
 )
+
+class_permissions(Document, [
+    PERMISSION_COMMENT_CREATE,
+    PERMISSION_COMMENT_DELETE,
+    PERMISSION_COMMENT_VIEW
+])
