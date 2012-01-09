78c99c77008bfd77dfeef236a6ae03dd27d7203c
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-01-09 02:21:17 -0400

    Add tags and smartlinks icon mappings

--- apps/acls/literals.py
+++ apps/acls/literals.py
@@ -1,8 +1,11 @@
 
+# Content type <-> fam fam icon mapping
 CONTENT_TYPE_ICON_MAP = {
     'auth.user': 'user',
     'auth.group': 'group',
     'documents.document': 'page',
     'permissions.role': 'medal_gold_1',
     'folders.folder': 'folder',
+    'taggit.tag': 'tag_blue',
+    'linking.smartlink': 'page_link',
 }
