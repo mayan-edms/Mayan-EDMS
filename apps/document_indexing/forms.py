65b20313d795409026b9db48f351efd11ec9357c
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-02-01 01:26:50 -0400

    Add index creation and edit form

---
+++ apps/document_indexing/forms.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from .models import Index, IndexTemplateNode
+
+
+class IndexForm(forms.ModelForm):
+    """
+    A standard model form to allow users to create a new index
+    """
+    class Meta:
+        model = Index
+
+
+class IndexTemplateNodeForm(forms.ModelForm):
+    """
+    A standard model form to allow users to create a new index template node
+    """
+    def __init__(self, *args, **kwargs):
+        super(IndexTemplateNodeForm, self).__init__(*args, **kwargs)
+        self.fields['index'].widget = forms.widgets.HiddenInput()
+        self.fields['parent'].widget = forms.widgets.HiddenInput()
+    
+    class Meta:
+        model = IndexTemplateNode
