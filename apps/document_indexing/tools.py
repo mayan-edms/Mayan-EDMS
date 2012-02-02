6b8dcb7240cde47c3a07ada8edfbf621fd14aa1a
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-02-02 10:28:25 -0400

    Update the index mirroring rebuild tool

--- apps/document_indexing/tools.py
+++ apps/document_indexing/tools.py
@@ -2,15 +2,17 @@ from __future__ import absolute_import
 
 from documents.models import Document
 
-from .models import IndexInstanceNode, DocumentRenameCount
+from .models import Index, IndexInstanceNode, DocumentRenameCount
 from .filesystem import fs_delete_directory_recusive
 from .api import update_indexes
 
 
 def do_rebuild_all_indexes():
-    fs_delete_directory_recusive()
-    IndexInstanceNode.objects.delete()
-    DocumentRenameCount.objects.delete()
+    for index in Index.objects.all():
+        fs_delete_directory_recusive(index)
+    
+    IndexInstanceNode.objects.all().delete()
+    DocumentRenameCount.objects.all().delete()
     for document in Document.objects.all():
         update_indexes(document)
 
