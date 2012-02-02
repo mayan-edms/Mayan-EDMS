4e3a16da829a557d7bfcfa775c3fc5b7a95e8477
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-02-02 00:38:58 -0400

    Return a QuerySet of documents instead

--- apps/folders/models.py
+++ apps/folders/models.py
@@ -26,7 +26,7 @@ class Folder(models.Model):
 
     @property
     def documents(self):
-        return [folder_document.document for folder_document in self.folderdocument_set.all()]
+        return Document.objects.filter(folderdocument__folder=self)
 
     def remove_document(self, document):
         folder_document = self.folderdocument_set.get(document=document)
