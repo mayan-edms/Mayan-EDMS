02e83d5c776b624218066b6d91c80d9568acfc66
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-02-06 15:03:50 -0400

    For folders app migration to depend of document's app migrations

--- apps/folders/migrations/0001_initial.py
+++ apps/folders/migrations/0001_initial.py
@@ -5,7 +5,10 @@ from south.v2 import SchemaMigration
 from django.db import models
 
 class Migration(SchemaMigration):
-
+    depends_on = (
+        ('documents', '0001_initial'),
+    )
+    
     def forwards(self, orm):
         
         # Adding model 'Folder'
