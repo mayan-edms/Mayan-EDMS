c596c8e24e533ae57b1568345184f3df9408c972
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-02-21 12:26:38 -0400

    Update a setup top menu entry children_view_regex entries after every new setup sub entry registration

--- apps/project_setup/__init__.py
+++ apps/project_setup/__init__.py
@@ -3,4 +3,4 @@ from django.utils.translation import ugettext_lazy as _
 from navigation.api import register_top_menu
 
 #TODO: FIXME dynamic children_path_regext on api register_setup
-register_top_menu('setup_menu', link={'text': _(u'setup'), 'view': 'setup_list', 'famfam': 'cog'}, children_path_regex=[r'^settings/', r'^user_management/', r'^permissions', r'^documents/type', r'^metadata/setup', r'sources/setup', r'grouping/setup'], position=-2)
+setup_link = register_top_menu('setup_menu', link={'text': _(u'setup'), 'view': 'setup_list', 'famfam': 'cog'}, children_path_regex=[r'^settings/', r'^user_management/', r'^permissions', r'^documents/type', r'^metadata/setup', r'sources/setup', r'grouping/setup'], position=-2)
