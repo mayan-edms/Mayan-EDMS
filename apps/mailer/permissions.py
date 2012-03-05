7cd651638acf8837d3a4acc00d67adece7f92701
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-03-05 01:23:43 -0400

    Add document attachment sending support

--- apps/mailer/permissions.py
+++ apps/mailer/permissions.py
@@ -7,3 +7,4 @@ from permissions.models import PermissionNamespace, Permission
 mailer_namespace = PermissionNamespace('mailing', _(u'Mailing'))
 
 PERMISSION_MAILING_LINK = Permission.objects.register(mailer_namespace, 'mail_link', _(u'Send document link via email'))
+PERMISSION_MAILING_SEND_DOCUMENT = Permission.objects.register(mailer_namespace, 'mail_document', _(u'Send document via email'))
--- apps/mailer/views.py
+++ apps/mailer/views.py
@@ -17,20 +17,25 @@ from documents.models import Document, RecentDocument
 from permissions.models import Permission
 from acls.models import AccessEntry
 
-from .permissions import PERMISSION_MAILING_LINK
+from .permissions import PERMISSION_MAILING_LINK, PERMISSION_MAILING_SEND_DOCUMENT
 from .forms import DocumentMailForm
 
 
-def send_document_link(request, document_id=None, document_id_list=None):
+def send_document_link(request, document_id=None, document_id_list=None, as_attachment=False):
     if document_id:
         documents = [get_object_or_404(Document, pk=document_id)]
     elif document_id_list:
         documents = [get_object_or_404(Document, pk=document_id) for document_id in document_id_list.split(',')]
 
+    if as_attachment:
+        permission = PERMISSION_MAILING_SEND_DOCUMENT
+    else:
+        permission = PERMISSION_MAILING_LINK
+
     try:
-        Permission.objects.check_permissions(request.user, [PERMISSION_MAILING_LINK])
+        Permission.objects.check_permissions(request.user, [permission])
     except PermissionDenied:
-        documents = AccessEntry.objects.filter_objects_by_access(PERMISSION_MAILING_LINK, request.user, documents)
+        documents = AccessEntry.objects.filter_objects_by_access(permission, request.user, documents)
 
     if not documents:
         messages.error(request, _(u'Must provide at least one document.'))
@@ -44,7 +49,7 @@ def send_document_link(request, document_id=None, document_id_list=None):
         RecentDocument.objects.add_document_for_user(request.user, document)
 
     if request.method == 'POST':
-        form = DocumentMailForm(request.POST)
+        form = DocumentMailForm(request.POST, as_attachment=as_attachment)
         if form.is_valid():
             context = Context({
                 'link': 'http://%s%s' % (Site.objects.get_current().domain, reverse('document_view_simple', args=[document.pk])),
@@ -59,27 +64,47 @@ def send_document_link(request, document_id=None, document_id_list=None):
 
             email_msg = EmailMultiAlternatives(subject_text, body_text_content, request.user.email, [form.cleaned_data['email']])
             email_msg.attach_alternative(body_html_content, 'text/html')
+            if as_attachment:
+                for document in documents:
+                    descriptor = document.open()
+                    email_msg.attach(document.filename, descriptor.read(), document.file_mimetype)
+                    descriptor.close()
+
             try:
                 email_msg.send()
             except Exception, exc:
-                messages.error(request, _(u'Error sending document link for document %(document)s; %(error)s.') % {
-                    'document': document, 'error': exc})
+                if as_attachment:
+                    messages.error(request, _(u'Error sending document: %(document)s, via email; %(error)s.') % {
+                        'document': document, 'error': exc})
+                else:
+                    messages.error(request, _(u'Error sending document link for document %(document)s; %(error)s.') % {
+                        'document': document, 'error': exc})
+                    
             else:
-                messages.success(request, _(u'Successfully sent document link via email.'))
+                if as_attachment:
+                    messages.success(request, _(u'Successfully sent document via email.'))
+                else:
+                    messages.success(request, _(u'Successfully sent document link via email.'))
                 return HttpResponseRedirect(next)
     else:
-        form = DocumentMailForm()
+        form = DocumentMailForm(as_attachment=as_attachment)
 
     context = {
         'form': form,
         'next': next,
-        'submit_label': _(u'send'),
+        'submit_label': _(u'Send'),
     }
     if len(documents) == 1:
         context['object'] = documents[0]
-        context['title'] = _(u'Email link for document: %s') % ', '.join([unicode(d) for d in documents])
+        if as_attachment:
+            context['title'] = _(u'Email document: %s') % ', '.join([unicode(d) for d in documents])
+        else:
+            context['title'] = _(u'Email link for document: %s') % ', '.join([unicode(d) for d in documents])
     elif len(documents) > 1:
-        context['title'] = _(u'Email links for documents: %s') % ', '.join([unicode(d) for d in documents])
+        if as_attachment:
+            context['title'] = _(u'Email documents: %s') % ', '.join([unicode(d) for d in documents])
+        else:
+            context['title'] = _(u'Email links for documents: %s') % ', '.join([unicode(d) for d in documents])
 
     return render_to_response('generic_form.html', context,
         context_instance=RequestContext(request))
