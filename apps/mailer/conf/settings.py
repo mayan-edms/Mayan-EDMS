55709c3cdfa44e8c720adef4df0d8e28bf16a0c6
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-03-05 03:09:57 -0400

    Setup mailer app to use new Setting class

--- apps/mailer/conf/settings.py
+++ apps/mailer/conf/settings.py
@@ -2,17 +2,31 @@
 
 from django.utils.translation import ugettext_lazy as _
 
-from smart_settings.api import register_settings
+from smart_settings.api import register_settings, Setting, SettingNamespace
 
 register_settings(
     namespace=u'mailer',
     module=u'mailer.conf.settings',
     settings=[
         # Links
-        {'name': u'LINK_SUBJECT_TEMPLATE', 'global_name': u'MAILER_LINK_SUBJECT_TEMPLATE', 'default': 'Link for document: {{ document }}', 'description': _(u'Template for the document link email form subject line.')},
+        #{'name': u'LINK_SUBJECT_TEMPLATE', 'global_name': u'MAILER_LINK_SUBJECT_TEMPLATE', 'default': 'Link for document: {{ document }}', 'description': _(u'Template for the document link email form subject line.')},
         {'name': u'LINK_BODY_TEMPLATE', 'global_name': u'MAILER_LINK_BODY_TEMPLATE', 'default': 'To access this document click on the following link: <a href="{{ link }}">{{ link }}</a><br /><br />\n\n--------<br />\nThis email has been sent from Mayan EDMS (http://www.mayan-edms.com)', 'description': _(u'Template for the document link email form body line.')},
         # Attachment
         {'name': u'DOCUMENT_SUBJECT_TEMPLATE', 'global_name': u'MAILER_DOCUMENT_SUBJECT_TEMPLATE', 'default': 'Document: {{ document }}', 'description': _(u'Template for the document email form subject line.')},
         {'name': u'DOCUMENT_BODY_TEMPLATE', 'global_name': u'MAILER_DOCUMENT_BODY_TEMPLATE', 'default': 'Attached to this email is the document: {{ document }}<br /><br />\n\n--------<br />\nThis email has been sent from Mayan EDMS (http://www.mayan-edms.com)', 'description': _(u'Template for the document email form body line.')},
     ]
 )
+
+"""
+mailer_namespace = SettingNamespace('mailer', _(u'Mailer'))
+
+Setting(
+    namespace=mailer_namespace,
+    module='mailer.conf.settings',
+    name='LINK_SUBJECT_TEMPLATE',
+    global_name='MAILER_LINK_SUBJECT_TEMPLATE',
+    default='Link for document: {{ document }}',
+    description=_(u'Template for the document link email form subject line.')
+)
+
+"""
