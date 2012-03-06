11a9d877012626a848c3ad18dbe851bd80749b5b
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-03-06 16:07:29 -0400

    Implement hidden settings

--- apps/smart_settings/views.py
+++ apps/smart_settings/views.py
@@ -11,64 +11,10 @@ from common.widgets import exists_with_famfam
 from .api import settings, settings_list, namespace_list
 
 
-"""
 def setting_list(request):
-    new_settings = []
-    for namespace, sub_settings in settings.items():
-        for sub_setting in sub_settings:
-            if not sub_setting.get('hidden', False):
-                new_settings.append({
-                    'module': sub_setting['module'],
-                    'name': sub_setting['name'],
-                    'global_name': sub_setting['global_name'],
-                    'description': sub_setting.get('description', None),
-                    'exists': sub_setting.get('exists', False),
-                    'default': sub_setting['default'],
-                    })
     context = {
         'title': _(u'settings'),
-        'object_list': new_settings,
-        'hide_link': True,
-        'hide_object': True,
-        'extra_columns': [
-            {'name': _(u'name'), 'attribute': encapsulate(lambda x: mark_safe(u'<span style="font-weight: bold;">%s</span><br />%s' % (x.get('global_name'), x.get('description'))))},
-            {'name': _(u'default'), 'attribute': encapsulate(lambda x: return_type(x['default']))},
-            {'name': _(u'value'), 'attribute': encapsulate(lambda x: mark_safe(u'<div class="nowrap">%s&nbsp;%s</div>' % (
-                    return_type(getattr(x['module'], x['name'])),
-                    exists_with_famfam(getattr(x['module'], x['name'])) if x['exists'] else ''
-                )))
-            },
-        ]
-    }
-
-    return render_to_response('generic_list.html', context,
-        context_instance=RequestContext(request))
-"""
-
-def setting_list(request):
-    #new_settings = []
-    #for namespace, sub_settings in settings.items():
-    #    for sub_setting in sub_settings:
-    #        if not sub_setting.get('hidden', False):
-    #            new_settings.append({
-    #                'module': sub_setting['module'],
-    #                'name': sub_setting['name'],
-    #                'global_name': sub_setting['global_name'],
-    #                'description': sub_setting.get('description', None),
-    #                'exists': sub_setting.get('exists', False),
-    #                'default': sub_setting['default'],
-    #                })
-    #print 'namespace_list', namespace_list
-    #links=[]
-    #for namespace in namespace_list:
-    #    link = {'text': namespace.label, 'view': 'about_view', 'famfam': 'pencil_add'}#, 'permissions': [PERMISSION_SIGNATURE_UPLOAD], 'conditional_disable': has_embedded_signature}
-    #    links.append(link)
-    #    print 'link', link
-    #register_links(['about_view'], [link], menu_name='sidebar')
-    
-    context = {
-        'title': _(u'settings'),
-        'object_list': settings_list,
+        'object_list': [setting for setting in settings_list if setting.hidden==False],
         'hide_link': True,
         'hide_object': True,
         'extra_columns': [
