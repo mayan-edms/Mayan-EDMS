d6af3c629a195d79d50f15974c9e379d3b3666e8
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-03-14 15:26:01 -0400

    Add smart link documents title on previews

--- apps/linking/forms.py
+++ apps/linking/forms.py
@@ -48,7 +48,7 @@ class SmartLinkImageWidget(forms.widgets.Widget):
             output.append(u'<div class="tc">%s: %d</div>' % (ugettext(u'Pages'), document.pages.count()))
             output.append(get_tags_inline_widget(document))
             output.append(u'<div style="padding: 5px;">' % document)
-            output.append(document_html_widget(document, click_view='document_display', view='document_preview_multipage', fancybox_class='fancybox-noscaling', gallery_name=u'smart_link_%d_documents_gallery' % value['smart_link_instance'].pk))
+            output.append(document_html_widget(document, click_view='document_display', view='document_preview_multipage', fancybox_class='fancybox-noscaling', gallery_name=u'smart_link_%d_documents_gallery' % value['smart_link_instance'].pk, title=document.filename))
             output.append(u'</div>')
             output.append(u'<div class="tc">')
             output.append(u'<a href="%s"><span class="famfam active famfam-page_go"></span>%s</a>' % (reverse('document_view_simple', args=[document.pk]), ugettext(u'Select')))
