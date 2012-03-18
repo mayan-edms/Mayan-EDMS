c3f337898d97c1cb10d65713a5e5706bdf9fe8a7
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-03-18 00:43:18 -0400

    Reorder statement for readability

--- apps/acls/__init__.py
+++ apps/acls/__init__.py
@@ -30,10 +30,11 @@ register_multi_item_links(['acl_detail'], [acl_grant, acl_revoke])
 
 bind_links([AccessObject], [acl_holder_new], menu_name='sidebar')
 
-register_setup(acl_setup_valid_classes)
 bind_links(['acl_setup_valid_classes', 'acl_class_acl_list', 'acl_class_new_holder_for', 'acl_class_acl_detail', 'acl_class_multiple_grant', 'acl_class_multiple_revoke'], [acl_class_list], menu_name='secondary_menu')
 
 bind_links([ClassAccessHolder], [acl_class_acl_detail])
 
 bind_links([AccessObjectClass], [acl_class_acl_list, acl_class_new_holder_for])
 register_multi_item_links(['acl_class_acl_detail'], [acl_class_grant, acl_class_revoke])
+
+register_setup(acl_setup_valid_classes)
