41facf623a4fbd822185aeb63d8c52124b30936a
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-03-29 23:44:21 -0400

    Only try to render resolved links

--- apps/navigation/widgets.py
+++ apps/navigation/widgets.py
@@ -24,10 +24,13 @@ def button_navigation_widget(request, link):
 def render_widget(request, link):
     context = RequestContext(request)
     resolved_link = link.resolve(context)
-    return mark_safe(u'<a style="text-decoration:none; margin-right: 10px;" href="%(url)s"><button style="vertical-align: top; padding: 1px; width: 110px; height: 100px; margin: 10px;"><img src="%(static_url)simages/icons/%(icon)s" alt="%(image_alt)s" /><p style="margin: 0px 0px 0px 0px;">%(string)s</p></button></a>' % {
-        'url': resolved_link.url,
-        'icon': getattr(resolved_link, 'icon', 'link_button.png'),
-        'static_url': settings.STATIC_URL,
-        'string': capfirst(resolved_link.text),
-        'image_alt': _(u'icon'),
-    })
+    if resolved_link:
+        return mark_safe(u'<a style="text-decoration:none; margin-right: 10px;" href="%(url)s"><button style="vertical-align: top; padding: 1px; width: 110px; height: 100px; margin: 10px;"><img src="%(static_url)simages/icons/%(icon)s" alt="%(image_alt)s" /><p style="margin: 0px 0px 0px 0px;">%(string)s</p></button></a>' % {
+            'url': resolved_link.url,
+            'icon': getattr(resolved_link, 'icon', 'link_button.png'),
+            'static_url': settings.STATIC_URL,
+            'string': capfirst(resolved_link.text),
+            'image_alt': _(u'icon'),
+        })
+    else:
+        return u''
