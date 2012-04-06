5dbceb5584a376639a4a2a97f730258d8d799eb8
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-04-05 23:45:50 -0400

    Initial changes to support haystack from the dynamic search app

--- apps/dynamic_search/urls.py
+++ apps/dynamic_search/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 
+from .views import CustomSearchView
+
 urlpatterns = patterns('dynamic_search.views',
-    url(r'^$', 'search', (), 'search'),
+    url(r'^$', CustomSearchView(), (), 'search'),
     url(r'^advanced/$', 'search', {'advanced': True}, 'search_advanced'),
     url(r'^again/$', 'search_again', (), 'search_again'),
     url(r'^results/$', 'results', (), 'results'),
