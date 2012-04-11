34ebf6a263f73323e0bf41bbc06b704b2adf9412
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2012-04-11 03:11:45 -0400

    Use a simple for the basic search

--- apps/dynamic_search/urls.py
+++ apps/dynamic_search/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 
+from haystack.forms import SearchForm
+
 from .views import CustomSearchView
 
 urlpatterns = patterns('dynamic_search.views',
-    url(r'^$', CustomSearchView(), (), 'search'),
+    url(r'^$', CustomSearchView(form_class=SearchForm), (), 'search'),
     url(r'^advanced/$', 'search', {'advanced': True}, 'search_advanced'),
     url(r'^again/$', 'search_again', (), 'search_again'),
     url(r'^results/$', 'results', (), 'results'),
