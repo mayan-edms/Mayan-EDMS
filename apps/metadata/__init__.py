235f77ec7044422f8d6c135098364ccc135b769a
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-05-23 01:20:43 -0400

    Have the metadata app add itself the metadata model list columns for the document model

--- apps/metadata/__init__.py
+++ apps/metadata/__init__.py
@@ -3,12 +3,14 @@ from __future__ import absolute_import
 from django.utils.translation import ugettext_lazy as _
 
 from navigation.api import (bind_links, register_multi_item_links,
-    register_sidebar_template, Link)
+    register_sidebar_template, Link, register_model_list_columns)
 from documents.models import Document, DocumentType
 from documents.permissions import PERMISSION_DOCUMENT_TYPE_EDIT
 from project_setup.api import register_setup
 from acls.api import class_permissions
+from common.utils import encapsulate
 
+from .api import get_metadata_string
 from .models import MetadataType, MetadataSet
 from .permissions import (PERMISSION_METADATA_DOCUMENT_EDIT,
     PERMISSION_METADATA_DOCUMENT_ADD, PERMISSION_METADATA_DOCUMENT_REMOVE,
@@ -40,7 +42,6 @@ setup_document_type_metadata = Link(text=_(u'default metadata'), view='setup_doc
 
 bind_links(['metadata_add', 'metadata_edit', 'metadata_remove', 'metadata_view'], [metadata_add, metadata_edit, metadata_remove], menu_name='sidebar')
 bind_links([Document], [metadata_view], menu_name='form_header')
-register_multi_item_links(['document_find_duplicates', 'folder_view', 'index_instance_list', 'document_type_document_list', 'search', 'results', 'document_group_view', 'document_list', 'document_list_recent'], [metadata_multiple_add, metadata_multiple_edit, metadata_multiple_remove])
 
 bind_links([MetadataType], [setup_metadata_type_edit, setup_metadata_type_delete])
 bind_links([MetadataType, 'setup_metadata_type_list', 'setup_metadata_type_create'], [setup_metadata_type_list, setup_metadata_type_create], menu_name='secondary_menu')
@@ -65,3 +66,9 @@ class_permissions(Document, [
     PERMISSION_METADATA_DOCUMENT_REMOVE,
     PERMISSION_METADATA_DOCUMENT_VIEW,
 ])
+
+register_model_list_columns(Document, [
+        {'name':_(u'metadata'), 'attribute':
+            encapsulate(lambda x: get_metadata_string(x))
+        },
+    ])
