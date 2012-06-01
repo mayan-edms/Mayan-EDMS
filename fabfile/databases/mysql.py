2b1af0c4c40b31b12ce8dd7b96db2bfb1b297ac9
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-06-01 03:21:34 -0400

    Add database_host to database creation task

--- fabfile/databases/mysql.py
+++ fabfile/databases/mysql.py
@@ -6,8 +6,8 @@ def create_database():
     Create the MySQL Mayan EDMS database
     """
     run('echo "CREATE DATABASE %(database_name)s;" |  mysql -u root --password=%(database_manager_admin_password)s' % env)
-    run('echo "CREATE USER \'%(database_username)s\'@\'localhost\' IDENTIFIED BY \'%(database_password)s\';" |  mysql -u root --password=%(database_manager_admin_password)s' % env)
-    run('echo "GRANT ALL PRIVILEGES ON %(database_name)s.* TO \'%(database_username)s\'@\'localhost\' WITH GRANT OPTION; |  mysql -u root --password=%(database_manager_admin_password)s' % env)
+    run('echo "CREATE USER \'%(database_username)s\'@\'%(database_host)s\' IDENTIFIED BY \'%(database_password)s\';" |  mysql -u root --password=%(database_manager_admin_password)s' % env)
+    run('echo "GRANT ALL PRIVILEGES ON %(database_name)s.* TO \'%(database_username)s\'@\'%(database_host)s\' WITH GRANT OPTION;" |  mysql -u root --password=%(database_manager_admin_password)s' % env)
 
 
 def drop_database():
