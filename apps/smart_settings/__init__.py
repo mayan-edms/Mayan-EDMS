9dda1d9035b01ef8fcbafde594ea19532dc0dcfe
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-06-08 18:33:08 -0400

    Move smart_settings links to a separate file

--- apps/smart_settings/__init__.py
+++ apps/smart_settings/__init__.py
@@ -1,12 +1,9 @@
+from __future__ import absolute_import
+
 from django.utils.translation import ugettext_lazy as _
 
 from project_setup.api import register_setup
-from navigation.api import Link
 
-
-def is_superuser(context):
-    return context['request'].user.is_staff or context['request'].user.is_superuser
-
-check_settings = Link(text=_(u'settings'), view='setting_list', sprite='cog', icon='cog.png', condition=is_superuser, children_view_regex=[r'^setting_'])
+from .links import check_settings
 
 register_setup(check_settings)
