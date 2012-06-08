e901f3d914f510f41e1245d1e96f7d6d0285af4d
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-06-08 18:51:49 -0400

    Move mailer app links to a separate file

--- apps/mailer/__init__.py
+++ apps/mailer/__init__.py
@@ -2,14 +2,12 @@ from __future__ import absolute_import
 
 from django.utils.translation import ugettext_lazy as _
 
-from navigation.api import bind_links, Link
+from navigation.api import bind_links
 from documents.models import Document
 from acls.api import class_permissions
 
 from .permissions import PERMISSION_MAILING_LINK, PERMISSION_MAILING_SEND_DOCUMENT
-
-send_document_link = Link(text=_(u'email link'), view='send_document_link', args='object.pk', sprite='email_link', permissions=[PERMISSION_MAILING_LINK])
-send_document = Link(text=_(u'email document'), view='send_document', args='object.pk', sprite='email_open', permissions=[PERMISSION_MAILING_SEND_DOCUMENT])
+from .links import send_document_link, send_document
 
 bind_links([Document], [send_document_link, send_document])
 
---
+++ apps/mailer/links.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+from django.utils.translation import ugettext_lazy as _
+
+from navigation.api import Link
+
+from .permissions import PERMISSION_MAILING_LINK, PERMISSION_MAILING_SEND_DOCUMENT
+
+send_document_link = Link(text=_(u'email link'), view='send_document_link', args='object.pk', sprite='email_link', permissions=[PERMISSION_MAILING_LINK])
+send_document = Link(text=_(u'email document'), view='send_document', args='object.pk', sprite='email_open', permissions=[PERMISSION_MAILING_SEND_DOCUMENT])
