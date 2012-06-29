e6d861abf521beb251ca48fa456b94c69157b885
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-06-29 02:39:13 -0400

    Refactor installation app to move code to model and display more information

--- apps/installation/__init__.py
+++ apps/installation/__init__.py
@@ -1,7 +1,38 @@
 from __future__ import absolute_import
 
+import uuid
+
+from south.signals import post_migrate
+
 from project_tools.api import register_tool
 
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.db.utils import DatabaseError
+
 from .links import installation_details
+from .models import Installation
     
+
+@receiver(post_migrate, dispatch_uid='trigger_first_time')
+def trigger_first_time(sender, **kwargs):
+    details = Installation.objects.get()
+    details.is_first_run = True
+    details.uuid = unicode(uuid.uuid4())
+    details.save()
+
+
+def check_first_run():
+    try:
+        details = Installation.objects.get()
+    except DatabaseError:
+        pass
+    else:
+        if details.is_first_run:
+            details.is_first_run = False
+            #details.save()
+
+
 register_tool(installation_details)
+
+check_first_run()
--- apps/installation/views.py
+++ apps/installation/views.py
@@ -1,46 +1,25 @@
 from __future__ import absolute_import
 
-import sys
-import platform
-
-from pbs import CommandNotFound
-
-try:
-    from pbs import lsb_release, uname
-except CommandNotFound:
-    POSIX = False
-else:
-    POSIX = True
-
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
-from django.shortcuts import get_object_or_404
-from django.contrib.contenttypes.models import ContentType
-from django.db.models.loading import get_model
-from django.http import Http404
 from django.core.exceptions import PermissionDenied
 
 from permissions.models import Permission
 
 from .permissions import PERMISSION_INSTALLATION_DETAILS
+from .models import Installation
+
 
 def installation_details(request):
     Permission.objects.check_permissions(request.user, [PERMISSION_INSTALLATION_DETAILS])
 
     paragraphs = []
     
-    if POSIX:
-        paragraphs.append(_(u'POSIX OS'))
-        paragraphs.append(_(u'Distributor ID: %s') % lsb_release('-i','-s'))
-        paragraphs.append(_(u'Description: %s') % lsb_release('-d','-s'))
-        paragraphs.append(_(u'Release: %s') % lsb_release('-r','-s'))
-        paragraphs.append(_(u'Codename: %s') % lsb_release('-c','-s'))
-        paragraphs.append(_(u'System info: %s') % uname('-a'))
+    for name, instance in Installation().get_properties().items():
+        paragraphs.append('%s: %s' % (unicode(instance.label), instance.value))
         
-    paragraphs.append(_(u'Platform: %s') % sys.platform)
-    paragraphs.append(_(u'Processor: %s') % platform.processor())
-        
+    print Installation().architecture
     return render_to_response('generic_template.html', {
         'paragraphs': paragraphs,
         'title': _(u'Installation environment details')
