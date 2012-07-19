dd8d35aec686c3239ab7227bd7d457e04c05daa0
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-07-19 17:09:01 -0400

    Add a permit agency type database bootstrap

--- apps/bootstrap/__init__.py
+++ apps/bootstrap/__init__.py
@@ -4,7 +4,8 @@ from project_setup.api import register_setup
 from navigation.api import register_links#, register_sidebar_template
     
 from .links import database_bootstrap, bootstrap_execute
-from .api import BootstrapSimple
+from .api import BootstrapSimple, BootstrapPermit
 
 register_setup(database_bootstrap)
 register_links(BootstrapSimple, [bootstrap_execute])
+register_links(BootstrapPermit, [bootstrap_execute])
--- apps/bootstrap/api.py
+++ apps/bootstrap/api.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext
 
 from metadata.models import MetadataType, MetadataSet
 from document_indexing.models import Index, IndexTemplateNode
+from documents.models import DocumentType, DocumentTypeFilename
 
 bootstrap_options = {}
 
@@ -35,7 +36,51 @@ class BootstrapSimple(BootstrapBase):
         node1 = IndexTemplateNode.objects.create(parent=template_root, index=index, expression='metadata.upload_date[0:4]', enabled=True, link_documents=False)
         node2 = IndexTemplateNode.objects.create(parent=node1, index=index, expression='metadata.upload_date[5:7]', enabled=True, link_documents=False)
         node3 = IndexTemplateNode.objects.create(parent=node2, index=index, expression='metadata.upload_date[8:10]', enabled=True, link_documents=True)
-        
 
-for bootstrap in [BootstrapSimple()]:
+
+class BootstrapPermit(BootstrapBase):
+    name = 'permits'
+    label = _(u'Permits')
+    description = _(u'A setup for handling permits and related documents.')
+    
+    def execute(self):
+        # Create document types
+        form = DocumentType.objects.create(name=ugettext(u'Form'))
+        DocumentTypeFilename.objects.create(document_type=form, filename=ugettext(u'Building construction form'))
+        DocumentTypeFilename.objects.create(document_type=form, filename=ugettext(u'Building usage form'))
+
+        blueprint = DocumentType.objects.create(name=ugettext(u'Blueprint'))
+        DocumentTypeFilename.objects.create(document_type=blueprint, filename=ugettext(u'Floorplan'))
+        DocumentTypeFilename.objects.create(document_type=blueprint, filename=ugettext(u'Plot plan'))
+       
+        # Create metadata types
+        date = MetadataType.objects.create(name='date', title=ugettext(u'Date'), default='current_date()')
+        client = MetadataType.objects.create(name='client', title=ugettext(u'Client'))
+        permit = MetadataType.objects.create(name='permit', title=ugettext(u'Permit number'))
+        project = MetadataType.objects.create(name='project', title=ugettext(u'Project'))
+        user = MetadataType.objects.create(name='user', title=ugettext(u'User'), lookup='sorted([user.get_full_name() or user for user in User.objects.all() if user.is_active])')
+        
+        # Create a segmented date index
+        index = Index.objects.create(name='main_index', title=ugettext(u'Permit index'), enabled=True)
+        
+        # Create index template
+        per_permit = IndexTemplateNode.objects.create(parent=index.template_root, index=index, expression=ugettext(u'Per permit'), enabled=True, link_documents=False)
+        per_permit_child = IndexTemplateNode.objects.create(parent=per_permit, index=index, expression='metadata.permit', enabled=True, link_documents=True)
+
+        per_project = IndexTemplateNode.objects.create(parent=index.template_root, index=index, expression=ugettext(u'Per project'), enabled=True, link_documents=False)
+        per_project_child = IndexTemplateNode.objects.create(parent=per_project, index=index, expression='metadata.project', enabled=True, link_documents=False)
+        per_permit = IndexTemplateNode.objects.create(parent=per_project_child, index=index, expression=ugettext(u'Per permit'), enabled=True, link_documents=False)
+        per_permit_child = IndexTemplateNode.objects.create(parent=per_permit, index=index, expression='metadata.permit', enabled=True, link_documents=True)
+
+        per_date = IndexTemplateNode.objects.create(parent=index.template_root, index=index, expression=ugettext(u'Per date'), enabled=True, link_documents=False)
+        per_date_child = IndexTemplateNode.objects.create(parent=per_date, index=index, expression='metadata.date', enabled=True, link_documents=True)
+
+        per_user = IndexTemplateNode.objects.create(parent=index.template_root, index=index, expression=ugettext(u'Per user'), enabled=True, link_documents=False)
+        per_user_child = IndexTemplateNode.objects.create(parent=per_user, index=index, expression='metadata.user', enabled=True, link_documents=True)
+
+        per_client = IndexTemplateNode.objects.create(parent=index.template_root, index=index, expression=ugettext(u'Per client'), enabled=True, link_documents=False)
+        per_client_child = IndexTemplateNode.objects.create(parent=per_client, index=index, expression='metadata.client', enabled=True, link_documents=True)
+       
+
+for bootstrap in [BootstrapSimple(), BootstrapPermit()]:
     bootstrap_options[bootstrap.name] = bootstrap
