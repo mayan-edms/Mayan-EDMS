9c2bd2afaeae05eb53817329cbbe0a9736fdbe46
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-07-27 00:04:13 -0400

    Add support to the message process tool to process single apps and single languages

--- misc/process_messages.py
+++ misc/process_messages.py
@@ -26,17 +26,17 @@ else:
     BASE_DIR = os.getcwd()
 
 
-def process_all(command):
+def process(command, app_list, language_list):
     if command == makemessages:
         print 'Making messages'
     elif command == compilemessages:
         print 'Compiling messages'
 
-    for app in APP_LIST:
+    for app in app_list:
         print 'Processing app: %s...' % app
         app_path = os.path.join(BASE_DIR, 'apps', app)
         os.chdir(app_path)
-        for lang in LANGUAGE_LIST:
+        for lang in language_list:
             print 'Doing language: %s' % lang
             command(locale=lang)
 
@@ -45,12 +45,24 @@ if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option('-m', '--make', help='create message sources file', dest='make', default=False, action='store_true')
     parser.add_option('-c', '--compile', help='compile message files', dest='compile', default=False, action='store_true')
+    parser.add_option('-a', '--app', help='specify which app to process', dest='app', action='store', metavar='appname')
+    parser.add_option('-l', '--lang', help='specify which language to process', dest='lang', action='store', metavar='language')
     (opts, args) = parser.parse_args()
     
     if not opts.make and not opts.compile:
         parser.print_help()
 
+    if opts.app:
+        app_list = [opts.app]
+    else:
+        app_list = APP_LIST
+
+    if opts.lang:
+        language_list = [opts.lang]
+    else:
+        language_list = LANGUAGE_LIST
+
     if opts.make:
-        process_all(makemessages)
+        process(makemessages, app_list, language_list)
     elif opts.compile:
-        process_all(compilemessages)
+        process(compilemessages, app_list, language_list)
