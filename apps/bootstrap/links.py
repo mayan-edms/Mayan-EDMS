e6bbd5335d597f15cb29a5eb19ae6a092ae65901
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-07-27 03:57:15 -0400

    Fix bootstrap execute link sprite name

--- apps/bootstrap/links.py
+++ apps/bootstrap/links.py
@@ -7,5 +7,5 @@ from navigation.api import Link
 from .permissions import PERMISSION_BOOTSTRAP_EXECUTE, PERMISSION_NUKE_DATABASE
 
 database_bootstrap = Link(text=_(u'bootstrap database'), view='bootstrap_type_list', icon='database_lightning.png', permissions=[PERMISSION_BOOTSTRAP_EXECUTE])
-bootstrap_execute = Link(text=_(u'execute'), view='bootstrap_execute', args='object.name', sprite='database_lightning.png', permissions=[PERMISSION_BOOTSTRAP_EXECUTE])
+bootstrap_execute = Link(text=_(u'execute'), view='bootstrap_execute', args='object.name', sprite='database_lightning', permissions=[PERMISSION_BOOTSTRAP_EXECUTE])
 erase_database_link = Link(text=_(u'erase database'), view='erase_database_view', icon='radioactivity.png', permissions=[PERMISSION_NUKE_DATABASE])
