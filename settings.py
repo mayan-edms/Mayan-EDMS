60fdd18e41cc7e328c65f633dc4a2c8bc298e117
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-07-27 05:06:48 -0400

    Add the messages context processor (Django 1.4)

--- settings.py
+++ settings.py
@@ -199,6 +199,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.i18n',
     'django.core.context_processors.static',
     'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',    
 )
 
 STATICFILES_FINDERS = (
