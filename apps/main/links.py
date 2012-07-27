f8d94be59d92fb498eb4beda1f93949e0bf9b388
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-07-27 00:48:09 -0400

    Remove unused links from main app

--- apps/main/links.py
+++ apps/main/links.py
@@ -11,5 +11,3 @@ def is_superuser(context):
 maintenance_menu = Link(text=_(u'maintenance'), view='maintenance_menu', sprite='wrench', icon='wrench.png')
 statistics = Link(text=_(u'statistics'), view='statistics', sprite='table', icon='blackboard_sum.png', condition=is_superuser, children_view_regex=[r'statistics'])
 diagnostics = Link(text=_(u'diagnostics'), view='diagnostics', sprite='pill', icon='pill.png')
-sentry = Link(text=_(u'sentry'), view='sentry', sprite='bug', icon='bug.png', condition=is_superuser)
-admin_site = Link(text=_(u'admin site'), view='admin:index', sprite='keyboard', icon='keyboard.png', condition=is_superuser)
