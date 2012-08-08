d9b246c5908c6fe571c8af6a7cb0fd7f2060f473
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-08-08 03:21:18 -0400

    Capitalize trash can names

--- apps/trash/__init__.py
+++ apps/trash/__init__.py
@@ -4,8 +4,10 @@ from django.utils.translation import ugettext_lazy as _
 from django.db import transaction, DatabaseError
 
 from documents.models import Document
+from folders.models import Folder
 from navigation.api import bind_links
 from project_tools.api import register_tool
+from taggit.models import Tag
 
 from .api import make_trashable
 from .links import trash_can_list, trash_can_items, trash_can_item_restore, trash_can_item_delete
@@ -19,10 +21,14 @@ bind_links([TrashCanItem], [trash_can_item_restore, trash_can_item_delete])
 @transaction.commit_on_success
 def create_trash_cans():
     try:
-        documents_trash_can, created = TrashCan.objects.get_or_create(name='documents', defaults={'label': _(u'documents')})
+        documents_trash_can, created = TrashCan.objects.get_or_create(name='documents', defaults={'label': _(u'Documents')})
+        folders_trash_can, created = TrashCan.objects.get_or_create(name='folders', defaults={'label': _(u'Folders')})
+        tags_trash_can, created = TrashCan.objects.get_or_create(name='tags', defaults={'label': _(u'Tags')})
     except DatabaseError:
         transaction.rollback()
     else:
         make_trashable(Document, documents_trash_can)
+        make_trashable(Folder, folders_trash_can)
+        make_trashable(Tag, tags_trash_can)
 
 create_trash_cans()
