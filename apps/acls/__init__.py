86ad9bbe46ab8602dd680120fd2479e8c6ac83bd
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-08-17 04:00:02 -0400

    Reorder imports

--- apps/acls/__init__.py
+++ apps/acls/__init__.py
@@ -5,7 +5,6 @@ from project_setup.api import register_setup
 
 from .classes import (AccessHolder, AccessObjectClass, ClassAccessHolder,
     AccessObject)
-
 from .links import (acl_detail, acl_grant, acl_revoke,
     acl_holder_new, acl_setup_valid_classes, acl_class_list,
     acl_class_acl_list, acl_class_acl_detail, acl_class_new_holder_for,
@@ -13,14 +12,9 @@ from .links import (acl_detail, acl_grant, acl_revoke,
 
 bind_links([AccessHolder], [acl_detail])
 register_multi_item_links(['acl_detail'], [acl_grant, acl_revoke])
-
 bind_links([AccessObject], [acl_holder_new], menu_name='sidebar')
-
 bind_links(['acl_setup_valid_classes', 'acl_class_acl_list', 'acl_class_new_holder_for', 'acl_class_acl_detail', 'acl_class_multiple_grant', 'acl_class_multiple_revoke'], [acl_class_list], menu_name='secondary_menu')
-
 bind_links([ClassAccessHolder], [acl_class_acl_detail])
-
 bind_links([AccessObjectClass], [acl_class_acl_list, acl_class_new_holder_for])
 register_multi_item_links(['acl_class_acl_detail'], [acl_class_grant, acl_class_revoke])
-
 register_setup(acl_setup_valid_classes)
