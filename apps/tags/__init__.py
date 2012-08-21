e14238bb707a22da11c3038c7776cda8bb55e63a
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-08-21 09:16:13 -0400

    Move tags app links to a separate file

--- apps/tags/__init__.py
+++ apps/tags/__init__.py
@@ -7,27 +7,18 @@ from navigation.api import (register_links, register_top_menu,
 from common.utils import encapsulate
 from documents.models import Document
 from acls.api import class_permissions
-from acls.permissions import ACLS_VIEW_ACL
 
 from taggit.models import Tag
 from taggit.managers import TaggableManager
 
-from .widgets import (get_tags_inline_widget_simple, single_tag_widget)
-from .permissions import (PERMISSION_TAG_CREATE, PERMISSION_TAG_ATTACH,
-    PERMISSION_TAG_REMOVE, PERMISSION_TAG_DELETE, PERMISSION_TAG_EDIT,
-    PERMISSION_TAG_VIEW)
+from .links import (tag_list, tag_create, tag_attach, tag_document_remove,
+    tag_document_remove_multiple, tag_document_list, tag_delete,
+    tag_edit, tag_tagged_item_list, tag_multiple_delete, tag_acl_list)
 
-tag_list = {'text': _(u'tag list'), 'view': 'tag_list', 'famfam': 'tag_blue'}
-tag_create = {'text': _(u'create new tag'), 'view': 'tag_create', 'famfam': 'tag_blue_add', 'permissions': [PERMISSION_TAG_CREATE]}
-tag_attach = {'text': _(u'attach tag'), 'view': 'tag_attach', 'args': 'object.pk', 'famfam': 'tag_blue_add', 'permissions': [PERMISSION_TAG_ATTACH]}
-tag_document_remove = {'text': _(u'remove'), 'view': 'tag_remove', 'args': ['object.id', 'document.id'], 'famfam': 'tag_blue_delete', 'permissions': [PERMISSION_TAG_REMOVE]}
-tag_document_remove_multiple = {'text': _(u'remove'), 'view': 'tag_multiple_remove', 'args': 'document.id', 'famfam': 'tag_blue_delete', 'permissions': [PERMISSION_TAG_REMOVE]}
-tag_document_list = {'text': _(u'tags'), 'view': 'document_tags', 'args': 'object.pk', 'famfam': 'tag_blue', 'permissions': [PERMISSION_TAG_REMOVE, PERMISSION_TAG_ATTACH], 'children_view_regex': ['tag']}
-tag_delete = {'text': _(u'delete'), 'view': 'tag_delete', 'args': 'object.id', 'famfam': 'tag_blue_delete', 'permissions': [PERMISSION_TAG_DELETE]}
-tag_edit = {'text': _(u'edit'), 'view': 'tag_edit', 'args': 'object.id', 'famfam': 'tag_blue_edit', 'permissions': [PERMISSION_TAG_EDIT]}
-tag_tagged_item_list = {'text': _(u'tagged documents'), 'view': 'tag_tagged_item_list', 'args': 'object.id', 'famfam': 'page'}
-tag_multiple_delete = {'text': _(u'delete'), 'view': 'tag_multiple_delete', 'famfam': 'tag_blue_delete', 'permissions': [PERMISSION_TAG_DELETE]}
-tag_acl_list = {'text': _(u'ACLs'), 'view': 'tag_acl_list', 'args': 'object.pk', 'famfam': 'lock', 'permissions': [ACLS_VIEW_ACL]}
+from .widgets import (get_tags_inline_widget_simple, single_tag_widget)
+from .permissions import (PERMISSION_TAG_ATTACH, PERMISSION_TAG_REMOVE,
+    PERMISSION_TAG_DELETE, PERMISSION_TAG_EDIT, PERMISSION_TAG_VIEW)
+
 
 register_model_list_columns(Tag, [
     {
