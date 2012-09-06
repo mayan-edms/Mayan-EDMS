40fcbc310f47135122a1b9f4366a6d23fb2a672b
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-06 14:18:42 -0400

    Register icon app with app registry add additional literals

--- apps/icons/registry.py
+++ apps/icons/registry.py
@@ -1,5 +1,11 @@
+from __future__ import absolute_import
+
 from django.utils.translation import ugettext_lazy as _
+
+from .icons import icon_icons_app
 
 name = 'icons'
 label = _(u'Icons')
-#app.set_dependencies(['app_registry'])
+description = _(u'Handles the registration and rendering of icons and sprites.')
+dependencies = ['app_registry']
+icon = icon_icons_app
