8e4418c1c76c0c31c4b1f7fe2fb0caa7c23428e9
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-06 14:19:25 -0400

    Convert project setup app to new icon system

--- apps/project_setup/__init__.py
+++ apps/project_setup/__init__.py
@@ -1,5 +1,9 @@
+from __future__ import absolute_import
+
 from django.utils.translation import ugettext_lazy as _
 
 from navigation.api import register_top_menu, Link
 
-setup_menu = register_top_menu('setup_menu', link=Link(text=_(u'setup'), view='setup_list', sprite='cog'), position=-2)
+from .icons import icon_setup
+
+setup_menu = register_top_menu('setup_menu', link=Link(text=_(u'setup'), view='setup_list', icon=icon_setup), position=-2)
