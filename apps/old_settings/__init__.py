9afba087bd7f0226fbb51707905aac3a42b06708
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-07 03:36:40 -0400

    Delete old settings app, move translations to new settings app

--- apps/old_settings/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-from project_setup.api import register_setup
-
-from .links import check_settings
-
-register_setup(check_settings)
--- apps/old_settings/links.py
+++
@@ -1,9 +0,0 @@
-from django.utils.translation import ugettext_lazy as _
-
-from navigation.api import Link
-
-
-def is_superuser(context):
-    return context['request'].user.is_staff or context['request'].user.is_superuser
-
-check_settings = Link(text=_(u'settings'), view='setting_list', sprite='cog', icon='cog.png', condition=is_superuser, children_view_regex=[r'^setting_'])
