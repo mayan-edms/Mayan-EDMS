86f2ed56e02f49eec22d4bcd01bae67a60230d25
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-08 02:54:08 -0400

    Update the diagnostics, maintenance and statistics apps icons

---
+++ apps/diagnostics/icons.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+from icons.literals import PILL, LIGHTNING
+from icons import Icon
+
+icon_diagnostic = Icon(PILL)
+icon_diagnostic_execute = Icon(LIGHTNING)
+
--- apps/statistics/links.py
+++ apps/statistics/links.py
@@ -4,9 +4,11 @@ from django.utils.translation import ugettext_lazy as _
 
 from navigation.api import Link
 
+from .icons import icon_statistics
+
 
 def is_superuser(context):
     return context['request'].user.is_staff or context['request'].user.is_superuser
 
 
-statistics_link = Link(text=_(u'statistics'), view='statistics', sprite='table', icon='blackboard_sum.png', condition=is_superuser, children_view_regex=[r'statistics'])
+statistics_link = Link(text=_(u'statistics'), view='statistics', icon=icon_statistics, condition=is_superuser, children_view_regex=[r'statistics'])
