87cb368c092237bd8e8da8981471a6aa61ee8199
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-11 12:59:40 -0400

    Update linking registy icon and description

--- apps/linking/registry.py
+++ apps/linking/registry.py
@@ -4,12 +4,12 @@ from django.utils.translation import ugettext_lazy as _
 
 from smart_settings import LocalScope
 
-#from .icons import icon_tick
+from .icons import icon_smart_link_setup
 
 label = _(u'Document linking')
-#description = _(u'Contains many commonly used models, views and utilities.')
+description = _(u'Links documents together using a rule based system.')
 dependencies = ['app_registry', 'icons', 'navigation', 'documents', 'metadata']
-#icon = icon_tick
+icon = icon_smart_link_setup
 settings = [
     {
         'name': 'SHOW_EMPTY_SMART_LINKS',
