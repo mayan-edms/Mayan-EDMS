3fe003b83b2e048fbed38432df5830e549409bb3
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-10 22:01:28 -0400

    Update the mimetype app to user the new registry and icon apps

---
+++ apps/mimetype/icons.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+from icons.literals import FILE_EXTENSION_ERROR, FILE_EXTENSION_UNKNOWN, ICON_SET_CUSTOM
+from icons import Icon
+
+icon_file_extension_error = Icon(FILE_EXTENSION_ERROR, ICON_SET_CUSTOM)
+icon_file_extension_unknown = Icon(FILE_EXTENSION_UNKNOWN, ICON_SET_CUSTOM)
