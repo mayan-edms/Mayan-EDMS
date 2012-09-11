29423dc85ba509d2adbe809523f57309fd108f38
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-11 12:59:23 -0400

    Cleanup remarked code

--- apps/metadata/models.py
+++ apps/metadata/models.py
@@ -5,11 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 
 from documents.models import Document, DocumentType
 
-#from .settings import (AVAILABLE_MODELS, AVAILABLE_FUNCTIONS)
-
-#available_models_string = (_(u' Available models: %s') % u','.join([name for name, model in AVAILABLE_MODELS.items()])) if AVAILABLE_MODELS else u''
-#available_functions_string = (_(u' Available functions: %s') % u','.join([u'%s()' % name for name, function in AVAILABLE_FUNCTIONS.items()])) if AVAILABLE_FUNCTIONS else u''
-
 
 class MetadataType(models.Model):
     """
@@ -19,11 +14,9 @@ class MetadataType(models.Model):
     title = models.CharField(max_length=48, verbose_name=_(u'title'), blank=True, null=True)
     default = models.CharField(max_length=128, blank=True, null=True,
         verbose_name=_(u'default'),
-        #help_text=_(u'Enter a string to be evaluated.%s') % available_functions_string)
         help_text=_(u'Enter a string to be evaluated.'))
     lookup = models.CharField(max_length=128, blank=True, null=True,
         verbose_name=_(u'lookup'),
-        #help_text=_(u'Enter a string to be evaluated.  Example: [user.get_full_name() for user in User.objects.all()].%s') % available_models_string)
         help_text=_(u'Enter a string to be evaluated.  Example: [user.get_full_name() for user in User.objects.all()].'))
     #TODO: datatype?
 
