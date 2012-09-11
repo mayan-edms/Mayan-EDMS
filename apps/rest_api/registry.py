08a0f80918baf859765de7a989e57b47fb3e598a
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-10 23:09:36 -0400

    Add rest api app description

--- apps/rest_api/registry.py
+++ apps/rest_api/registry.py
@@ -5,6 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 #from .icons import icon_settings
 
 label = _(u'API')
-#description = _(u'Handles the configuration settings of all apps')
+description = _(u'Exposes an application programming interface to the project internal objects.')
 #icon = icon_settings
 dependencies = ['app_registry']
