a7c76e1a736e09c7d6c3aec699ca1bc1fa3664b0
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-10 22:05:32 -0400

    Update the rest api app to use app registry

---
+++ apps/rest_api/registry.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+from django.utils.translation import ugettext_lazy as _
+
+#from .icons import icon_settings
+
+label = _(u'API')
+#description = _(u'Handles the configuration settings of all apps')
+#icon = icon_settings
+dependencies = ['app_registry']
