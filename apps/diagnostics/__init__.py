602bf45d1b6b1efcbae4ca4d5010ce8d9b5f409f
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-14 03:30:43 -0400

    Remove explicit tool registration

--- apps/diagnostics/__init__.py
+++ apps/diagnostics/__init__.py
@@ -1,11 +1,9 @@
 from __future__ import absolute_import
 
-from project_tools.api import register_tool
 from navigation.api import bind_links
 
 from .links import diagnostic_list, diagnostic_execute
 from .api import DiagnosticTool
 
-register_tool(diagnostic_list)
 bind_links(['diagnostic_list'], diagnostic_list, menu_name='secondary_menu')
 bind_links([DiagnosticTool], diagnostic_execute)
