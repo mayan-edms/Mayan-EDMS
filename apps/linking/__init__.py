799c0cfd5765d0ea6b53e9fc9d0b49c5ebf7cdec
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-22 00:49:57 -0400

    Don't register setup link in __init__

--- apps/linking/__init__.py
+++ apps/linking/__init__.py
@@ -6,10 +6,9 @@ from acls.api import class_permissions
 from acls.permissions import ACLS_EDIT_ACL, ACLS_VIEW_ACL
 from documents.models import Document
 from navigation.api import bind_links, register_sidebar_template
-from project_setup.api import register_setup
 
 from .links import (smart_link_instances_for_document,
-    smart_link_setup, smart_link_list, smart_link_create, smart_link_edit,
+    smart_link_list, smart_link_create, smart_link_edit,
     smart_link_delete, smart_link_condition_list, smart_link_condition_create,
     smart_link_condition_edit, smart_link_condition_delete, smart_link_acl_list)
 from .models import SmartLink, SmartLinkCondition
@@ -23,7 +22,6 @@ bind_links([SmartLink, 'smart_link_list', 'smart_link_create'], [smart_link_list
 bind_links([SmartLinkCondition], [smart_link_condition_edit, smart_link_condition_delete])
 bind_links(['smart_link_condition_list', 'smart_link_condition_create', 'smart_link_condition_edit', 'smart_link_condition_delete'], [smart_link_condition_create], menu_name='sidebar')
 
-register_setup(smart_link_setup)
 register_sidebar_template(['smart_link_list'], 'smart_links_help.html')
 
 class_permissions(SmartLink, [
