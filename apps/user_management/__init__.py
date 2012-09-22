571ce1723ce06ead5f94ecb83f0708bdbfb27801
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-22 00:43:32 -0400

    Update user management app to use app registry

--- apps/user_management/__init__.py
+++ apps/user_management/__init__.py
@@ -4,11 +4,10 @@ from django.contrib.auth.models import User, Group
 from django.utils.translation import ugettext_lazy as _
 
 from navigation.api import bind_links, register_multi_item_links
-from project_setup.api import register_setup
 
-from .links import (user_list, user_setup, user_edit, user_add, user_delete,
+from .links import (user_list, user_edit, user_add, user_delete,
     user_multiple_delete, user_set_password, user_multiple_set_password,
-    group_list, group_setup, group_edit, group_add, group_delete,
+    group_list, group_edit, group_add, group_delete,
     group_multiple_delete, group_members)
 
 bind_links([User], [user_edit, user_set_password, user_delete])
@@ -18,13 +17,3 @@ register_multi_item_links(['user_list'], [user_multiple_set_password, user_multi
 bind_links([Group], [group_edit, group_members, group_delete])
 bind_links(['group_multiple_delete', 'group_delete', 'group_edit', 'group_list', 'group_add', 'group_members'], [group_list, group_add], menu_name=u'secondary_menu')
 register_multi_item_links(['group_list'], [group_multiple_delete])
-
-user_management_views = [
-    'user_list', 'user_edit', 'user_add', 'user_delete',
-    'user_multiple_delete', 'user_set_password',
-    'user_multiple_set_password', 'group_list', 'group_edit', 'group_add',
-    'group_delete', 'group_multiple_delete', 'group_members'
-]
-
-register_setup(user_setup)
-register_setup(group_setup)
--- apps/user_management/registry.py
+++ apps/user_management/registry.py
@@ -3,8 +3,10 @@ from __future__ import absolute_import
 from django.utils.translation import ugettext_lazy as _
 
 from .icons import icon_user
+from .links import user_setup, group_setup
 
 label = _(u'User management')
 description = _(u'Handles user accounts and groups.')
 icon = icon_user
 dependencies = ['app_registry', 'icons', 'navigation', 'permissions']
+setup_links = [user_setup, group_setup]
