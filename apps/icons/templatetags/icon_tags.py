aef795bffa423e52705965e3c1eacb547666c2b7
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-22 00:45:03 -0400

    Add template tag to render icons directly from the templates

---
+++ apps/icons/templatetags/icon_tags.py
@@ -0,0 +1,16 @@
+from django.template import Library
+
+from icons import Icon
+
+register = Library()
+
+
+@register.simple_tag
+def icon_small(icon_name):
+    print "*******icon_name", icon_name
+    return Icon(icon_name).display_small()
+
+
+@register.simple_tag
+def icon_big(icon_name):
+    return Icon(icon_name).display_big()
