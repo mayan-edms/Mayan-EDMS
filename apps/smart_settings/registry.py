88bfe2936506f126ee9407b3426eb6cf33d974ac
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-22 00:51:33 -0400

    Register setup link in registry

--- apps/smart_settings/registry.py
+++ apps/smart_settings/registry.py
@@ -3,9 +3,11 @@ from __future__ import absolute_import
 from django.utils.translation import ugettext_lazy as _
 
 from .icons import icon_settings
+from .links import link_settings
 
 name = 'smart_settings'
 label = _(u'Smart settings')
 description = _(u'Handles the configuration settings of all apps')
 icon = icon_settings
 dependencies = ['app_registry']
+setup_links = [link_settings]
