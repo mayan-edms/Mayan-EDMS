2bc008d298803e10835e5db85ffbe640eaf45d4c
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-22 00:47:36 -0400

    Add new default icon for the cancel button in forms

--- apps/common/templatetags/default_icons.py
+++ apps/common/templatetags/default_icons.py
@@ -1,6 +1,6 @@
 from django.template import Library
 
-from common.icons import icon_tick
+from common.icons import icon_tick, icon_cross
 
 register = Library()
 
@@ -8,4 +8,5 @@ register = Library()
 @register.simple_tag(takes_context=True)
 def default_icons(context):
     context['submit_icon_default'] = icon_tick
+    context['cancel_icon_default'] = icon_cross
     return u''
