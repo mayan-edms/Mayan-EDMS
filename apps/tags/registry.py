0a72fccb975aed306dcd6318dfb4aaee7e8680f6
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-24 17:38:00 -0400

    Add tags app registry description

--- apps/tags/registry.py
+++ apps/tags/registry.py
@@ -5,6 +5,8 @@ from django.utils.translation import ugettext_lazy as _
 from .icons import icon_tag_list
 
 label = _(u'Tagging')
-#description = _(u'Central place to store and display app statistics.')
+description = _(u'Handles document tagging.')
 dependencies = ['app_registry', 'icons', 'navigation']
 icon = icon_tag_list
+#bootstrap_models = ['taggit.tag', 'tagproperties']
+#TODO: Disabled until a solution for natural keys for Tags is found
