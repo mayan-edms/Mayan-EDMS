2aa1c2fcce87801fae7db4ecfa798a943015033c
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-27 01:50:06 -0400

    Remove AutoAdminSingleton from common app

--- apps/common/models.py
+++ apps/common/models.py
@@ -53,15 +53,6 @@ class AnonymousUserSingleton(Singleton):
         verbose_name_plural = _(u'anonymous user')
 
 
-class AutoAdminSingleton(Singleton):
-    account = models.ForeignKey(User, null=True, blank=True, related_name='auto_admin_account', verbose_name=_(u'account'))
-    password = models.CharField(null=True, blank=True, verbose_name=_(u'password'), max_length=128)
-    password_hash = models.CharField(null=True, blank=True, verbose_name=_(u'password hash'), max_length=128)
-
-    class Meta:
-        verbose_name = verbose_name_plural = _(u'auto admin properties')
-
-
 class TranslatableLabelMixin(models.Model):
     _translatable_registry = {}
     
