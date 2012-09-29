fe2d57e2f3aa1532645a572e548a5d38c34de2b6
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-29 01:11:25 -0400

    Swtich to composed icons

--- apps/permissions/links.py
+++ apps/permissions/links.py
@@ -7,12 +7,12 @@ from navigation import Link
 from .permissions import (PERMISSION_ROLE_VIEW, PERMISSION_ROLE_EDIT,
     PERMISSION_ROLE_CREATE, PERMISSION_ROLE_DELETE,
     PERMISSION_PERMISSION_GRANT, PERMISSION_PERMISSION_REVOKE)
-from .icons import (icon_role, icon_role_add, icon_role_delete, icon_permissions,
-    icon_permission_grant, icon_permission_revoke, icon_role_members)
+from .icons import (icon_role, icon_role_add, icon_role_delete, icon_role_edit,
+    icon_permissions, icon_permission_grant, icon_permission_revoke, icon_role_members)
 
 role_list = Link(text=_(u'roles'), view='role_list', icon=icon_role, permissions=[PERMISSION_ROLE_VIEW])#, 'children_view_regex': [r'^permission_', r'^role_'])
 role_create = Link(text=_(u'create new role'), view='role_create', icon=icon_role_add, permissions=[PERMISSION_ROLE_CREATE])
-role_edit = Link(text=_(u'edit'), view='role_edit', args='object.id', icon=icon_role, permissions=[PERMISSION_ROLE_EDIT])
+role_edit = Link(text=_(u'edit'), view='role_edit', args='object.id', icon=icon_role_edit, permissions=[PERMISSION_ROLE_EDIT])
 role_members = Link(text=_(u'members'), view='role_members', args='object.id', icon=icon_role_members, permissions=[PERMISSION_ROLE_EDIT])
 role_permissions = Link(text=_(u'role permissions'), view='role_permissions', args='object.id', icon=icon_permissions, permissions=[PERMISSION_PERMISSION_GRANT, PERMISSION_PERMISSION_REVOKE])
 role_delete = Link(text=_(u'delete'), view='role_delete', args='object.id', icon=icon_role_delete, permissions=[PERMISSION_ROLE_DELETE])
