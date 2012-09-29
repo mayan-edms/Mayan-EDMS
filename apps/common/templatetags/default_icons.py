deef1dc19c43a799e6fa2c6aadf348392cba22c8
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-29 02:12:45 -0400

    Add icon_current_user_details icon to the default icons templatetag

--- apps/common/templatetags/default_icons.py
+++ apps/common/templatetags/default_icons.py
@@ -1,6 +1,6 @@
 from django.template import Library
 
-from common.icons import icon_tick, icon_cross, icon_resultset_previous
+from common.icons import (icon_tick, icon_cross, icon_resultset_previous, icon_current_user_details)
 
 register = Library()
 
@@ -10,4 +10,5 @@ def default_icons(context):
     context['icon_submit_default'] = icon_tick
     context['icon_cancel_default'] = icon_cross
     context['icon_current_link'] = icon_resultset_previous
+    context['icon_current_user_details'] = icon_current_user_details
     return u''
