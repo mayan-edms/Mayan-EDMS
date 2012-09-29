0ee2c98d516aaaa22f974f7bce4c1797a3f343b6
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-09-29 01:09:37 -0400

    Swtich to composed icons

--- apps/bootstrap/icons.py
+++ apps/bootstrap/icons.py
@@ -1,13 +1,14 @@
 from __future__ import absolute_import
 
-from icons.literals import WIZARD, LIGHTNING, RADIOACTIVITY, MAGIC_WAND_2
+from icons.literals import (WIZARD, LIGHTNING, RADIOACTIVITY, MAGIC_WAND_2,
+    WIZARD_ADD, WIZARD_EDIT, WIZARD_DELETE, WIZARD_LIGHTNING)
 from icons import Icon
 
 icon_bootstrap_setup = Icon(WIZARD)
 icon_bootstrap_setup_view = Icon(WIZARD)
-icon_bootstrap_setup_create = Icon(WIZARD)
-icon_bootstrap_setup_edit = Icon(WIZARD)
-icon_bootstrap_setup_delete = Icon(WIZARD)
-icon_bootstrap_setup_execute = Icon(LIGHTNING)
+icon_bootstrap_setup_create = Icon(WIZARD_ADD)
+icon_bootstrap_setup_edit = Icon(WIZARD_EDIT)
+icon_bootstrap_setup_delete = Icon(WIZARD_DELETE)
+icon_bootstrap_setup_execute = Icon(WIZARD_LIGHTNING)
 icon_bootstrap_setup_dump = Icon(MAGIC_WAND_2)
 icon_nuke_database = Icon(RADIOACTIVITY)
