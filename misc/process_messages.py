6f1c1a8b9803e1e8f697960d2368e8e94a4dc12c
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-10-01 09:05:09 -0400

    Remove the old language processing scripts and backport the process_message python utility from the development branch

--- misc/process_messages.py
+++ misc/process_messages.py
@@ -5,13 +5,12 @@ import optparse
 
 import pbs
 
-APP_LIST = ('common', 'converter', 'documents', 'document_comments',
-    'document_indexing', 'dynamic_search', 'folders', 'history',
-    'linking', 'main', 'metadata', 'navigation', 'ocr', 'permissions',
-    'project_setup', 'project_tools', 'smart_settings', 'sources',
-    'tags', 'user_management', 'web_theme', 'django_gpg', 'document_signatures',
-    'acls', 'installation', 'scheduler', 'checkouts')
-LANGUAGE_LIST = ('en', 'pt', 'pt_BR', 'ru', 'es', 'it', 'pl', 'de_DE', 'fr', 'nl_NL', 'bg')
+APP_LIST = ('acls', 'checkouts', 'common', 'converter', 'django_gpg', 'documents',
+    'document_comments', 'document_indexing', 'document_signatures', 'dynamic_search',
+    'folders', 'history', 'installation', 'linking', 'main', 'metadata', 'navigation',
+    'ocr', 'permissions', 'project_setup', 'project_tools', 'scheduler', 'smart_settings',
+    'sources', 'tags', 'user_management', 'web_theme')
+LANGUAGE_LIST = ('bg', 'de_DE', 'en', 'es', 'fr', 'it', 'nl_NL', 'pl', 'pt', 'pt_BR', 'ru')
 
 makemessages = pbs.Command('django-admin.py')
 makemessages = makemessages.bake('makemessages')
