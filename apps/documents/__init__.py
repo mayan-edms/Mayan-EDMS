3186b8a300265b4db0d038d4e0f76afa1698c4ef
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2012-10-01 12:13:45 -0400

    Show multi document actions in the tagged document list view

--- apps/documents/__init__.py
+++ apps/documents/__init__.py
@@ -130,7 +130,7 @@ register_links([DocumentType], [document_type_filename_create], menu_name='sideb
 
 # Register document links
 register_links(Document, [document_view_simple, document_edit, document_print, document_delete, document_download, document_find_duplicates, document_clear_transformations, document_create_siblings])
-register_multi_item_links(['document_find_duplicates', 'folder_view', 'index_instance_node_view', 'document_type_document_list', 'search', 'results', 'document_group_view', 'document_list', 'document_list_recent'], [document_multiple_clear_transformations, document_multiple_delete, document_multiple_download])
+register_multi_item_links(['document_find_duplicates', 'folder_view', 'index_instance_node_view', 'document_type_document_list', 'search', 'results', 'document_group_view', 'document_list', 'document_list_recent', 'tag_tagged_item_list'], [document_multiple_clear_transformations, document_multiple_delete, document_multiple_download])
 
 # Document Version links
 register_links(DocumentVersion, [document_version_revert, document_version_download])
--- apps/tags/__init__.py
+++ apps/tags/__init__.py
@@ -47,7 +47,7 @@ register_links(Document, [tag_document_list], menu_name='form_header')
 register_links(['document_tags', 'tag_remove', 'tag_multiple_remove', 'tag_attach'], [tag_attach], menu_name='sidebar')
 register_multi_item_links(['document_tags'], [tag_document_remove_multiple])
 
-register_multi_item_links(['document_find_duplicates', 'folder_view', 'index_instance_node_view', 'document_type_document_list', 'search', 'results', 'document_group_view', 'document_list', 'document_list_recent'], [tag_multiple_attach])
+register_multi_item_links(['document_find_duplicates', 'folder_view', 'index_instance_node_view', 'document_type_document_list', 'search', 'results', 'document_group_view', 'document_list', 'document_list_recent', 'tag_tagged_item_list'], [tag_multiple_attach])
 
 class_permissions(Document, [
     PERMISSION_TAG_ATTACH,
