53528e55fb716b4aac8842087fe8e4143ef5d1b9
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-05-17 19:04:43 -0400

    Remove convenience scripts

--- runserver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -n "$1" ]; then
-        ./manage.py runserver $1
-else
-        ./manage.py runserver
-fi
--- runserver_plus.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -n "$1" ]; then
-	./manage.py runserver_plus $1 --adminmedia ./static/admin/
-else
-	./manage.py runserver_plus --adminmedia ./static/admin/
-fi
