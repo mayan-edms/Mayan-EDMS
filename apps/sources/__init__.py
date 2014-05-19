c7205aaa59d985b32fbe7560380ec3a5c0261705
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-05-19 00:38:25 -0400

    Fix the document upload link not appearing on some views

--- apps/sources/__init__.py
+++ apps/sources/__init__.py
@@ -76,5 +76,5 @@ register_model_list_columns(StagingFile, [
 
 register_setup(setup_sources)
 
-register_links(['document_list_recent', 'document_list', 'document_create', 'document_create_multiple', 'upload_interactive', 'staging_file_delete'], [document_create_multiple], menu_name='secondary_menu')
+register_links([Document, 'document_list_recent', 'document_list', 'document_create', 'document_create_multiple', 'upload_interactive', 'staging_file_delete'], [document_create_multiple], menu_name='secondary_menu')
 register_links(Document, [document_create_siblings])
