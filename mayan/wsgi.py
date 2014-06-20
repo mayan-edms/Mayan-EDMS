ae5317ae83405bd18d638bbe92922083b82d723d
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-06-20 17:34:05 -0400

    Fix wsgi.py comment typo

--- mayan/wsgi.py
+++ mayan/wsgi.py
@@ -1,8 +1,14 @@
 """
+WSGI config for mayan project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mayan.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
