c4439f45de7b5e336c51cedf5cd9dec01ad5f70d
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-06-25 01:25:53 -0400

    Move project tool links to their own module

--- mayan/apps/project_tools/__init__.py
+++ mayan/apps/project_tools/__init__.py
@@ -1,5 +1,9 @@
+from __future__ import absolute_import
+
 from django.utils.translation import ugettext_lazy as _
 
 from navigation.api import register_top_menu
 
-tool_link = register_top_menu('tools', link={'text': _(u'tools'), 'view': 'tools_list', 'famfam': 'wrench'}, position=-3)
+from .links import link_tools
+
+tool_link = register_top_menu('tools', link=link_tools, position=-3)
---
+++ mayan/apps/project_tools/links.py
@@ -0,0 +1,3 @@
+from django.utils.translation import ugettext_lazy as _
+
+link_tools = link={'text': _(u'tools'), 'view': 'tools_list', 'famfam': 'wrench'}
