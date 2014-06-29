eecdd9dcea66c5eca5c3e1e6737c04bc793a3164
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-06-29 15:58:48 -0400

    Update settings.py file to conform with the Django 1.6 settings files

--- mayan/settings.py
+++ mayan/settings.py
@@ -1,132 +1,37 @@
-# Django settings for mayan project.
-import os
-import sys
+"""
+Django settings for testproject project.
 
-from django.core.urlresolvers import reverse_lazy
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
 
 ugettext = lambda s: s
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SITE_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-sys.path.append(os.path.join(BASE_DIR, 'apps'))
 
-PROJECT_TITLE = 'Mayan EDMS'
-PROJECT_NAME = 'mayan'
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-DEBUG = False
-DEVELOPMENT = False
-TEMPLATE_DEBUG = False
-
-ADMINS = ()
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(SITE_ROOT, '%s.sqlite' % PROJECT_NAME),     # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Puerto_Rico'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en'
-
-LANGUAGES = (
-    ('ar', ugettext('Arabic')),
-    ('bg', ugettext('Bulgarian')),
-    ('bs', ugettext('Bosnian (Bosnia and Herzegovina)')),
-    ('da', ugettext('Danish')),
-    ('de', ugettext('German (Germany)')),
-    ('en', ugettext('English')),
-    ('es', ugettext('Spanish')),
-    ('fa', ugettext('Persian')),
-    ('fr', ugettext('French')),
-    ('hu', ugettext('Hungarian')),
-    ('hr', ugettext('Croatian')),
-    ('id', ugettext('Indonesian')),
-    ('it', ugettext('Italian')),
-    ('nl', ugettext('Dutch (Nethherlands)')),
-    ('pl', ugettext('Polish')),
-    ('pt', ugettext('Portuguese')),
-    ('pt-br', ugettext('Portuguese (Brazil)')),
-    ('ro', ugettext('Romanian (Romania)')),
-    ('ru', ugettext('Russian')),
-    ('sl', ugettext('Slovenian')),
-    ('tr', ugettext('Turkish')),
-    ('vi', ugettext('Vietnamese (Viet Nam)')),
-)
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
-
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/"
-STATIC_ROOT = os.path.join(SITE_ROOT, 'static/')
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-STATIC_URL = '/%s-static/' % PROJECT_NAME
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-
-# Make this unique, and don't share it with anybody.
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'om^a(i8^6&h+umbd2%pt91cj!qu_@oztw117rgxmn(n2lp^*c!'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    ('django.template.loaders.cached.Loader', (
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader',
-    )),
-)
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'common.middleware.strip_spaces_widdleware.SpacelessMiddleware',
-    'common.middleware.login_required_middleware.LoginRequiredMiddleware',
-    'permissions.middleware.permission_denied_middleware.PermissionDeniedMiddleware',
-    'pagination.middleware.PaginationMiddleware',
-)
+TEMPLATE_DEBUG = True
 
-ROOT_URLCONF = 'mayan.urls'
+ALLOWED_HOSTS = []
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    #os.path.join(PROJECT_ROOT, 'templates')
-)
+
+# Application definition
 
 INSTALLED_APPS = (
 #Django
@@ -193,6 +98,112 @@ INSTALLED_APPS = (
     'signaler',
 )
 
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'common.middleware.strip_spaces_widdleware.SpacelessMiddleware',
+    'common.middleware.login_required_middleware.LoginRequiredMiddleware',
+    'permissions.middleware.permission_denied_middleware.PermissionDeniedMiddleware',
+    'pagination.middleware.PaginationMiddleware',
+)
+
+ROOT_URLCONF = 'mayan.urls'
+
+WSGI_APPLICATION = 'mayan.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(SITE_ROOT, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Custom settings section
+
+import sys
+
+from django.core.urlresolvers import reverse_lazy
+
+sys.path.append(os.path.join(BASE_DIR, 'apps'))
+
+PROJECT_TITLE = 'Mayan EDMS'
+PROJECT_NAME = 'mayan'
+
+LANGUAGES = (
+    ('ar', ugettext('Arabic')),
+    ('bg', ugettext('Bulgarian')),
+    ('bs', ugettext('Bosnian (Bosnia and Herzegovina)')),
+    ('da', ugettext('Danish')),
+    ('de', ugettext('German (Germany)')),
+    ('en', ugettext('English')),
+    ('es', ugettext('Spanish')),
+    ('fa', ugettext('Persian')),
+    ('fr', ugettext('French')),
+    ('hu', ugettext('Hungarian')),
+    ('hr', ugettext('Croatian')),
+    ('id', ugettext('Indonesian')),
+    ('it', ugettext('Italian')),
+    ('nl', ugettext('Dutch (Nethherlands)')),
+    ('pl', ugettext('Polish')),
+    ('pt', ugettext('Portuguese')),
+    ('pt-br', ugettext('Portuguese (Brazil)')),
+    ('ro', ugettext('Romanian (Romania)')),
+    ('ru', ugettext('Russian')),
+    ('sl', ugettext('Slovenian')),
+    ('tr', ugettext('Turkish')),
+    ('vi', ugettext('Vietnamese (Viet Nam)')),
+)
+
+SITE_ID = 1
+
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static/')
+
+STATIC_URL = '/%s-static/' % PROJECT_NAME
+
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
+)
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    # os.path.join(PROJECT_ROOT, 'templates')
+)
+
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.i18n',
@@ -207,17 +218,19 @@ STATICFILES_FINDERS = (
     # other finders..
     'compressor.finders.CompressorFinder',
 )
+
+# --------- Django compressor -------------
 COMPRESS_PARSER = 'compressor.parser.HtmlParser'
 COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter', 'compressor.filters.cssmin.CSSMinFilter']
-
 COMPRESS_ENABLED=False
+# ---------- Django sendfile --------------
 SENDFILE_BACKEND = 'sendfile.backends.simple'
-#--------- Web theme ---------------
+# --------- Web theme ---------------
 WEB_THEME_ENABLE_SCROLL_JS = False
-#--------- Django -------------------
+# --------- Django -------------------
 LOGIN_URL = reverse_lazy('login_view')
 LOGIN_REDIRECT_URL = reverse_lazy('home')
-#-------- LoginRequiredMiddleware ----------
+# -------- LoginRequiredMiddleware ----------
 LOGIN_EXEMPT_URLS = (
     r'^favicon\.ico$',
     r'^about\.html$',
@@ -236,15 +249,15 @@ LOGIN_EXEMPT_URLS = (
     r'^password/reset/complete/$',
     r'^password/reset/done/$',
 )
-#--------- Pagination ----------------
+# --------- Pagination ----------------
 PAGINATION_INVALID_PAGE_RAISES_404 = True
-#---------- Search ------------------
+# ---------- Search ------------------
 SEARCH_SHOW_OBJECT_TYPE = False
 
 SERIALIZATION_MODULES = {
     'better_yaml': 'common.serializers.better_yaml',
 }
-#--------- Taggit ------------
+# --------- Taggit ------------
 SOUTH_MIGRATION_MODULES = {
     'taggit': 'taggit.south_migrations',
 }
@@ -254,8 +267,7 @@ try:
 except ImportError:
     pass
 
-
-if DEVELOPMENT:
+if DEBUG:
     INTERNAL_IPS = ('127.0.0.1',)
 
     TEMPLATE_LOADERS = (
