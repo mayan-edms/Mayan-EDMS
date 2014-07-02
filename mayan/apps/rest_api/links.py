b0eca8bb3e9a9f5b038416c014ba606c33c3481e
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-07-02 16:18:56 -0400

    Add REST API app links to the setup menu

---
+++ mayan/apps/rest_api/links.py
@@ -0,0 +1,3 @@
+from django.utils.translation import ugettext_lazy as _
+
+link_api = {'text': _(u'REST API'), 'view': 'api-root', 'famfam': 'disconnect', 'icon': 'disconnect.png'}
---
+++ mayan/apps/rest_api/registry.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from .links import link_api
+
+setup_links = [link_api]
