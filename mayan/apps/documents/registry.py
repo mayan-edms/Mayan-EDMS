e2b3cee69b3606cda9dae1b7780199f891cf2a6a
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-07-02 14:11:39 -0400

    Update API endpoint description

--- mayan/apps/documents/registry.py
+++ mayan/apps/documents/registry.py
@@ -17,6 +17,6 @@ bootstrap_models = [
 cleanup_functions = [cleanup]
 
 version_0_api_services = [
-    {'urlpattern': url(r'^document/(?P<pk>[0-9]+)/$', APIReadOnlyInstanceModelView.as_view(), name='document-detail'), 'description': 'Show document data', 'url': 'document/<document_id>'},
-    {'urlpattern': url(r'^document/(?P<pk>[0-9]+)/image/$', APIDocumentImageView.as_view(), name='document-image'), 'description': 'Return a base64 image of the document', 'url': 'document/<document_id>/image/?page&zoom&rotate'},
+    {'urlpattern': url(r'^document/(?P<pk>[0-9]+)/$', APIReadOnlyInstanceModelView.as_view(), name='document-detail'), 'description': 'Show document data', 'url': 'document/<document ID>'},
+    {'urlpattern': url(r'^document/(?P<pk>[0-9]+)/image/$', APIDocumentImageView.as_view(), name='document-image'), 'description': 'Return a base64 image of the document', 'url': 'document/<document_id>/image/?page=<page number>&zoom=<zoom percent>&rotate=<rotation degrees>'},
 ]
