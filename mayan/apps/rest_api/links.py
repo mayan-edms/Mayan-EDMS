12ab3854f994cbdbe47dfda8de7db00097e515db
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-07-09 14:42:07 -0400

    Add a link to the REST API documentation app

--- mayan/apps/rest_api/links.py
+++ mayan/apps/rest_api/links.py
@@ -1,3 +1,4 @@
 from django.utils.translation import ugettext_lazy as _
 
 link_api = {'text': _(u'REST API'), 'view': 'api-root', 'famfam': 'disconnect', 'icon': 'disconnect.png'}
+link_api_documentation = {'text': _(u'REST API Documentation'), 'view': 'django.swagger.base.view', 'famfam': 'book', 'icon': 'book.png'}
