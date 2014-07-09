0e8fa18b1c28620bda52b3949da5c015181197b5
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-07-09 19:41:06 -0400

    Add config folder and example.py settings file

---
+++ mayan/config/example.py
@@ -0,0 +1,16 @@
+import os
+
+from mayan.settings import *
+
+# Production database settings
+#
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': '<DB_NAME>',
+#
+#    }
+#}
+
+# Production file locations (document_storage, gpg_home, image_cache)
+# SITE_ROOT = '/local/mayan_files/
