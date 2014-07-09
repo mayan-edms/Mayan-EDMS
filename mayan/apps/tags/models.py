db15e0f85d7b7014de56b7cb25c0008ab4395649
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-07-09 19:11:47 -0400

    Add related name accesor to the tag properties model

--- mayan/apps/tags/models.py
+++ mayan/apps/tags/models.py
@@ -9,7 +9,8 @@ from .literals import COLOR_CHOICES, COLOR_CODES
 
 
 class TagProperties(models.Model):
-    tag = models.ForeignKey(Tag, verbose_name=_(u'tag'))
+    # TODO: this should be a One to One relation not a ForeignKey
+    tag = models.ForeignKey(Tag, verbose_name=_(u'tag'), related_name='properties')
     color = models.CharField(max_length=3, choices=COLOR_CHOICES, verbose_name=_(u'color'))
 
     class Meta:
