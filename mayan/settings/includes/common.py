b9bf845964bba7bfee4b45d47b9c52cd7aee2761
pwhipp <paul.whipp@gmail.com>
2014-07-10 17:01:44 +1000

    Added required postgres database configuration

--- mayan/settings/includes/common.py
+++ mayan/settings/includes/common.py
@@ -129,10 +129,12 @@ WSGI_APPLICATION = 'mayan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mayan',
+        'USER': 'mayan',
+        'PASSWORD': DBPASSWORD,
+        'HOST': '',
+        'PORT': ''}}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
