af7effb9cb6a6e98b0aaedc53dfc8d7f07e3959e
pwhipp <paul.whipp@gmail.com>
2014-07-10 17:42:07 +1000

    Added deployment information for production site

--- mayan/settings/production.py
+++ mayan/settings/production.py
@@ -6,3 +6,15 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = ['.crossculturalconsult.com']
+
+
+###################
+# DEPLOY SETTINGS #
+###################
+
+GUNICORN_BIND = "127.0.0.1:8731"
+PROCESS_USER = 'mayan'
+PROCESS_NAME = 'mayan_production'
+SITE_TITLE = 'Mayan CCCS'
+SITE_TAGLINE = None
+VIRTUALENV = 'production'
