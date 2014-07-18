f074f5431c20aba7c953db255e51e95a2665dc8e
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-07-18 18:36:12 -0400

    Create RegistrationSingleton only after db exists

--- mayan/apps/registration/__init__.py
+++ mayan/apps/registration/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
 
-from django.db import transaction, DatabaseError
+from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
+
+from south.signals import post_migrate
 
 from common import about_view, license_view
 from navigation.api import register_links
@@ -18,8 +20,7 @@ form_view = {'text': _('Registration'), 'view': 'form_view', 'famfam': 'telephon
 register_links(['form_view'], [about_view, license_view], menu_name='secondary_menu')
 register_links(['form_view', 'about_view', 'license_view'], [form_view], menu_name='secondary_menu')
 
-try:
-    with transaction.atomic():
-        RegistrationSingleton.objects.get_or_create()
-except DatabaseError:
-    pass
+
+@receiver(post_migrate, dispatch_uid='trigger_first_time', sender=RegistrationSingleton)
+def trigger_first_time(sender, **kwargs):
+    RegistrationSingleton.objects.get_or_create()
