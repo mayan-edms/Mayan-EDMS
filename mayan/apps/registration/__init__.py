2b490a08d410951ea474066d62c9ec77a28660d1
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-07-18 18:44:51 -0400

    Remove common app link importing

--- mayan/apps/registration/__init__.py
+++ mayan/apps/registration/__init__.py
@@ -4,13 +4,11 @@ from django.dispatch import receiver
 
 from south.signals import post_migrate
 
-from common import about_view, license_view
 from navigation.api import register_links
 
 from .links import form_view
 from .models import RegistrationSingleton
 
-register_links(['form_view'], [about_view, license_view], menu_name='secondary_menu')
 register_links(['form_view', 'about_view', 'license_view'], [form_view], menu_name='secondary_menu')
 
 
