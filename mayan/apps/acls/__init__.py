54f5d6cd5a3f230a37ac7b21ac15b2da7cafa359
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-07-19 00:15:11 -0400

    Create the CreatorSingleton instance after the post_migrate signal

--- mayan/apps/acls/__init__.py
+++ mayan/apps/acls/__init__.py
@@ -1,7 +1,11 @@
 from __future__ import absolute_import
 
+from django.dispatch import receiver
+
 from navigation.api import register_links, register_multi_item_links
 from project_setup.api import register_setup
+
+from south.signals import post_migrate
 
 from .classes import (AccessHolder, AccessObjectClass, ClassAccessHolder,
     AccessObject)
@@ -9,6 +13,7 @@ from .links import (acl_detail, acl_grant, acl_revoke, acl_holder_new,
     acl_setup_valid_classes, acl_class_list, acl_class_acl_list,
     acl_class_acl_detail, acl_class_new_holder_for, acl_class_grant,
     acl_class_revoke)
+from .models import CreatorSingleton
 
 register_links(AccessHolder, [acl_detail])
 register_multi_item_links(['acl_detail'], [acl_grant, acl_revoke])
@@ -22,3 +27,9 @@ register_links(ClassAccessHolder, [acl_class_acl_detail])
 
 register_links(AccessObjectClass, [acl_class_acl_list, acl_class_new_holder_for])
 register_multi_item_links(['acl_class_acl_detail'], [acl_class_grant, acl_class_revoke])
+
+
+@receiver(post_migrate, dispatch_uid='create_creator_user')
+def create_creator_user(sender, **kwargs):
+    if kwargs['app'] == 'acls':
+        CreatorSingleton.objects.get_or_create()
