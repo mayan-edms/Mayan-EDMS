e80209117a902e9229c72efb197f022373eec034
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-08-18 01:29:23 -0400

    Add production settings file and point wsgi by default to it

---
+++ mayan/settings/production.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from .local import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']  # Update this accordingly; https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+~                     
