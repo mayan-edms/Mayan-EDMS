def792790ce48789131726bd7539dbdb2a334b76
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-09-21 23:17:10 -0400

    A beautiful hack for an unknown problem: hidden imports. Hide the import for DocumentSerializer otherwise an unexplained ImportError is raised

--- mayan/apps/folders/serializers.py
+++ mayan/apps/folders/serializers.py
@@ -6,6 +6,15 @@ from .models import Folder
 
 
 class FolderSerializer(serializers.HyperlinkedModelSerializer):
+    # FIXME: Doing a: from documents.serializers import DocumentSerializer
+    # causes an unexplained ImportError, so we import it hidden until the issue
+    # is resolved
+
+    def __init__(self, *args, **kwargs):
+        from documents.serializers import DocumentSerializer
+        super(FolderSerializer, self).__init__(*args, **kwargs)
+        self.fields['documents'] = DocumentSerializer()
+
     class Meta:
         fields = ('id', 'url', 'title', 'user', 'datetime_created')
         model = Folder
--- mayan/apps/tags/serializers.py
+++ mayan/apps/tags/serializers.py
@@ -5,6 +5,15 @@ from taggit.models import Tag
 
 
 class TagSerializer(serializers.HyperlinkedModelSerializer):
+    # FIXME: Doing a: from documents.serializers import DocumentSerializer
+    # causes an unexplained ImportError, so we import it hidden until the issue
+    # is resolved
+
+    def __init__(self, *args, **kwargs):
+        from documents.serializers import DocumentSerializer
+        super(TagSerializer, self).__init__(*args, **kwargs)
+        self.fields['documents'] = DocumentSerializer()
+
     color = serializers.CharField(source='properties.get.color')
 
     class Meta:
