e4cdc69ca1c6ee7802403eb178af1c759d8b7042
Ford Guo <agile.guo@gmail.com>
2014-09-26 13:54:49 +0800

    add zh_CN mo files

--- mayan/settings/local.py
+++ mayan/settings/local.py
@@ -2,3 +2,17 @@ from __future__ import absolute_import
 from .base import *
 
 SECRET_KEY = 'rv4^n)!7-6np1crot2y4wki@-o7)^_oehl-zmaymu9$3k-6d7&'
+
+WEB_THEME_VERBOSE_LOGIN = False
+MAIN_DISABLE_HOME_VIEW = True
+COMMON_ALLOW_ANONYMOUS_ACCESS = True
+WEB_THEME_THEME = "activo"
+CONVERTER_IM_CONVERT_PATH = "/usr/local/bin/convert"
+CONVERTER_LIBREOFFICE_PATH = "/Applications/LibreOffice.app/Contents/MacOS/soffice"
+OCR_TESSERACT_PATH = "/usr/local/bin/tesseract"
+CONVERTER_IM_IDENTIFY_PATH = "/usr/local/bin/identify"
+OCR_TESSERACT_PATH = "/usr/local/bin/tesseract"
+OCR_UNPAPER_PATH = "/usr/local/bin/unpaper"
+CONVERTER_GM_PATH = "/usr/local/bin/gm"
+CONVERTER_GRAPHICS_BACKEND = "converter.backends.imagemagick.ImageMagick"
+OCR_PDFTOTEXT_PATH = "/usr/local/bin/pdftotext"
