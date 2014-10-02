2c12c617852b858281e95807a9670258204f853c
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-10-01 22:53:48 -0400

    Issue #56, remove scheduled tasks list view

--- mayan/apps/scheduler/__init__.py
+++ mayan/apps/scheduler/__init__.py
@@ -3,9 +3,6 @@ from __future__ import absolute_import
 import atexit
 import logging
 
-from project_tools.api import register_tool
-
-from .links import job_list
 from .runtime import scheduler
 import logging
 
@@ -18,4 +15,3 @@ def schedule_shutdown_on_exit():
 
 
 atexit.register(schedule_shutdown_on_exit)
-register_tool(job_list)
