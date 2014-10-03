0a20d46752b55245e6c1dbaf2efa50fc6e918d3c
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-10-03 14:37:51 -0400

    Remove unused code

--- mayan/apps/sources/widgets.py
+++ mayan/apps/sources/widgets.py
@@ -1,7 +1,5 @@
-from django import forms
 from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.utils.encoding import force_unicode
 from django.utils.html import strip_tags
 from django.utils.http import urlencode
 from django.utils.safestring import mark_safe
@@ -10,25 +8,6 @@ from django.utils.translation import ugettext_lazy as _
 from converter.literals import (DEFAULT_PAGE_NUMBER, DEFAULT_ROTATION,
                                 DEFAULT_ZOOM_LEVEL)
 from documents.settings import PREVIEW_SIZE, THUMBNAIL_SIZE
-
-
-class FamFamRadioFieldRenderer(forms.widgets.RadioFieldRenderer):
-    def render(self):
-        results = []
-        results.append(u'<ul>\n')
-        for w in self:
-            if w.choice_value:
-                famfam_template = u'<span class="famfam active famfam-%s" style="vertical-align: bottom;"></span>' % w.choice_value
-            else:
-                famfam_template = u'<span class="famfam active famfam-cross" style="vertical-align: bottom;"></span>'
-            results.append(u'<li class="undecorated_list">%s%s</li>' % (famfam_template, force_unicode(w)))
-
-        results.append(u'\n</ul>')
-        return mark_safe(u'\n'.join(results))
-
-
-class FamFamRadioSelect(forms.widgets.RadioSelect):
-    renderer = FamFamRadioFieldRenderer
 
 
 def staging_file_thumbnail(staging_file, **kwargs):
