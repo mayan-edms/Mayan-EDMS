9cbbd07d78226dd1fca8547cbd9b4cb42763053e
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-10-08 09:37:19 -0400

    PEP8 cleanups

--- mayan/apps/document_indexing/__init__.py
+++ mayan/apps/document_indexing/__init__.py
@@ -7,15 +7,17 @@ from django.utils.translation import ugettext_lazy as _
 from documents.models import Document
 from main.api import register_maintenance_links
 from metadata.models import DocumentMetadata
-from navigation.api import (register_top_menu, register_sidebar_template,
-    register_links)
+from navigation.api import (register_links, register_sidebar_template,
+                            register_top_menu)
 from project_setup.api import register_setup
 
 from .api import update_indexes, delete_indexes
-from .links import (index_setup, index_setup_list, index_setup_create, index_setup_edit,
-    index_setup_delete, index_setup_view, index_setup_document_types, template_node_create,
-    template_node_edit, template_node_delete, index_parent, document_index_list,
-    rebuild_index_instances, document_index_main_menu_link)
+from .links import (document_index_list, document_index_main_menu_link,
+                    index_parent, index_setup, index_setup_create,
+                    index_setup_document_types, index_setup_delete,
+                    index_setup_edit, index_setup_list, index_setup_view,
+                    rebuild_index_instances, template_node_create,
+                    template_node_delete, template_node_edit)
 from .models import Index, IndexTemplateNode, IndexInstanceNode
 
 
@@ -40,20 +42,15 @@ def document_metadata_index_post_delete(sender, **kwargs):
     update_indexes(kwargs['instance'].document)
 
 
-register_top_menu('indexes', document_index_main_menu_link)
-
 register_maintenance_links([rebuild_index_instances], namespace='document_indexing', title=_(u'Indexes'))
 
-register_sidebar_template(['indexing:index_instance_list'], 'indexing_help.html')
-
-register_links(IndexInstanceNode, [index_parent])
-
 register_links(Document, [document_index_list], menu_name='form_header')
+register_links([Index, 'indexing:index_setup_list', 'indexing:index_setup_create', 'indexing:template_node_edit', 'indexing:template_node_delete'], [index_setup_list, index_setup_create], menu_name='secondary_menu')
+register_links(Index, [index_setup_edit, index_setup_delete, index_setup_view, index_setup_document_types])
+register_links(IndexInstanceNode, [index_parent])
+register_links(IndexTemplateNode, [template_node_create, template_node_edit, template_node_delete])
 
+register_sidebar_template(['indexing:index_instance_list'], 'indexing_help.html')
 register_setup(index_setup)
 
-register_links([Index, 'indexing:index_setup_list', 'indexing:index_setup_create', 'indexing:template_node_edit', 'indexing:template_node_delete'], [index_setup_list, index_setup_create], menu_name='secondary_menu')
-
-register_links(Index, [index_setup_edit, index_setup_delete, index_setup_view, index_setup_document_types])
-
-register_links(IndexTemplateNode, [template_node_create, template_node_edit, template_node_delete])
+register_top_menu('indexes', document_index_main_menu_link)
