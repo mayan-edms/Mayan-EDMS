d253c8137dc632fe58b174c16b750a568894820d
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-10-08 10:16:06 -0400

    Issue #39, Fix capitalization of common app links

--- mayan/apps/common/links.py
+++ mayan/apps/common/links.py
@@ -7,9 +7,9 @@ def has_usable_password(context):
     return context['request'].user.has_usable_password
 
 
-link_password_change = {'text': _(u'change password'), 'view': 'common:password_change_view', 'famfam': 'computer_key', 'condition': has_usable_password}
-link_current_user_details = {'text': _(u'user details'), 'view': 'common:current_user_details', 'famfam': 'vcard'}
-link_current_user_edit = {'text': _(u'edit details'), 'view': 'common:current_user_edit', 'famfam': 'vcard_edit'}
+link_password_change = {'text': _(u'Change password'), 'view': 'common:password_change_view', 'famfam': 'computer_key', 'condition': has_usable_password}
+link_current_user_details = {'text': _(u'User details'), 'view': 'common:current_user_details', 'famfam': 'vcard'}
+link_current_user_edit = {'text': _(u'Edit details'), 'view': 'common:current_user_edit', 'famfam': 'vcard_edit'}
 
-link_about = {'text': _(u'about'), 'view': 'common:about_view', 'famfam': 'information'}
-link_license = {'text': _(u'license'), 'view': 'common:license_view', 'famfam': 'script'}
+link_about = {'text': _(u'About'), 'view': 'common:about_view', 'famfam': 'information'}
+link_license = {'text': _(u'License'), 'view': 'common:license_view', 'famfam': 'script'}
