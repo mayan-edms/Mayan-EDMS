d1c03f0417b55c8b5724acc69e6308f5e87646fc
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-10-16 09:46:40 -0400

    Bump version

--- mayan/__init__.py
+++ mayan/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'Mayan EDMS'
-__version__ = '1.0.0'
-__build__ = 0x010000
+__version__ = '1.1.0'
+__build__ = 0x010100
 __author__ = 'Roberto Rosario'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2011 Roberto Rosario'
