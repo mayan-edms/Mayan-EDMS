5557cc40cd1d96137052444f69b2bbfc7fc288bb
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-10-17 22:48:58 -0400

    Update model name

--- mayan/apps/acls/literals.py
+++ mayan/apps/acls/literals.py
@@ -6,7 +6,7 @@ CONTENT_TYPE_ICON_MAP = {
     'documents.document': 'page',
     'permissions.role': 'medal_gold_1',
     'folders.folder': 'folder',
-    'taggit.tag': 'tag_blue',
+    'tags.tag': 'tag_blue',
     'linking.smartlink': 'page_link',
     'common.anonymoususersingleton': 'user',
     'acls.creatorsingleton': 'user',
