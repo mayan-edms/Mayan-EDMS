ba9ab75f5d151afd7ade7ea7251201b8f7fcac26
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-10-23 12:41:59 -0400

    Don't use hyperlinked serializer for folder, makes it easier to add folder by just specifying user id instead of user URL

--- mayan/apps/folders/serializers.py
+++ mayan/apps/folders/serializers.py
@@ -5,9 +5,9 @@ from rest_framework import serializers
 from .models import Folder
 
 
-class FolderSerializer(serializers.HyperlinkedModelSerializer):
+class FolderSerializer(serializers.ModelSerializer):
     documents = serializers.HyperlinkedIdentityField(view_name='folder-document-list')
 
     class Meta:
-        fields = ('id', 'url', 'title', 'user', 'datetime_created', 'documents')
+        fields = ('id', 'title', 'user', 'datetime_created', 'documents')
         model = Folder
