6bca65088f8f3f25676d68ef2df109fe5581ebc8
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-10-25 02:30:11 -0400

    Remove static context processor

--- mayan/settings/base.py
+++ mayan/settings/base.py
@@ -193,7 +193,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
-    'django.core.context_processors.static',
     'django.core.context_processors.request',
     'django.contrib.messages.context_processors.messages',
 )
