df74239e85f1fce8005347419a12c88a7102ece1
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-10-27 04:46:31 -0400

    Move language choices to the literals module

--- mayan/apps/documents/literals.py
+++ mayan/apps/documents/literals.py
@@ -1,3 +1,5 @@
+import pycountry
+
 PICTURE_ERROR_SMALL = u'picture_error.png'
 PICTURE_ERROR_MEDIUM = u'1297211435_error.png'
 PICTURE_UNKNOWN_SMALL = u'1299549572_unknown2.png'
@@ -8,3 +10,6 @@ VERSION_UPDATE_MINOR = u'minor'
 VERSION_UPDATE_MICRO = u'micro'
 
 DEFAULT_ZIP_FILENAME = u'document_bundle.zip'
+
+LANGUAGE_CHOICES = [(i.bibliographic, i.name) for i in list(pycountry.languages)]
+
