165d01610b0287e9b60b987eeea91c6b074092a8
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-10-27 04:47:42 -0400

    Improve User model serializer

--- mayan/apps/user_management/serializers.py
+++ mayan/apps/user_management/serializers.py
@@ -5,7 +5,13 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('id', 'url', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'is_superuser', 'last_login', 'date_joined')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'is_superuser', 'last_login', 'date_joined')
         model = User
+        write_only_fields = ('password',)
+
+    def restore_object(self, attrs, instance=None):
+        user = super(UserSerializer, self).restore_object(attrs, instance)
+        user.set_password(attrs['password'])
+        return user
