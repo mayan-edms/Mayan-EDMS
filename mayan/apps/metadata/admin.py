879f90547c4cb4dac089fcc386e8a84e72764b8c
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-10-30 02:26:18 -0400

    Update metadata admin

--- mayan/apps/metadata/admin.py
+++ mayan/apps/metadata/admin.py
@@ -2,23 +2,11 @@ from __future__ import absolute_import
 
 from django.contrib import admin
 
-from .models import DocumentMetadata, DocumentTypeDefaults, MetadataType
-
-
-class DocumentTypeDefaultsAdmin(admin.ModelAdmin):
-    filter_horizontal = ('default_metadata',)
+from .models import DocumentMetadata, MetadataType
 
 
 class MetadataTypeAdmin(admin.ModelAdmin):
     list_display = ('name', 'title', 'default', 'lookup')
 
 
-class DocumentMetadataInline(admin.StackedInline):
-    model = DocumentMetadata
-    extra = 0
-    classes = ('collapse-open',)
-    allow_add = False
-
-
-admin.site.register(DocumentTypeDefaults, DocumentTypeDefaultsAdmin)
 admin.site.register(MetadataType, MetadataTypeAdmin)
