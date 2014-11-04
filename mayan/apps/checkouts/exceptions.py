57eb4df1ba69924af61806992a0add4b070e0e2a
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-11-04 17:37:22 -0400

    Add default message to the exception

--- mayan/apps/checkouts/exceptions.py
+++ mayan/apps/checkouts/exceptions.py
@@ -1,3 +1,6 @@
+from django.utils.translation import ugettext
+
+
 class DocumentNotCheckedOut(Exception):
     """
     Raised when trying to checkin a document that is not checkedout
@@ -9,4 +12,5 @@ class DocumentAlreadyCheckedOut(Exception):
     """
     Raised when trying to checkout an already checkedout document
     """
-    pass
+    def __unicode__(self):
+        return ugettext('Document already checked out.')
