9b895e084416acaa12c5780beea0ae8eb89018a4
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-11-11 04:13:02 -0400

    Hide DocumentSerializer import

--- mayan/apps/checkouts/serializers.py
+++ mayan/apps/checkouts/serializers.py
@@ -2,32 +2,23 @@ from __future__ import absolute_import
 
 from rest_framework import serializers
 
-from documents.serializers import DocumentSerializer
-
 from .models import DocumentCheckout
 
 
-#class DocumentCheckoutSerializer(serializers.Serializer):
-#    document_id = serializers.IntegerField()
-
-
 class DocumentCheckoutSerializer(serializers.ModelSerializer):
-    document = DocumentSerializer()
-    #document = serializers.IntegerField()
+    def __init__(self, *args, **kwargs):
+        # Hide this import otherwise strange circular import error occur
+        from documents.serializers import DocumentSerializer
+
+        super(DocumentCheckoutSerializer, self).__init__(*args, **kwargs)
+        self.fields['document'] = DocumentSerializer()
 
     class Meta:
         model = DocumentCheckout
-        #fields = ('id', 'document', 'checkout_datetime', 'expiration_datetime', 'block_new_version')
         read_only_fields = ('user_content_type', 'user_object_id')
 
 
 class NewDocumentCheckoutSerializer(serializers.Serializer):
-    #document = DocumentSerializer()
     document = serializers.IntegerField()
     expiration_datetime = serializers.DateTimeField()
     block_new_version = serializers.BooleanField()
-
-    #class Meta:
-    #    model = DocumentCheckout
-    #    fields = ('id', 'document', 'checkout_datetime', 'expiration_datetime', 'block_new_version')
-    #    #read_only_fields = ('user_content_type', 'user_object_id')
