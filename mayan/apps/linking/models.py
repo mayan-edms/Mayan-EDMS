0916bbacb0733e791e2c1d41d6ef9023040d6e1f
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2014-11-11 03:01:17 -0400

    Expand the size of the foreign_document_data field

--- mayan/apps/linking/models.py
+++ mayan/apps/linking/models.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext
 
 from documents.models import DocumentType
 
@@ -31,9 +32,9 @@ class SmartLink(models.Model):
 class SmartLinkCondition(models.Model):
     smart_link = models.ForeignKey(SmartLink, verbose_name=_(u'Smart link'))
     inclusion = models.CharField(default=INCLUSION_AND, max_length=16, choices=INCLUSION_CHOICES, help_text=_(u'The inclusion is ignored for the first item.'))
-    foreign_document_data = models.CharField(max_length=32, verbose_name=_(u'Foreign document attribute'), help_text=_(u'This represents the metadata of all other documents.'))
+    foreign_document_data = models.CharField(max_length=128, verbose_name=_(u'Foreign document attribute'), help_text=_(u'This represents the metadata of all other documents.'))
     operator = models.CharField(max_length=16, choices=OPERATOR_CHOICES)
-    expression = models.TextField(verbose_name=_(u'Expression'), help_text=_(u'This expression will be evaluated against the current selected document.'))
+    expression = models.TextField(verbose_name=_(u'Expression'), help_text=ugettext(u'This expression will be evaluated against the current document.'))
     negated = models.BooleanField(default=False, verbose_name=_(u'Negated'), help_text=_(u'Inverts the logic of the operator.'))
     enabled = models.BooleanField(default=True, verbose_name=_(u'Enabled'))
 
