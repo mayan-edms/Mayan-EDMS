9c5196ae2b0a8a2180c89f78263a0f8a0ae4bf26
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-01-03 23:38:54 -0400

    Don't pass any argument to task_delete_empty_index_nodes, it requires none now

--- mayan/apps/document_indexing/__init__.py
+++ mayan/apps/document_indexing/__init__.py
@@ -35,7 +35,7 @@ def document_metadata_index_update(sender, **kwargs):
 @receiver(post_delete, dispatch_uid='document_metadata_index_post_delete', sender=DocumentMetadata)
 def document_metadata_index_post_delete(sender, **kwargs):
     task_index_document.apply_async(kwargs=dict(document_id=kwargs['instance'].document.pk), queue='indexing')
-    task_delete_empty_index_nodes.apply_async(kwargs=dict(document_id=kwargs['instance'].document.pk), queue='indexing')
+    task_delete_empty_index_nodes.apply_async(queue='indexing')
 
 
 register_maintenance_links([rebuild_index_instances], namespace='document_indexing', title=_(u'Indexes'))
