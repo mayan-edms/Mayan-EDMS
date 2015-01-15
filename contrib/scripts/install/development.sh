007ac0f9780b31cbb231bbbdbd18196f62fc337b
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-01-15 03:48:59 -0400

    Add libreoffice to the Vagrant provissioning script

--- contrib/scripts/install/development.sh
+++ contrib/scripts/install/development.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install git-core python-virtualenv gcc python-dev libjpeg-dev libpng-dev libtiff-dev tesseract-ocr poppler-utils unpaper redis-server
+sudo apt-get -y install git-core python-virtualenv gcc python-dev libjpeg-dev libpng-dev libtiff-dev tesseract-ocr poppler-utils unpaper redis-server libreoffice
 git clone /mayan-edms-repository/ /home/vagrant/mayan-edms
 cd /home/vagrant/mayan-edms
 git checkout development
