070e306503a88861e4616f2917dc13b54e6b748b
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-01-17 02:29:48 -0400

    Fix double menu entry at linking/setup/smart_link/condition/1/edit/. Issue #150

--- mayan/apps/document_indexing/__init__.py
+++ mayan/apps/document_indexing/__init__.py
@@ -39,7 +39,7 @@ def document_metadata_index_post_delete(sender, **kwargs):
 register_maintenance_links([rebuild_index_instances], namespace='document_indexing', title=_(u'Indexes'))
 
 register_links(Document, [document_index_list], menu_name='form_header')
-register_links([Index, 'indexing:index_setup_list', 'indexing:index_setup_create', 'indexing:template_node_edit', 'indexing:template_node_delete'], [index_setup_list, index_setup_create], menu_name='secondary_menu')
+register_links([Index, 'indexing:index_setup_list', 'indexing:index_setup_create'], [index_setup_list, index_setup_create], menu_name='secondary_menu')
 register_links(Index, [index_setup_edit, index_setup_view, index_setup_document_types, index_setup_delete])
 register_links(IndexInstanceNode, [index_parent])
 register_links(IndexTemplateNode, [template_node_create, template_node_edit, template_node_delete])
