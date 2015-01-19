a46ac1e9ef48528156995bc1325b725a649b6a37
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-01-19 00:42:33 -0400

    Allow setting the GPG binary path

--- mayan/apps/django_gpg/runtime.py
+++ mayan/apps/django_gpg/runtime.py
@@ -1,14 +1,4 @@
-from __future__ import absolute_import
-
-import sys
-
 from .api import GPG
-from .settings import KEYSERVERS, GPG_HOME
+from .settings import GPG_HOME, GPG_PATH, KEYSERVERS
 
-try:
-    gpg = GPG(home=GPG_HOME, keyservers=KEYSERVERS)
-except Exception as exception:
-    gpg = GPG(keyservers=KEYSERVERS)
-    # TODO: Maybe raise a standard exception to signify configuration error
-    sys.stderr.write(u'ERROR: GPG initialization error: %s\n' % exception)
-    sys.stderr.write(u'INFO: Initializating GPG with system default home\n')
+gpg = GPG(binary_path=GPG_PATH, home=GPG_HOME, keyservers=KEYSERVERS)
