ac72537a60efcb6148ffbdfaa9e2d1389d4daa9f
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-04-06 01:53:56 -0400

    Commit uncommitted change to consolidate variable_tags with project_tags

--- mayan/apps/common/templatetags/project_tags.py
+++ mayan/apps/common/templatetags/project_tags.py
@@ -1,9 +1,17 @@
 from django.conf import settings
 from django.template import Library
 
+import mayan
+
 register = Library()
 
 
 @register.simple_tag
 def project_name():
     return settings.PROJECT_TITLE
+
+
+@register.simple_tag
+def project_version():
+    return mayan.__version__
+
