b49617d3e75ded88cdd52676bdf1f50813c36fbe
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-06-04 21:05:02 -0400

    Add "dangerous" tag to the document comment delete link

--- mayan/apps/document_comments/links.py
+++ mayan/apps/document_comments/links.py
@@ -10,6 +10,6 @@ from .permissions import (
 )
 
 link_comment_add = Link(permissions=[PERMISSION_COMMENT_CREATE], text=_('Add comment'), view='comments:comment_add', args='object.pk')
-link_comment_delete = Link(permissions=[PERMISSION_COMMENT_DELETE], text=_('Delete'), view='comments:comment_delete', args='object.pk')
-link_comment_multiple_delete = Link(permissions=[PERMISSION_COMMENT_DELETE], text=_('Delete'), view='comments:comment_multiple_delete', args='object.pk')
+link_comment_delete = Link(permissions=[PERMISSION_COMMENT_DELETE], tags='dangerous', text=_('Delete'), view='comments:comment_delete', args='object.pk')
+link_comment_multiple_delete = Link(permissions=[PERMISSION_COMMENT_DELETE], tags='dangerous', text=_('Delete'), view='comments:comment_multiple_delete', args='object.pk')
 link_comments_for_document = Link(permissions=[PERMISSION_COMMENT_VIEW], text=_('Comments'), view='comments:comments_for_document', args='object.pk')
