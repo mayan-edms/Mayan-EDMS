eddce6611aadc11fe369ced052911857cfeb8005
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-06-30 02:43:15 -0400

    Remove permission field from list display columns

--- mayan/apps/acls/admin.py
+++ mayan/apps/acls/admin.py
@@ -6,7 +6,7 @@ from .models import AccessControlList
 
 
 class AccessControlListAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'role', 'permission', 'content_object')
+    list_display = ('pk', 'role', 'content_object')
     list_display_links = ('pk',)
     related_lookup_fields = {
         'generic': (('content_type', 'object_id'),),
