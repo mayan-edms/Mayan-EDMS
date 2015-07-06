dcf3a77c66f40f0bda9557048568ce10ce2d5e2d
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-07-06 00:22:09 -0400

    Don't try to set locale preferences for new users or existing users that have not set any preferences.

--- mayan/apps/common/handlers.py
+++ mayan/apps/common/handlers.py
@@ -9,14 +9,17 @@ from .models import UserLocaleProfile
 def user_locale_profile_session_config(sender, request, user, **kwargs):
     user_locale_profile, created = UserLocaleProfile.objects.get_or_create(user=user)
 
-    timezone.activate(user_locale_profile.timezone)
-    translation.activate(user_locale_profile.language)
+    if not created and user_locale_profile.timezone and user_locale_profile.language:
+        # Don't try to set locale preferences for new users or existing users
+        # that have not set any preferences
+        timezone.activate(user_locale_profile.timezone)
+        translation.activate(user_locale_profile.language)
 
-    if hasattr(request, 'session'):
-        request.session[translation.LANGUAGE_SESSION_KEY] = user_locale_profile.language
-        request.session['django_timezone'] = user_locale_profile.timezone
-    else:
-        request.set_cookie(settings.LANGUAGE_COOKIE_NAME, user_locale_profile.language)
+        if hasattr(request, 'session'):
+            request.session[translation.LANGUAGE_SESSION_KEY] = user_locale_profile.language
+            request.session['django_timezone'] = user_locale_profile.timezone
+        else:
+            request.set_cookie(settings.LANGUAGE_COOKIE_NAME, user_locale_profile.language)
 
 
 def user_locale_profile_create(sender, instance, created, **kwargs):
