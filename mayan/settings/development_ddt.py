3c486e14a124322320ca61807e2376d843771747
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-07-06 00:13:17 -0400

    DRY, make development w/ DDT settings inhering from normal development settings file.

--- mayan/settings/development_ddt.py
+++ mayan/settings/development_ddt.py
@@ -1,20 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
-from . import *  # NOQA
-
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-ALLOWED_HOSTS = ['*']
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader'
-)
+from .development import *  # NOQA
 
 INSTALLED_APPS += (
-    'rosetta',
-    'django_extensions',
     'debug_toolbar'
 )
 
