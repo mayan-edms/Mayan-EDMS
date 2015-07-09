66b3424fbe863f963faa6a32966cbbd1d3b52298
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-07-09 01:24:28 -0400

    Simplify smart settings views.

--- mayan/apps/smart_settings/views.py
+++ mayan/apps/smart_settings/views.py
@@ -10,34 +10,22 @@ from .classes import Namespace
 class NamespaceListView(SimpleView):
     template_name = 'appearance/generic_list.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(NamespaceListView, self).get_context_data(**kwargs)
-
-        context.update(
-            {
-                'hide_link': True,
-                'object_list': Namespace.get_all(),
-                'title': _('Setting namespaces'),
-            }
-        )
-
-        return context
+    def get_extra_context(self):
+        return {
+            'hide_link': True,
+            'object_list': Namespace.get_all(),
+            'title': _('Setting namespaces'),
+        }
 
 
 class NamespaceDetailView(SimpleView):
     template_name = 'appearance/generic_list.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(NamespaceDetailView, self).get_context_data(**kwargs)
-
+    def get_extra_context(self):
         namespace = Namespace.get(self.kwargs['namespace_name'])
 
-        context.update(
-            {
-                'hide_object': True,
-                'object_list': namespace.settings,
-                'title': _('Settings in namespace: %s') % namespace,
-            }
-        )
-
-        return context
+        return {
+            'hide_object': True,
+            'object_list': namespace.settings,
+            'title': _('Settings in namespace: %s') % namespace,
+        }
