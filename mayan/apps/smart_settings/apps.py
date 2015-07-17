2b114652493f2bafb196b613847e865300ad5a49
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-07-17 01:15:00 -0400

    Import all apps' settings file to initialize them.

--- mayan/apps/smart_settings/apps.py
+++ mayan/apps/smart_settings/apps.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
 
+from importlib import import_module
+import logging
+
+from django.apps import apps
 from django.utils.translation import ugettext_lazy as _
 
 from common import MayanAppConfig, menu_setup, menu_object
@@ -10,6 +14,8 @@ from navigation import SourceColumn
 from .classes import Namespace, Setting
 from .links import link_namespace_detail, link_namespace_list
 from .widgets import setting_widget
+
+logger = logging.getLogger(__name__)
 
 
 class SmartSettingsApp(MayanAppConfig):
@@ -28,3 +34,12 @@ class SmartSettingsApp(MayanAppConfig):
 
         menu_object.bind_links(links=(link_namespace_detail,), sources=(Namespace,))
         menu_setup.bind_links(links=(link_namespace_list,))
+
+        for app in apps.get_app_configs():
+            try:
+                import_module('{}.settings'.format(app.name))
+            except ImportError:
+                logger.debug('App %s has not settings.py file', app.name)
+            else:
+                logger.debug('Imported settings.py file for app %s', app.name)
+
