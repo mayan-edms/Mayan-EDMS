016231a0f5a413ca7777e9e46f262b7c70e46d9b
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-08-15 01:26:11 -0400

    Use Python function to get OS default temp directory.

--- mayan/apps/common/settings.py
+++ mayan/apps/common/settings.py
@@ -1,15 +1,17 @@
 from __future__ import unicode_literals
 
+import tempfile
+
 from django.utils.translation import ugettext_lazy as _
 
 from smart_settings import Namespace
 
 namespace = Namespace(name='common', label=_('Common'))
 setting_temporary_directory = namespace.add_setting(
-    global_name='COMMON_TEMPORARY_DIRECTORY', default='/tmp',
+    global_name='COMMON_TEMPORARY_DIRECTORY', default=tempfile.gettempdir(),
     help_text=_('Temporary directory used site wide to store thumbnails, previews and temporary files.  If none is specified, one will be created using tempfile.mkdtemp()'),
     is_path=True
-)  # TODO: get os default temp directory
+)
 setting_shared_storage = namespace.add_setting(
     global_name='COMMON_SHARED_STORAGE',
     default='storage.backends.filebasedstorage.FileBasedStorage',
