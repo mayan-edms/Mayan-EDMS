e79cd4a2e63615da28e8e3d42ed1c43855f9fc80
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-08-24 17:43:06 -0400

    Remove unused imports.

--- mayan/apps/document_comments/views.py
+++ mayan/apps/document_comments/views.py
@@ -1,12 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.conf import settings
-from django.contrib import messages
 from django.core.exceptions import PermissionDenied
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext
+from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
 
 from acls.models import AccessControlList
