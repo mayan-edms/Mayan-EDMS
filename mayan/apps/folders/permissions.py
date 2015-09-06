dd59a6af72314e781df881dc65afb20a7f34665d
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-09-06 00:40:52 -0400

    Simplify folder app's permission labels.

--- mayan/apps/folders/permissions.py
+++ mayan/apps/folders/permissions.py
@@ -7,20 +7,20 @@ from permissions import PermissionNamespace
 namespace = PermissionNamespace('folders', _('Folders'))
 
 permission_folder_create = namespace.add_permission(
-    name='folder_create', label=_('Create new folders')
+    name='folder_create', label=_('Create folders')
 )
 permission_folder_edit = namespace.add_permission(
-    name='folder_edit', label=_('Edit new folders')
+    name='folder_edit', label=_('Edit folders')
 )
 permission_folder_delete = namespace.add_permission(
-    name='folder_delete', label=_('Delete new folders')
+    name='folder_delete', label=_('Delete folders')
 )
 permission_folder_remove_document = namespace.add_permission(
     name='folder_remove_document', label=_('Remove documents from folders')
 )
 permission_folder_view = namespace.add_permission(
-    name='folder_view', label=_('View existing folders')
+    name='folder_view', label=_('View folders')
 )
 permission_folder_add_document = namespace.add_permission(
-    name='folder_add_document', label=_('Add documents to existing folders')
+    name='folder_add_document', label=_('Add documents to folders')
 )
