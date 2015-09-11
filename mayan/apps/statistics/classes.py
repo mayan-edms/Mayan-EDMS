7687618ea62837f6f813987037c8ff2e5975d151
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-09-11 08:28:16 -0400

    Invert series palette.

--- mayan/apps/statistics/classes.py
+++ mayan/apps/statistics/classes.py
@@ -96,20 +96,20 @@ class CharJSLine(ChartRenderer):
 
     dataset_palette = (
         {
-            'fillColor': "rgba(220,220,220,0.2)",
-            'strokeColor': "rgba(220,220,220,1)",
-            'pointColor': "rgba(220,220,220,1)",
-            'pointStrokeColor': "#fff",
-            'pointHighlightFill': "#fff",
-            'pointHighlightStroke': "rgba(220,220,220,1)",
-        },
-        {
             'fillColor': "rgba(151,187,205,0.2)",
             'strokeColor': "rgba(151,187,205,1)",
             'pointColor': "rgba(151,187,205,1)",
             'pointStrokeColor': "#fff",
             'pointHighlightFill': "#fff",
             'pointHighlightStroke': "rgba(151,187,205,1)",
+        },
+        {
+            'fillColor': "rgba(220,220,220,0.2)",
+            'strokeColor': "rgba(220,220,220,1)",
+            'pointColor': "rgba(220,220,220,1)",
+            'pointStrokeColor': "#fff",
+            'pointHighlightFill': "#fff",
+            'pointHighlightStroke': "rgba(220,220,220,1)",
         }
     )
 
