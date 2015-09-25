6edafc814fa7a61c60020f0c476cf1791085563f
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-09-24 23:28:36 -0400

    Add testing setting files for gitlab-ci. Improve gitlab-ci config file.

---
+++ mayan/settings/testing/gitlab-ci/db_postgres.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from ..base import *  # NOQA
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mayan_edms',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+    }
+}
