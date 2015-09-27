22a29eede3fb367dd69f7122eead692988590a7a
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-09-26 23:52:10 -0400

    Use explicit literals for the time delta units used for retention policies.

--- mayan/apps/common/literals.py
+++ mayan/apps/common/literals.py
@@ -4,9 +4,14 @@ from django.utils.translation import ugettext_lazy as _
 
 
 DELETE_STALE_UPLOADS_INTERVAL = 60 * 10  # 10 minutes
+
+TIME_DELTA_UNIT_DAYS = 'days'
+TIME_DELTA_UNIT_HOURS = 'hours'
+TIME_DELTA_UNIT_MINUTES = 'minutes'
+
 TIME_DELTA_UNIT_CHOICES = (
-    ('days', _('Days')),
-    ('hours', _('Hours')),
-    ('minutes', _('Minutes')),
+    (TIME_DELTA_UNIT_DAYS, _('Days')),
+    (TIME_DELTA_UNIT_HOURS, _('Hours')),
+    (TIME_DELTA_UNIT_MINUTES, _('Minutes')),
 )
 UPLOAD_EXPIRATION_INTERVAL = 60 * 60 * 24 * 7  # 7 days
