1556fa92e4acbe31499d37bc4b289d4e5b97155a
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-10-07 19:32:00 -0400

    Add test for the about view.

---
+++ mayan/apps/common/tests/test_views.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import User
+from django.core.files import File
+from django.core.urlresolvers import reverse
+from django.test.client import Client
+from django.test import TestCase
+
+from documents.tests.literals import (
+    TEST_ADMIN_PASSWORD, TEST_ADMIN_USERNAME, TEST_ADMIN_EMAIL,
+    TEST_SMALL_DOCUMENT_PATH, TEST_DOCUMENT_TYPE
+)
+
+
+class DocumentsViewsFunctionalTestCase(TestCase):
+    def setUp(self):
+        self.admin_user = User.objects.create_superuser(
+            username=TEST_ADMIN_USERNAME, email=TEST_ADMIN_EMAIL,
+            password=TEST_ADMIN_PASSWORD
+        )
+        self.client = Client()
+        # Login the admin user
+        logged_in = self.client.login(
+            username=TEST_ADMIN_USERNAME, password=TEST_ADMIN_PASSWORD
+        )
+        self.assertTrue(logged_in)
+        self.assertTrue(self.admin_user.is_authenticated())
+
+    def tearDown(self):
+        self.admin_user.delete()
+
+    def test_about_view(self):
+        response = self.client.get(reverse('common:about_view'))
+        self.assertContains(response, text='About', status_code=200)
