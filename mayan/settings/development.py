8a566d5b91d0470d12e5f2cf2810bcb7eafeff41
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2015-10-08 02:57:02 -0400

    Remove sample debug config from development settings, it is done automatically now.

--- mayan/settings/development.py
+++ mayan/settings/development.py
@@ -19,39 +19,4 @@ INSTALLED_APPS += (
 
 WSGI_AUTO_RELOAD = True
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(name)s %(process)d %(thread)d %(message)s'
-        },
-        'intermediate': {
-            'format': '%(name)s <%(process)d> [%(levelname)s] "%(funcName)s() %(message)s"'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'intermediate'
-        }
-    },
-    'loggers': {
-        'documents': {
-            'handlers': ['console'],
-            'propagate': True,
-            'level': 'DEBUG',
-        },
-        'common': {
-            'handlers': ['console'],
-            'propagate': True,
-            'level': 'DEBUG',
-        },
-    }
-}
-
 CELERY_EAGER_PROPAGATES_EXCEPTIONS = CELERY_ALWAYS_EAGER
