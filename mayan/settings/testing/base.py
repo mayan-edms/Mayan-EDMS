2f298df1a04dead790563dd5a17d5d4b1a7b383a
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2016-01-17 00:23:51 -0400

    Testing base settings now import from settings/local.py first.

--- mayan/settings/testing/base.py
+++ mayan/settings/testing/base.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 
-from ..base import *  # NOQA
+from .. import *  # NOQA
 
 INSTALLED_APPS += ('test_without_migrations',)
 TEMPLATE_LOADERS = (
