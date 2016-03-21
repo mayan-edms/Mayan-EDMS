ccd6e8c88c9a15fba4f0c692d43a9437d9260e36
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2016-03-21 18:59:22 -0400

    Add document mailing view tests.

--- mayan/apps/mailer/tests/test_views.py
+++ mayan/apps/mailer/tests/test_views.py
@@ -1,13 +1,7 @@
-# -*- coding: utf-8 -*-
-
 from __future__ import unicode_literals
 
-from django.contrib.contenttypes.models import ContentType
 from django.core import mail
-from django.test import override_settings
-from django.utils.six import BytesIO
 
-from common.tests.test_views import GenericViewTestCase
 from documents.tests.test_views import GenericDocumentViewTestCase
 from user_management.tests.literals import (
     TEST_USER_PASSWORD, TEST_USER_USERNAME
@@ -45,6 +39,38 @@ class MailerViewsTestCase(GenericDocumentViewTestCase):
         )
         response = self.post(
             'mailer:send_document_link', args=(self.document.pk,),
+            data={'email': TEST_EMAIL_ADDRESS},
+            follow=True
+        )
+
+        self.assertContains(
+            response, 'queued', status_code=200
+        )
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].to, [TEST_EMAIL_ADDRESS])
+
+    def test_mail_document_view_no_permissions(self):
+        self.login(
+            username=TEST_USER_USERNAME, password=TEST_USER_PASSWORD
+        )
+
+        response = self.post(
+            'mailer:send_document', args=(self.document.pk,),
+            data={'email': TEST_EMAIL_ADDRESS},
+        )
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_mail_document_view_with_permission(self):
+        self.login(
+            username=TEST_USER_USERNAME, password=TEST_USER_PASSWORD
+        )
+
+        self.role.permissions.add(
+            permission_mailing_send_document.stored_permission
+        )
+        response = self.post(
+            'mailer:send_document', args=(self.document.pk,),
             data={'email': TEST_EMAIL_ADDRESS},
             follow=True
         )
