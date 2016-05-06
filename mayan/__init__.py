94b968963ee7d9bd009fad571839667cae479bed
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2016-05-05 23:44:18 -0400

    Bump version to 2.1rc2

--- mayan/__init__.py
+++ mayan/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 __title__ = 'Mayan EDMS'
-__version__ = '2.1rc1'
+__version__ = '2.1rc2'
 __build__ = 0x020100
 __author__ = 'Roberto Rosario'
 __author_email__ = 'roberto.rosario@mayan-edms.com'
