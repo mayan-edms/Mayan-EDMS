7a746653dad87b526feed15a38f0728e1a3a0f35
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2016-05-13 21:16:05 -0400

    Bump version to 2.1

--- mayan/__init__.py
+++ mayan/__init__.py
@@ -1,10 +1,10 @@
 from __future__ import unicode_literals
 
 __title__ = 'Mayan EDMS'
-__version__ = '2.1rc2'
+__version__ = '2.1'
 __build__ = 0x020100
 __author__ = 'Roberto Rosario'
 __author_email__ = 'roberto.rosario@mayan-edms.com'
 __description__ = 'Free Open Source Electronic Document Management System'
 __license__ = 'Apache 2.0'
-__copyright__ = 'Copyright 2011-2015 Roberto Rosario'
+__copyright__ = 'Copyright 2011-2016 Roberto Rosario'
