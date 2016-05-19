fba20b0a9101f67809d4d6b24a2884bd9d7a2244
Baptiste GAILLET <baptiste.gaillet@cstb.fr>
2016-05-19 16:07:09 +0200

    Fix postgresql organization creation id error after the first migration

--- mayan/apps/organizations/migrations/0002_add_data_default_organization.py
+++ mayan/apps/organizations/migrations/0002_add_data_default_organization.py
@@ -8,7 +8,7 @@ def default_data(apps, schema_editor):
     # We can't import the Organization model directly as it may be a newer
     # version than this migration expects. We use the historical version.
     Organization = apps.get_model("organizations", "Organization")
-    default_organization = Organization(id=1, label='Default')
+    default_organization = Organization(label='Default')
     default_organization.save()
 
 
