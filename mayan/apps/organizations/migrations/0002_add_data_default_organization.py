2ca3a67c9c5f8c54a4c3369d0b459cfb340a9e0e
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2016-05-25 02:44:24 -0400

    Don't make the default organization name hardcoded.

--- mayan/apps/organizations/migrations/0002_add_data_default_organization.py
+++ mayan/apps/organizations/migrations/0002_add_data_default_organization.py
@@ -3,12 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
+from ..literals import DEFAULT_ORGANIZATION_LABEL
+
 
 def default_data(apps, schema_editor):
     # We can't import the Organization model directly as it may be a newer
     # version than this migration expects. We use the historical version.
     Organization = apps.get_model("organizations", "Organization")
-    default_organization = Organization(label='Default')
+    default_organization = Organization(label=DEFAULT_ORGANIZATION_LABEL)
     default_organization.save()
 
 
