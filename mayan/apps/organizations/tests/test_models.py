3ebadf763bca96b0bea2956dfd89eb5aa4f4bc39
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2016-05-30 19:18:23 -0400

    Add clean up method to OrganizationModelTestCase

--- mayan/apps/organizations/tests/test_models.py
+++ mayan/apps/organizations/tests/test_models.py
@@ -7,11 +7,15 @@ from ..models import Organization
 from .literals import TEST_ORGANIZATION_LABEL, TEST_ORGANIZATION_EDITED_LABEL
 
 
-class OrganizationTestCase(TestCase):
+class OrganizationModelTestCase(TestCase):
     def setUp(self):
         self.organization = Organization.objects.create(
             label=TEST_ORGANIZATION_LABEL
         )
+
+    def tearDown(self):
+        self.organization.delete()
+        Organization.objects.clear_cache()
 
     def test_basic(self):
         queryset = Organization.objects.get_for_now()
