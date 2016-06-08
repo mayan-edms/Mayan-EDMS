ff30268a4bba2526c225e3ee1809cc188f33ae24
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2016-06-08 01:59:32 -0400

    Fix folder api tests.

--- mayan/apps/folders/tests/test_api.py
+++ mayan/apps/folders/tests/test_api.py
@@ -4,10 +4,9 @@ from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 from django.test import override_settings
 
-from rest_framework.test import APITestCase
-
 from documents.models import DocumentType
 from documents.tests import TEST_DOCUMENT_TYPE, TEST_SMALL_DOCUMENT_PATH
+from rest_api.tests import GenericAPITestCase
 from user_management.tests.literals import (
     TEST_ADMIN_EMAIL, TEST_ADMIN_PASSWORD, TEST_ADMIN_USERNAME
 )
@@ -17,20 +16,10 @@ from ..models import Folder
 from .literals import TEST_FOLDER_EDITED_LABEL, TEST_FOLDER_LABEL
 
 
-class FolderAPITestCase(APITestCase):
+class FolderAPITestCase(GenericAPITestCase):
     """
     Test the folder API endpoints
     """
-
-    def setUp(self):
-        self.admin_user = get_user_model().objects.create_superuser(
-            username=TEST_ADMIN_USERNAME, email=TEST_ADMIN_EMAIL,
-            password=TEST_ADMIN_PASSWORD
-        )
-
-        self.client.login(
-            username=TEST_ADMIN_USERNAME, password=TEST_ADMIN_PASSWORD
-        )
 
     def test_folder_create(self):
         response = self.client.post(
