9b3b54eb75b44b45f0b681f38d6914553bdbfab4
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2016-12-22 03:37:07 -0400

    Make DEBUG False by default.

--- mayan/settings/base.py
+++ mayan/settings/base.py
@@ -30,9 +30,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'secret_key_missing'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
--- mayan/settings/production.py
+++ mayan/settings/production.py
@@ -2,13 +2,11 @@ from __future__ import absolute_import, unicode_literals
 
 from . import *  # NOQA
 
-DEBUG = False
-
 # Update this accordingly;
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*']
 
-TEMPLATES['OPTIONS']['loaders'] = (
+TEMPLATES[0]['OPTIONS']['loaders'] = (
     'django.template.loaders.cached.Loader', (
         'django.template.loaders.filesystem.Loader',
         'django.template.loaders.app_directories.Loader',
