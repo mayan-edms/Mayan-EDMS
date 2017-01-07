a013ab0bf5f20a15caa0313ccc7e7a77544596c8
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2017-01-07 01:03:51 -0400

    Fix and streamline the folder app view tests.

--- mayan/apps/common/tests/test_views.py
+++ mayan/apps/common/tests/test_views.py
@@ -76,6 +76,11 @@ class GenericViewTestCase(BaseTestCase):
             data=data, follow=follow
         )
 
+    def grant(self, permission):
+        self.role.permissions.add(
+            permission.stored_permission
+        )
+
     def login(self, username, password):
         logged_in = self.client.login(username=username, password=password)
 
@@ -83,6 +88,9 @@ class GenericViewTestCase(BaseTestCase):
 
         self.assertTrue(logged_in)
         self.assertTrue(user.is_authenticated())
+
+    def login_user(self):
+        self.login(username=TEST_USER_USERNAME, password=TEST_USER_PASSWORD)
 
     def post(self, viewname, *args, **kwargs):
         data = kwargs.pop('data', {})
