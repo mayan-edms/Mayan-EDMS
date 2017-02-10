c218819728f641a7f4d666fffeab5cc864da5433
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2017-02-10 18:16:46 -0400

    Add API endpoints for the message of the day app.

--- mayan/apps/motd/urls.py
+++ mayan/apps/motd/urls.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
 
+from .api_views import APIMessageListView, APIMessageView
 from .views import (
     MessageCreateView, MessageDeleteView, MessageEditView, MessageListView
 )
@@ -10,9 +11,19 @@ urlpatterns = patterns(
     '',
     url(r'^list/$', MessageListView.as_view(), name='message_list'),
     url(r'^create/$', MessageCreateView.as_view(), name='message_create'),
-    url(r'^(?P<pk>\d+)/edit/$', MessageEditView.as_view(), name='message_edit'),
+    url(
+        r'^(?P<pk>\d+)/edit/$', MessageEditView.as_view(), name='message_edit'
+    ),
     url(
         r'^(?P<pk>\d+)/delete/$', MessageDeleteView.as_view(),
         name='message_delete'
     ),
 )
+
+api_urls = [
+    url(r'^messages/$', APIMessageListView.as_view(), name='message-list'),
+    url(
+        r'^messages/(?P<pk>[0-9]+)/$', APIMessageView.as_view(),
+        name='message-detail'
+    ),
+]
