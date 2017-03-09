f30cf440fef07250e332f245ef887b51d632a008
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2017-03-09 02:20:41 -0400

    PEP8 cleanups.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/cabinets/apps.py
+++ mayan/apps/cabinets/apps.py
@@ -7,7 +7,7 @@ from acls import ModelPermission
 from acls.permissions import permission_acl_edit, permission_acl_view
 from common import (
     MayanAppConfig, menu_facet, menu_main, menu_multi_item, menu_object,
-    menu_sidebar, menu_secondary
+    menu_sidebar
 )
 from rest_api.classes import APIEndPoint
 
--- mayan/apps/cabinets/tests/literals.py
+++ mayan/apps/cabinets/tests/literals.py
@@ -2,4 +2,3 @@ from __future__ import absolute_import, unicode_literals
 
 TEST_CABINET_LABEL = 'test cabinet label'
 TEST_CABINET_EDITED_LABEL = 'test cabinet edited label'
-
