0eaf8cea79a859137b56aa88022006666e769838
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2017-08-03 00:17:24 -0400

    Skip UUID migration on Oracle backends. GitHub issue #251.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/documents/migrations/0032_auto_20160315_0537.py
+++ mayan/apps/documents/migrations/0032_auto_20160315_0537.py
@@ -6,17 +6,24 @@ import uuid
 from django.db import connection, migrations, models
 
 
+def forwards(apps, schema_editor):
+    if not schema_editor.connection.vendor == 'oracle':
+        # Skip this migration for Oracle
+        # GitHub issue #251
+        migrations.AlterField(
+            model_name='document',
+            name='uuid',
+            field=models.UUIDField(default=uuid.uuid4, editable=False),
+        )
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ('documents', '0031_convert_uuid'),
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='document',
-            name='uuid',
-            field=models.UUIDField(default=uuid.uuid4, editable=False),
-        ),
+        migrations.RunPython(forwards)
     ]
 
     def __init__(self, *args, **kwargs):
