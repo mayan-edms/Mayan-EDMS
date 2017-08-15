6aa47a26ae5b15f5928cde6f279805809e661e48
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2017-08-15 02:57:54 -0400

    Remove the folders app. Reset English translation source files.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/folders/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import unicode_literals
-
-default_app_config = 'folders.apps.FoldersApp'
--- mayan/apps/folders/menus.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-
-from django.utils.translation import ugettext_lazy as _
-
-from navigation import Menu
-
-menu_folders = Menu(
-    icon='fa fa-folder', label=_('Folders'), name='folders menu'
-)
