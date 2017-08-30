2e4cb03f2dd9fd97d3e018489dc6df06533caeba
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2017-08-30 02:20:07 -0400

    Fix local setting file template indent.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/common/management/commands/literals.py
+++ mayan/apps/common/management/commands/literals.py
@@ -1,10 +1,10 @@
 from __future__ import unicode_literals
 
 SETTING_FILE_TEMPLATE = '''
-    from __future__ import absolute_import
+from __future__ import absolute_import
 
-    from .base import *
+from .base import *
 
-    SECRET_KEY = '{0}'
+SECRET_KEY = '{0}'
 
 '''
