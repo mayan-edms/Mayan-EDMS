82000bfe3998174512ce215e9547e55bb72ec35f
Michael Price <loneviking72@gmail.com>
2018-03-13 02:07:33 -0400

    Bump version to 3.0
    Signed-off-by: Michael Price <loneviking72@gmail.com>
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/__init__.py
+++ mayan/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
 
 __title__ = 'Mayan EDMS'
-__version__ = '2.7.3'
-__build__ = 0x020703
+__version__ = '3.0'
+__build__ = 0x030000
 __author__ = 'Roberto Rosario'
 __author_email__ = 'roberto.rosario@mayan-edms.com'
 __description__ = 'Free Open Source Electronic Document Management System'
