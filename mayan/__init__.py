1248908f1f05d0d29b81616bf2e72dad410b05b0
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2018-04-03 03:58:12 -0400

    Update copyright years.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/__init__.py
+++ mayan/__init__.py
@@ -7,6 +7,6 @@ __author__ = 'Roberto Rosario'
 __author_email__ = 'roberto.rosario@mayan-edms.com'
 __description__ = 'Free Open Source Electronic Document Management System'
 __license__ = 'Apache 2.0'
-__copyright__ = 'Copyright 2011-2016 Roberto Rosario'
+__copyright__ = 'Copyright 2011-2018 Roberto Rosario'
 __website__ = 'https://www.mayan-edms.com'
 
