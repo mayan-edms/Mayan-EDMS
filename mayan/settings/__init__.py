c482bb604cac9bd846bc4c45fba503411ab89a91
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2018-04-13 15:53:00 -0400

    Add better explanation when a .local.py setting import error occurs.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/settings/__init__.py
+++ mayan/settings/__init__.py
@@ -1,10 +1,18 @@
 from __future__ import absolute_import
 
+import logging
+
 from django.utils.encoding import force_text
+
+logger = logging.getLogger(__name__)
 
 try:
     from .local import *  # NOQA
 except ImportError as exception:
     if force_text(exception) != 'No module named local' and force_text(exception) != 'No module named \'mayan.settings.local\'':
+        logger.error('Error importing user\'s local.py; %s', exception)
         raise
+    else:
+        logger.info('No local.py settings file. Using defaults.')
+else:
     from .base import *  # NOQA
