9fbedd244cc589fc2007e8aa2edcbc8a6d05c7a1
Eric Riggs <ericriggs42@gmail.com>
2018-04-22 13:51:14 -0400

    - Put the log file in the /tmp folder. - This is temporary and should be fixed. Create /media when not found?
    Signed-off-by: Eric Riggs <ericriggs42@gmail.com>

--- mayan/settings/testing/base.py
+++ mayan/settings/testing/base.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from .. import *  # NOQA
 
+COMMON_PRODUCTION_ERROR_LOG_PATH = '/tmp'
 INSTALLED_APPS += ('test_without_migrations',)
 TEMPLATES[0]['OPTIONS']['loaders'] = (
     'django.template.loaders.filesystem.Loader',
