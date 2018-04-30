a788a99d93c91daed6f7c0e1b3006890f8f81bee
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2018-04-29 23:14:55 -0400

    Set CELERY_ALWAYS_EAGER to True on the development setting file.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/settings/development.py
+++ mayan/settings/development.py
@@ -2,20 +2,21 @@ from __future__ import absolute_import, unicode_literals
 
 from . import *  # NOQA
 
-DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
-TEMPLATES[0]['OPTIONS']['loaders'] = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
+DEBUG = True
+
+CELERY_ALWAYS_EAGER = True
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = CELERY_ALWAYS_EAGER
 
 INSTALLED_APPS += (
     'rosetta',
     'django_extensions',
 )
 
-WSGI_AUTO_RELOAD = True
+TEMPLATES[0]['OPTIONS']['loaders'] = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
 
-CELERY_EAGER_PROPAGATES_EXCEPTIONS = CELERY_ALWAYS_EAGER
+WSGI_AUTO_RELOAD = True
