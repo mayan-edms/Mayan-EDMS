3689df4aacdcaa29442ddbbba6726e03f742b9a9
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2018-04-29 23:32:22 -0400

    Better default for the testing error log file full path.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/settings/testing/base.py
+++ mayan/settings/testing/base.py
@@ -2,8 +2,10 @@ from __future__ import absolute_import, unicode_literals
 
 from .. import *  # NOQA
 
-COMMON_PRODUCTION_ERROR_LOG_PATH = '/tmp'
 INSTALLED_APPS += ('test_without_migrations',)
+
+COMMON_PRODUCTION_ERROR_LOG_PATH = '/tmp/mayan-errors.log'
+
 TEMPLATES[0]['OPTIONS']['loaders'] = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
