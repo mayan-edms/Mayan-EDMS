b2b8549f8aa39022dba3db43165f9be3ddf7c284
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2018-05-10 04:32:57 -0400

    Use variables for default values instead of literals.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,12 +6,14 @@ INSTALL_FLAG=/var/lib/mayan/media/system/SECRET_KEY
 
 export MAYAN_DEFAULT_BROKER_URL=redis://127.0.0.1:6379/0
 export MAYAN_DEFAULT_CELERY_RESULT_BACKEND=redis://127.0.0.1:6379/0
+
 export MAYAN_ALLOWED_HOSTS=*
 export MAYAN_BIN=/usr/local/lib/python2.7/dist-packages/mayan/bin/mayan-edms.py
-export MAYAN_BROKER_URL=${MAYAN_BROKER_URL:-redis://127.0.0.1:6379/0}
-export MAYAN_CELERY_RESULT_BACKEND=${MAYAN_CELERY_RESULT_BACKEND:-redis://127.0.0.1:6379/0}
+export MAYAN_BROKER_URL=${MAYAN_BROKER_URL:-${MAYAN_DEFAULT_BROKER_URL}}
+export MAYAN_CELERY_RESULT_BACKEND=${MAYAN_CELERY_RESULT_BACKEND:-${MAYAN_DEFAULT_CELERY_RESULT_BACKEND}}
 export MAYAN_GUNICORN_WORKERS=${MAYAN_GUNICORN_WORKERS:-2}
 export MAYAN_INSTALL_DIR=/usr/local/lib/python2.7/dist-packages/mayan
+export MAYAN_PYTHON_DIR=/usr/local/bin
 export MAYAN_MEDIA_ROOT=/var/lib/mayan
 export MAYAN_SETTINGS_FILE=${MAYAN_SETTINGS_FILE:-mayan.settings.production}
 
