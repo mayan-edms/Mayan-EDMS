e4af58d4b7cbd6a57020516c43001f485e9f60b5
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2018-05-24 23:35:11 -0400

    Remove whitenoise from middlewares during test suit as it causes out of memory errors.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/settings/testing/__init__.py
+++ mayan/settings/testing/__init__.py
@@ -1,1 +1,7 @@
 from .base import *  # NOQA
+
+# Remove whitenoise from middlewares. Causes out of memory errors during test
+# suit
+MIDDLEWARE_CLASSES = [
+    cls for cls in MIDDLEWARE_CLASSES if cls !='whitenoise.middleware.WhiteNoiseMiddleware'
+]
