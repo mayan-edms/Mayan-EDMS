e524c3bd799e8708f57e8b53740c28d9cf38575f
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2018-06-02 21:47:12 -0400

    Remove transparent import of local.py
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/settings/__init__.py
+++ mayan/settings/__init__.py
@@ -1,18 +1,3 @@
 from __future__ import absolute_import
 
-import logging
-
-from django.utils.encoding import force_text
-
 from .base import *  # NOQA
-
-logger = logging.getLogger(__name__)
-
-try:
-    from .local import *  # NOQA
-except ImportError as exception:
-    if force_text(exception) != 'No module named local' and force_text(exception) != 'No module named \'mayan.settings.local\'':
-        logger.error('Error importing user\'s local.py; %s', exception)
-        raise
-    else:
-        logger.info('No local.py settings file. Using defaults.')
