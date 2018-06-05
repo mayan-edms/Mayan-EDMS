d1973e6e05bd96a19aacff1259e0316a0bfbdee0
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2018-06-05 02:12:28 -0400

    Explicitly set CELERY ALWAYS EAGER to False in the production setting file.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/settings/production.py
+++ mayan/settings/production.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, unicode_literals
 
 from . import *  # NOQA
 
+CELERY_ALWAYS_EAGER = False
+
 TEMPLATES[0]['OPTIONS']['loaders'] = (
     (
         'django.template.loaders.cached.Loader', (
