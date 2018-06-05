b5a4a9cf4519dc0f63088f5342e5a5dc453d5105
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2018-06-05 02:13:09 -0400

    Add a short version of the copyright package metadata for the documentation template.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/__init__.py
+++ mayan/__init__.py
@@ -7,6 +7,7 @@ __author__ = 'Roberto Rosario'
 __author_email__ = 'roberto.rosario@mayan-edms.com'
 __description__ = 'Free Open Source Electronic Document Management System'
 __license__ = 'Apache 2.0'
-__copyright__ = 'Copyright 2011-2018 Roberto Rosario'
+__copyright_short__ = '2011-2018 Roberto Rosario'
+__copyright__ = '{} {}'.format('Copyright', __copyright_short__)
 __website__ = 'https://www.mayan-edms.com'
 
