8f0ad91d96e6874fdd3164be0796818e53a83f36
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2018-06-28 16:47:34 -0400

    Update the tags app to use the Icon class.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

---
+++ mayan/apps/tags/icons.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, unicode_literals
+
+from appearance.classes import Icon
+
+icon_tag_create = Icon(driver_name='fontawesome', symbol='plus')
+icon_tag_document_list = Icon(driver_name='fontawesome', symbol='tag')
+icon_tag_list = Icon(driver_name='fontawesome', symbol='tag')
