e2a4892b1b029224ffefed3a41f0aa91d0c901b4
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2018-08-16 19:27:32 -0400

    Bump version to 3.0.2
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/__init__.py
+++ mayan/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import unicode_literals
 
 __title__ = 'Mayan EDMS'
-__version__ = '3.0.1'
-__build__ = 0x030001
+__version__ = '3.0.2'
+__build__ = 0x030002
 __author__ = 'Roberto Rosario'
 __author_email__ = 'roberto.rosario@mayan-edms.com'
 __description__ = 'Free Open Source Electronic Document Management System'
