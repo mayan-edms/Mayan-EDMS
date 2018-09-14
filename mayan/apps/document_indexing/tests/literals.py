0fadd845677c5d9f3c17d07f0b2f0ce1b9b6ab15
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2018-09-14 00:17:32 -0400

    document_indexing: Fix typo in test literal.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/document_indexing/tests/literals.py
+++ mayan/apps/document_indexing/tests/literals.py
@@ -6,4 +6,4 @@ TEST_INDEX_SLUG = 'test_slug'
 TEST_METADATA_TYPE_LABEL = 'test metadata label'
 TEST_METADATA_TYPE_NAME = 'test_metadata_name'
 TEST_INDEX_TEMPLATE_METADATA_EXPRESSION = '{{ document.metadata_value_of.%s }}' % TEST_METADATA_TYPE_NAME
-TEST_INDEX_TEMPLATE_LABEL_EXPRESSION = '{{ document.labe }}'
+TEST_INDEX_TEMPLATE_LABEL_EXPRESSION = '{{ document.label }}'
