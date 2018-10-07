c715d0fc8ecf99055be1a3240a36118bc1b8cbfe
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2018-10-07 14:18:40 -0400

    Prepare file for release.
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/__init__.py
+++ mayan/__init__.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 __title__ = 'Mayan EDMS'
 __version__ = '3.1.5'
 __build__ = 0x030105
-__build_string__ = 'v3.1.5_Sun Oct 7 04:45:31 2018 -0400'
+__build_string__ = 'v3.1.5-5-ga49e7ebd0_Sun Oct 7 14:17:48 2018 -0400'
 __author__ = 'Roberto Rosario'
 __author_email__ = 'roberto.rosario@mayan-edms.com'
 __description__ = 'Free Open Source Electronic Document Management System'
