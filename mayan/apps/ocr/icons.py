0865975f2e04337216c0b6a03c9739ae95607969
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2019-04-22 21:29:51 -0400

    Add OCR icons
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/ocr/icons.py
+++ mayan/apps/ocr/icons.py
@@ -2,15 +2,19 @@ from __future__ import absolute_import, unicode_literals
 
 from mayan.apps.appearance.classes import Icon
 
-icon_document_content = Icon(driver_name='fontawesome', symbol='font')
+icon_document_ocr_content = Icon(driver_name='fontawesome', symbol='font')
+icon_document_page_ocr_content = Icon(driver_name='fontawesome', symbol='font')
+icon_document_multiple_submit = Icon(driver_name='fontawesome', symbol='font')
 icon_document_ocr_download = Icon(
-    driver_name='fontawesome', symbol='file-alt'
+    driver_name='fontawesome', symbol='font'
 )
 icon_document_ocr_errors_list = Icon(
-    driver_name='fontawesome', symbol='file-alt'
+    driver_name='fontawesome', symbol='font'
 )
 icon_document_type_ocr_settings = Icon(
     driver_name='fontawesome', symbol='font'
 )
 icon_document_type_submit = Icon(driver_name='fontawesome', symbol='font')
-icon_entry_list = Icon(driver_name='fontawesome', symbol='file-alt')
+icon_entry_list = Icon(driver_name='fontawesome', symbol='font')
+
+icon_document_submit = icon_document_multiple_submit
--- mayan/apps/ocr/links.py
+++ mayan/apps/ocr/links.py
@@ -4,54 +4,58 @@ from django.utils.translation import ugettext_lazy as _
 
 from mayan.apps.navigation import Link
 
-from .icons import (
-    icon_document_content, icon_document_ocr_download,
-    icon_document_ocr_errors_list, icon_document_type_ocr_settings,
-    icon_document_type_submit, icon_entry_list
-)
 from .permissions import (
     permission_ocr_content_view, permission_ocr_document,
     permission_document_type_ocr_setup
 )
 
 link_document_page_ocr_content = Link(
-    args='resolved_object.id', icon_class=icon_document_content,
+    args='resolved_object.id',
+    icon_class_path='mayan.apps.ocr.icons.icon_document_page_ocr_content',
     permissions=(permission_ocr_content_view,), text=_('OCR'),
     view='ocr:document_page_ocr_content',
 )
 link_document_ocr_content = Link(
-    args='resolved_object.id', icon_class=icon_document_content,
+    args='resolved_object.id',
+    icon_class_path='mayan.apps.ocr.icons.icon_document_ocr_content',
     permissions=(permission_ocr_content_view,), text=_('OCR'),
     view='ocr:document_ocr_content',
 )
 link_document_submit = Link(
-    args='resolved_object.id', permissions=(permission_ocr_document,),
-    text=_('Submit for OCR'), view='ocr:document_submit'
+    args='resolved_object.id',
+    icon_class_path='mayan.apps.ocr.icons.icon_document_submit',
+    permissions=(permission_ocr_document,), text=_('Submit for OCR'),
+    view='ocr:document_submit'
 )
 link_document_submit_multiple = Link(
+    icon_class_path='mayan.apps.ocr.icons.icon_document_submit',
     text=_('Submit for OCR'), view='ocr:document_submit_multiple'
 )
 link_document_type_ocr_settings = Link(
-    args='resolved_object.id', icon_class=icon_document_type_ocr_settings,
+    args='resolved_object.id',
+    icon_class_path='mayan.apps.ocr.icons.icon_document_type_ocr_settings',
     permissions=(permission_document_type_ocr_setup,), text=_('Setup OCR'),
     view='ocr:document_type_ocr_settings',
 )
 link_document_type_submit = Link(
-    icon_class=icon_document_type_submit,
+    icon_class_path='mayan.apps.ocr.icons.icon_document_type_submit',
     permissions=(permission_ocr_document,), text=_('OCR documents per type'),
     view='ocr:document_type_submit'
 )
 link_entry_list = Link(
-    icon_class=icon_entry_list, permissions=(permission_ocr_document,),
-    text=_('OCR errors'), view='ocr:entry_list'
+    icon_class_path='mayan.apps.ocr.icons.icon_entry_list',
+    permissions=(permission_ocr_document,), text=_('OCR errors'),
+    view='ocr:entry_list'
 )
 link_document_ocr_errors_list = Link(
-    args='resolved_object.id', icon_class=icon_document_ocr_errors_list,
+    args='resolved_object.id',
+    icon_class_path='mayan.apps.ocr.icons.icon_document_ocr_errors_list',
     permissions=(permission_ocr_content_view,), text=_('OCR errors'),
     view='ocr:document_ocr_error_list'
 )
 link_document_ocr_download = Link(
-    args='resolved_object.id', icon_class=icon_document_ocr_download,
+    args='resolved_object.id',
+    icon_class_path='mayan.apps.ocr.icons.icon_document_ocr_download',
     permissions=(permission_ocr_content_view,), text=_('Download OCR text'),
     view='ocr:document_ocr_download'
 )
