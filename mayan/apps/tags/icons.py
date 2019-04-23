48249b977d383a9370abd733d30206716a8a0551
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2019-04-23 04:24:45 -0400

    Add tags app icons
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/tags/icons.py
+++ mayan/apps/tags/icons.py
@@ -1,11 +1,41 @@
 from __future__ import absolute_import, unicode_literals
 
 from mayan.apps.appearance.classes import Icon
+from mayan.apps.documents.icons import icon_document_list
 
-icon_menu_tags = Icon(driver_name='fontawesome', symbol='tag')
-icon_tag_attach = Icon(driver_name='fontawesome', symbol='plus')
-icon_tag_create = Icon(driver_name='fontawesome', symbol='plus')
+icon_document_multiple_tag_multiple_attach = Icon(
+    driver_name='fontawesome-dual', primary_symbol='tag',
+    secondary_symbol='arrow-right'
+)
+icon_document_multiple_tag_multiple_remove = Icon(
+    driver_name='fontawesome-dual', primary_symbol='tag',
+    secondary_symbol='minus'
+)
+icon_document_tag_multiple_attach = Icon(
+    driver_name='fontawesome-dual', primary_symbol='tag',
+    secondary_symbol='arrow-right'
+)
+icon_document_tag_multiple_remove = Icon(
+    driver_name='fontawesome-dual', primary_symbol='tag',
+    secondary_symbol='minus'
+)
+
+icon_document_tag_remove_submit = Icon(
+    driver_name='fontawesome', symbol='minus'
+)
+icon_document_tag_list = Icon(driver_name='fontawesome', symbol='tags')
+
+icon_menu_tags = Icon(driver_name='fontawesome', symbol='tags')
+
+icon_tag_create = Icon(
+    driver_name='fontawesome-dual', primary_symbol='tag',
+    secondary_symbol='plus'
+)
+icon_tag_delete = Icon(driver_name='fontawesome', symbol='times')
 icon_tag_delete_submit = Icon(driver_name='fontawesome', symbol='times')
-icon_tag_document_list = Icon(driver_name='fontawesome', symbol='tag')
+icon_tag_edit = Icon(driver_name='fontawesome', symbol='pen')
+icon_tag_document_list = icon_document_list
 icon_tag_list = Icon(driver_name='fontawesome', symbol='tag')
-icon_tag_remove_submit = Icon(driver_name='fontawesome', symbol='minus')
+
+
+
