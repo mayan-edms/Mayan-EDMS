e01017ffe5f067ebb0587ac363a94e5ec71aac74
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2019-04-28 02:06:01 -0400

    Add missing linking app icons
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/linking/icons.py
+++ mayan/apps/linking/icons.py
@@ -1,13 +1,29 @@
 from __future__ import absolute_import, unicode_literals
 
 from mayan.apps.appearance.classes import Icon
+from mayan.apps.documents.icons import icon_document_type
 
-icon_smart_link_condition_create = Icon(
-    driver_name='fontawesome', symbol='plus'
-)
 icon_smart_link_condition = Icon(driver_name='fontawesome', symbol='code')
-icon_smart_link_create = Icon(driver_name='fontawesome', symbol='plus')
+icon_smart_link_condition_create = Icon(
+    driver_name='fontawesome-dual', primary_symbol='code',
+    secondary_symbol='plus'
+)
+icon_smart_link_condition_delete = Icon(
+    driver_name='fontawesome', symbol='times'
+)
+icon_smart_link_condition_edit = Icon(
+    driver_name='fontawesome', symbol='pencil-alt'
+)
+icon_smart_link_condition_list = icon_smart_link_condition
+icon_smart_link_create = Icon(
+    driver_name='fontawesome-dual', primary_symbol='link',
+    secondary_symbol='plus'
+)
+icon_smart_link_delete = Icon(driver_name='fontawesome', symbol='times')
+icon_smart_link_document_types = icon_document_type
+icon_smart_link_edit = Icon(driver_name='fontawesome', symbol='pencil-alt')
 icon_smart_link_instances_for_document = Icon(
     driver_name='fontawesome', symbol='link'
 )
 icon_smart_link_setup = Icon(driver_name='fontawesome', symbol='link')
+icon_smart_link_list = Icon(driver_name='fontawesome', symbol='link')
--- mayan/apps/linking/links.py
+++ mayan/apps/linking/links.py
@@ -5,48 +5,61 @@ from django.utils.translation import ugettext_lazy as _
 from mayan.apps.documents.permissions import permission_document_view
 from mayan.apps.navigation.classes import Link
 
-from .icons import (
-    icon_smart_link_condition_create, icon_smart_link_create,
-    icon_smart_link_instances_for_document, icon_smart_link_setup
-)
+#from .icons import (
+#    icon_smart_link_condition_create, icon_smart_link_create,
+#    icon_smart_link_instances_for_document, icon_smart_link_setup
+#)
 from .permissions import (
     permission_smart_link_create, permission_smart_link_delete,
     permission_smart_link_edit, permission_smart_link_view
 )
 
 link_smart_link_condition_create = Link(
-    args='object.pk', icon_class=icon_smart_link_condition_create,
+    args='object.pk',
+    icon_class_path='mayan.apps.linking.icons.icon_smart_link_condition_create',
     permissions=(permission_smart_link_edit,), text=_('Create condition'),
     view='linking:smart_link_condition_create',
 )
 link_smart_link_condition_delete = Link(
-    args='resolved_object.pk', permissions=(permission_smart_link_edit,),
+    args='resolved_object.pk',
+    icon_class_path='mayan.apps.linking.icons.icon_smart_link_condition_delete',
+    permissions=(permission_smart_link_edit,),
     tags='dangerous', text=_('Delete'),
     view='linking:smart_link_condition_delete',
 )
 link_smart_link_condition_edit = Link(
-    args='resolved_object.pk', permissions=(permission_smart_link_edit,),
+    args='resolved_object.pk',
+    icon_class_path='mayan.apps.linking.icons.icon_smart_link_condition_edit',
+    permissions=(permission_smart_link_edit,),
     text=_('Edit'), view='linking:smart_link_condition_edit',
 )
 link_smart_link_condition_list = Link(
-    args='object.pk', permissions=(permission_smart_link_edit,),
+    args='object.pk',
+    icon_class_path='mayan.apps.linking.icons.icon_smart_link_condition_list',
+    permissions=(permission_smart_link_edit,),
     text=_('Conditions'), view='linking:smart_link_condition_list',
 )
 link_smart_link_create = Link(
-    icon_class=icon_smart_link_create,
+    icon_class_path='mayan.apps.linking.icons.icon_smart_link_create',
     permissions=(permission_smart_link_create,),
     text=_('Create new smart link'), view='linking:smart_link_create'
 )
 link_smart_link_delete = Link(
-    args='object.pk', permissions=(permission_smart_link_delete,),
+    args='object.pk',
+    icon_class_path='mayan.apps.linking.icons.icon_smart_link_delete',
+    permissions=(permission_smart_link_delete,),
     tags='dangerous', text=_('Delete'), view='linking:smart_link_delete',
 )
 link_smart_link_document_types = Link(
-    args='object.pk', permissions=(permission_smart_link_edit,),
+    args='object.pk',
+    icon_class_path='mayan.apps.linking.icons.icon_smart_link_document_types',
+    permissions=(permission_smart_link_edit,),
     text=_('Document types'), view='linking:smart_link_document_types',
 )
 link_smart_link_edit = Link(
-    args='object.pk', permissions=(permission_smart_link_edit,),
+    args='object.pk',
+    icon_class_path='mayan.apps.linking.icons.icon_smart_link_edit',
+    permissions=(permission_smart_link_edit,),
     text=_('Edit'), view='linking:smart_link_edit',
 )
 link_smart_link_instance_view = Link(
@@ -56,16 +69,17 @@ link_smart_link_instance_view = Link(
 )
 link_smart_link_instances_for_document = Link(
     args='resolved_object.pk',
-    icon_class=icon_smart_link_instances_for_document,
+    icon_class_path='mayan.apps.linking.icons.icon_smart_link_instances_for_document',
     permissions=(permission_document_view,), text=_('Smart links'),
     view='linking:smart_link_instances_for_document',
 )
 link_smart_link_list = Link(
+    icon_class_path='mayan.apps.linking.icons.icon_smart_link_list',
     permissions=(permission_smart_link_create,), text=_('Smart links'),
     view='linking:smart_link_list'
 )
 link_smart_link_setup = Link(
-    icon_class=icon_smart_link_setup,
+    icon_class_path='mayan.apps.linking.icons.icon_smart_link_setup',
     permissions=(permission_smart_link_create,), text=_('Smart links'),
     view='linking:smart_link_list'
 )
