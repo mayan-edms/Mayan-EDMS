aee3645c7adf3f52bd057c0bc928898c64698dfa
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2019-04-29 00:30:50 -0400

    Fix mailer app event tests
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/mailer/tests/test_events.py
+++ mayan/apps/mailer/tests/test_events.py
@@ -24,24 +24,25 @@ class MailerEventsTestCase(DocumentTestMixin, MailerTestMixin, MailerViewTestMix
         self._create_test_user_mailer()
 
     def test_email_send_event(self):
-        Action.objects.all().delete()
-
         self.grant_access(
             obj=self.test_user_mailer, permission=permission_user_mailer_use
         )
 
+        Action.objects.all().delete()
+
         response = self._request_test_user_mailer_test_view()
+
+        action = Action.objects.last()
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(len(mail.outbox), 1)
 
-        self.assertEqual(Action.objects.last().target, self.test_user_mailer)
-        self.assertEqual(Action.objects.last().verb, event_email_sent.id)
-        self.assertEqual(Action.objects.last().action_object, None)
+        self.assertEqual(action.target, self.test_user_mailer)
+        self.assertEqual(action.verb, event_email_sent.id)
+        self.assertEqual(action.action_object, None)
 
     def test_document_email_send_event(self):
         self.upload_document()
-        Action.objects.all().delete()
 
         self.grant_access(
             obj=self.test_user_mailer, permission=permission_user_mailer_use
@@ -49,12 +50,15 @@ class MailerEventsTestCase(DocumentTestMixin, MailerTestMixin, MailerViewTestMix
         self.grant_access(
             obj=self.test_document, permission=permission_mailing_send_document
         )
+        Action.objects.all().delete()
 
         response = self._request_test_document_send_view()
+
+        action = Action.objects.last()
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(len(mail.outbox), 1)
 
-        self.assertEqual(Action.objects.last().target, self.test_user_mailer)
-        self.assertEqual(Action.objects.last().verb, event_email_sent.id)
-        self.assertEqual(Action.objects.last().action_object, self.test_document)
+        self.assertEqual(action.target, self.test_user_mailer)
+        self.assertEqual(action.verb, event_email_sent.id)
+        self.assertEqual(action.action_object, self.test_document)
