f6afec031dd3ae33a784b2dc5d9a753689fa957e
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2019-05-08 19:18:04 -0400

    Allow skipping dependencies install during upgrade
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/common/management/commands/performupgrade.py
+++ mayan/apps/common/management/commands/performupgrade.py
@@ -9,6 +9,12 @@ from ...signals import perform_upgrade, post_upgrade, pre_upgrade
 class Command(management.BaseCommand):
     help = 'Performs the required steps after a version upgrade.'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--no-javascript', action='store_true', dest='no_javascript',
+            help='Don\'t download the JavaScript dependencies.',
+        )
+
     def handle(self, *args, **options):
         try:
             pre_upgrade.send(sender=self)
@@ -19,9 +25,10 @@ class Command(management.BaseCommand):
                 )
             )
 
-        management.call_command(
-            command_name='installjavascript', interactive=False
-        )
+        if not options.get('no_javascript', False):
+            management.call_command(
+                command_name='installjavascript', interactive=False
+            )
 
         try:
             perform_upgrade.send(sender=self)
