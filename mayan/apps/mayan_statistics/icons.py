b687a7f51989cc11c52e626586c6a65e258db6de
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2019-05-14 17:02:26 -0400

    Add missing statistics app icons
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/mayan_statistics/icons.py
+++ mayan/apps/mayan_statistics/icons.py
@@ -3,3 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from mayan.apps.appearance.classes import Icon
 
 icon_statistics = Icon(driver_name='fontawesome', symbol='sort-numeric-up')
+icon_execute = Icon(driver_name='fontawesome', symbol='cog')
+icon_namespace_details = Icon(driver_name='fontawesome', symbol='sort-numeric-up')
+icon_namespace_list = Icon(driver_name='fontawesome', symbol='sort-numeric-up')
+icon_view = Icon(driver_name='fontawesome', symbol='eye')
--- mayan/apps/mayan_statistics/links.py
+++ mayan/apps/mayan_statistics/links.py
@@ -4,27 +4,31 @@ from django.utils.translation import ugettext_lazy as _
 
 from mayan.apps.navigation.classes import Link
 
-from .icons import icon_statistics
 from .permissions import permission_statistics_view
 
 # Translators: 'Queue' here is the verb, to queue a statistic to update
 link_execute = Link(
+    icon_class_path='mayan.apps.mayan_statistics.icons.icon_execute',
     permissions=(permission_statistics_view,), text=_('Queue'),
     view='statistics:statistic_queue', args='resolved_object.slug'
 )
 link_view = Link(
+    icon_class_path='mayan.apps.mayan_statistics.icons.icon_view',
     permissions=(permission_statistics_view,), text=_('View'),
     view='statistics:statistic_detail', args='resolved_object.slug'
 )
 link_namespace_details = Link(
+    icon_class_path='mayan.apps.mayan_statistics.icons.icon_namespace_details',
     permissions=(permission_statistics_view,), text=_('Namespace details'),
     view='statistics:namespace_details', args='resolved_object.slug'
 )
 link_namespace_list = Link(
+    icon_class_path='mayan.apps.mayan_statistics.icons.icon_namespace_list',
     permissions=(permission_statistics_view,), text=_('Namespace list'),
     view='statistics:namespace_list'
 )
 link_statistics = Link(
-    icon_class=icon_statistics, permissions=(permission_statistics_view,),
+    icon_class_path='mayan.apps.mayan_statistics.icons.icon_statistics',
+    permissions=(permission_statistics_view,),
     text=_('Statistics'), view='statistics:namespace_list'
 )
