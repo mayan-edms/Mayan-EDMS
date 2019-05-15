dfb368e6dc253e2d6a3d8be49b98cbc7388043c6
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2019-05-15 02:07:24 -0400

    Fix file metadata queue definition
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/file_metadata/queues.py
+++ mayan/apps/file_metadata/queues.py
@@ -3,11 +3,12 @@ from __future__ import absolute_import, unicode_literals
 from django.utils.translation import ugettext_lazy as _
 
 from mayan.apps.task_manager.classes import CeleryQueue
+from mayan.apps.task_manager.workers import worker_medium
 
 queue = CeleryQueue(
-    label=_('File metadata'), name='file_metadata'
+    label=_('File metadata'), name='file_metadata', worker=worker_medium
 )
 queue.add_task_type(
     label=_('Process document version'),
-    name='mayan.apps.file_metadata.tasks.task_process_document_version'
+    dotted_path='mayan.apps.file_metadata.tasks.task_process_document_version'
 )
