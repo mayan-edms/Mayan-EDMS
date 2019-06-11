fe3d2e85c2bcc1840d6a3cd53a081fde09e900aa
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2019-06-11 01:11:46 -0400

    Transform the UUID search field based on database
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/documents/search.py
+++ mayan/apps/documents/search.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.apps import apps
+from django.db import connection, models
 from django.utils.translation import ugettext_lazy as _
 
 from mayan.apps.common.literals import LIST_MODE_CHOICE_ITEM
@@ -9,8 +10,11 @@ from mayan.apps.dynamic_search.classes import SearchModel
 from .permissions import permission_document_view
 
 
-def format_uuid(term_string):
-    return term_string.replace('-', '')
+def transformation_format_uuid(term_string):
+    if connection.vendor in ('mysql', 'sqlite'):
+        return term_string.replace('-', '')
+    else:
+        return term_string
 
 
 def get_queryset_page_search_queryset():
@@ -36,7 +40,8 @@ document_search.add_model_field(
 document_search.add_model_field(field='label', label=_('Label'))
 document_search.add_model_field(field='description', label=_('Description'))
 document_search.add_model_field(
-    field='uuid', label=_('UUID'), transformation_function=format_uuid
+    field='uuid', label=_('UUID'),
+    transformation_function=transformation_format_uuid
 )
 document_search.add_model_field(
     field='versions__checksum', label=_('Checksum')
