0c3768530260f1e1b27c9ebee63251627cdb9bcf
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2019-06-13 02:33:13 -0400

    Remove copyright date range
    Copyright date ranges are not necessary. Berne Convention and all
    copyright reciprocity pacts offer protection for at least 50 years
    after the death of the author. Copyright is automatic for all countries
    in the agreement and equally enforceable.
    https://www.copyright.gov/circs/circ38a.pdf
    
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/__init__.py
+++ mayan/__init__.py
@@ -9,6 +9,6 @@ __author__ = 'Roberto Rosario'
 __author_email__ = 'roberto.rosario@mayan-edms.com'
 __description__ = 'Free Open Source Electronic Document Management System'
 __license__ = 'Apache 2.0'
-__copyright_short__ = '2011-2018 Roberto Rosario'
+__copyright_short__ = '2011 Roberto Rosario'
 __copyright__ = '{} {}'.format('Copyright', __copyright_short__)
 __website__ = 'https://www.mayan-edms.com'
