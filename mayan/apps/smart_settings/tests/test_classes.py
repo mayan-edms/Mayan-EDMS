f17d13d68f02f605f86812d283f9d803311cae2e
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2019-06-14 03:29:21 -0400

    Delete backup config without raising exceptions
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/smart_settings/tests/test_classes.py
+++ mayan/apps/smart_settings/tests/test_classes.py
@@ -5,9 +5,11 @@ import os
 from pathlib2 import Path
 
 from django.conf import settings
+from django.utils.encoding import force_text
 
 from mayan.apps.common.settings import setting_paginate_by
 from mayan.apps.common.tests import BaseTestCase
+from mayan.apps.storage.utils import fs_cleanup
 
 from ..classes import Setting
 
@@ -23,14 +25,14 @@ class ClassesTestCase(BaseTestCase):
 
     def test_config_backup_creation(self):
         path_config_backup = Path(settings.CONFIGURATION_LAST_GOOD_FILEPATH)
-        path_config_backup.unlink()
+        fs_cleanup(filename=force_text(path_config_backup))
 
         Setting.save_last_known_good()
         self.assertTrue(path_config_backup.exists())
 
     def test_config_backup_creation_no_tags(self):
         path_config_backup = Path(settings.CONFIGURATION_LAST_GOOD_FILEPATH)
-        path_config_backup.unlink()
+        fs_cleanup(filename=force_text(path_config_backup))
 
         Setting.save_last_known_good()
         self.assertTrue(path_config_backup.exists())
