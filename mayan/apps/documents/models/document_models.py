3f86dfc902a7fdf5736cfee073d651a46e199806
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2019-06-15 02:25:00 -0400

    Fix typo
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/documents/models/document_models.py
+++ mayan/apps/documents/models/document_models.py
@@ -36,7 +36,7 @@ class Document(models.Model):
     """
     uuid = models.UUIDField(
         default=uuid.uuid4, editable=False, help_text=_(
-            'UUID of a document, universally Unique ID. An unique identifier'
+            'UUID of a document, universally Unique ID. An unique identifier '
             'generated for each document.'
         ), verbose_name=_('UUID')
     )
