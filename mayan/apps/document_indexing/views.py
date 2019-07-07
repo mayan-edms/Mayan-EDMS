7913b5ddcc29e90de785674adbadbbd7d39e9fc8
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2019-07-07 01:06:58 -0400

    Sort dictionary entry
    Signed-off-by: Roberto Rosario <roberto.rosario.gonzalez@gmail.com>

--- mayan/apps/document_indexing/views.py
+++ mayan/apps/document_indexing/views.py
@@ -308,8 +308,8 @@ class IndexListView(SingleObjectListView):
 
     def get_extra_context(self):
         return {
-            'hide_object': True,
             'hide_links': True,
+            'hide_object': True,
             'no_results_icon': icon_index,
             'no_results_main_link': link_index_template_create.resolve(
                 context=RequestContext(request=self.request)
