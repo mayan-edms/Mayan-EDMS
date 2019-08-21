c833b7a74c9e52b5a02ad9942790ebb1cd5a393a
Roberto Rosario <Roberto.Rosario@mayan-edms.com>
2019-08-21 03:54:05 -0400

    Add as_id_list helper for tests
    Signed-off-by: Roberto Rosario <Roberto.Rosario@mayan-edms.com>

--- mayan/apps/common/tests/utils.py
+++ mayan/apps/common/tests/utils.py
@@ -1,10 +1,20 @@
+from __future__ import absolute_import, unicode_literals
+
 from contextlib import contextmanager
 import sys
+
+from django.utils.encoding import force_text
 
 
 class NullFile(object):
     def write(self, string):
         """Writes here go nowhere"""
+
+
+def as_id_list(items):
+    return ','.join(
+        [force_text(item.pk) for item in items]
+    )
 
 
 @contextmanager
