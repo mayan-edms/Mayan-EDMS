e7faece3425028d326527e6576f84e0413d69c4a
Roberto Rosario <Roberto.Rosario@mayan-edms.com>
2019-08-21 04:25:56 -0400

    Reorganize and improve checkouts tests
    Signed-off-by: Roberto Rosario <Roberto.Rosario@mayan-edms.com>

--- mayan/apps/checkouts/tests/test_links.py
+++ mayan/apps/checkouts/tests/test_links.py
@@ -10,38 +10,40 @@ from ..permissions import (
 from .mixins import DocumentCheckoutTestMixin
 
 
-class CheckoutLinksTestCase(DocumentCheckoutTestMixin, GenericDocumentViewTestCase):
-    def _resolve_checkout_link(self):
+class CheckoutLinksTestCase(
+    DocumentCheckoutTestMixin, GenericDocumentViewTestCase
+):
+    def _resolve_document_check_out_link(self):
         self.add_test_view(test_object=self.test_document)
         context = self.get_test_view()
         context['user'] = self._test_case_user
         return link_check_out_document.resolve(context=context)
 
-    def test_checkout_link_no_access(self):
-        resolved_link = self._resolve_checkout_link()
-        self.assertEqual(resolved_link, None)
-
-    def test_checkout_link_with_access(self):
-        self.grant_access(
-            obj=self.test_document, permission=permission_document_check_out
-        )
-        resolved_link = self._resolve_checkout_link()
-        self.assertNotEqual(resolved_link, None)
-
-    def _resolve_checkout_info_link(self):
+    def _resolve_document_check_out_info_link(self):
         self.add_test_view(test_object=self.test_document)
         context = self.get_test_view()
         context['user'] = self._test_case_user
         return link_check_out_info.resolve(context=context)
 
-    def test_checkout_info_link_no_access(self):
-        resolved_link = self._resolve_checkout_info_link()
+    def test_document_check_out_link_no_access(self):
+        resolved_link = self._resolve_document_check_out_link()
         self.assertEqual(resolved_link, None)
 
-    def test_checkout_info_link_with_access(self):
+    def test_document_check_out_link_with_access(self):
+        self.grant_access(
+            obj=self.test_document, permission=permission_document_check_out
+        )
+        resolved_link = self._resolve_document_check_out_link()
+        self.assertNotEqual(resolved_link, None)
+
+    def test_document_check_out_info_link_no_access(self):
+        resolved_link = self._resolve_document_check_out_info_link()
+        self.assertEqual(resolved_link, None)
+
+    def test_document_check_out_info_link_with_access(self):
         self.grant_access(
             obj=self.test_document,
             permission=permission_document_check_out_detail_view
         )
-        resolved_link = self._resolve_checkout_info_link()
+        resolved_link = self._resolve_document_check_out_info_link()
         self.assertNotEqual(resolved_link, None)
