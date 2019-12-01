c3b45c3d61de6b921718ee71e49ebc9216ebd14f
Roberto Rosario <roberto.rosario@mayan-edms.com>
2019-11-30 21:47:51 -0400

    Add missing migrations
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

---
+++ mayan/apps/documents/migrations/0052_auto_20191130_2209.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('documents', '0051_documentpage_enabled'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='documentpage',
+            name='document_version',
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='version_pages', to='documents.DocumentVersion',
+                verbose_name='Document version'
+            ),
+        ),
+    ]
