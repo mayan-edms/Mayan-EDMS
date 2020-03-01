44aa1b3bd2ae4096b162d0ceb376c6f4600e32e4
Roberto Rosario <roberto.rosario@mayan-edms.com>
2020-03-01 03:16:13 -0400

    Bump API version to 4 to match major version
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/rest_api/schemas.py
+++ mayan/apps/rest_api/schemas.py
@@ -9,7 +9,7 @@ from mayan.apps.common.settings import setting_project_title
 
 openapi_info = openapi.Info(
     title=_('%s API') % setting_project_title.value,
-    default_version='v3',
+    default_version='v4',
     description=mayan.__description__,
     license=openapi.License(name=mayan.__license__),
 )
