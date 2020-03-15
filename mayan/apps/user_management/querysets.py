1c09660fb3ca5ebfaf3235aff695ec74d74b45c7
Roberto Rosario <roberto.rosario@mayan-edms.com>
2020-03-15 04:36:09 -0400

    Style cleanup
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/user_management/querysets.py
+++ mayan/apps/user_management/querysets.py
@@ -4,4 +4,6 @@ from django.contrib.auth import get_user_model
 
 
 def get_user_queryset():
-    return get_user_model().objects.filter(is_superuser=False, is_staff=False)
+    return get_user_model().objects.filter(
+        is_superuser=False, is_staff=False
+    )
