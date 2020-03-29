02aef60545fb8d49377aa209c1edd5a0daf1554d
Roberto Rosario <roberto.rosario@mayan-edms.com>
2020-03-28 20:33:32 -0400

    Update activiy stream version
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/events/dependencies.py
+++ mayan/apps/events/dependencies.py
@@ -31,5 +31,5 @@ PythonDependency(
         (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
         OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
     ''', module=__name__, name='django-activity-stream',
-    version_string='==0.7.0'
+    version_string='==0.8.0'
 )
