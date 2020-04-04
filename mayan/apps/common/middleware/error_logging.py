970ff42150c9168880965ab8a1488da506325d6f
Roberto Rosario <roberto.rosario@mayan-edms.com>
2020-04-04 03:12:26 -0400

    Add TESTING variable to silence log errors
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/common/middleware/error_logging.py
+++ mayan/apps/common/middleware/error_logging.py
@@ -2,6 +2,9 @@ from __future__ import unicode_literals
 
 import logging
 
+from django.conf import settings
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
 from django.utils.deprecation import MiddlewareMixin
 
 logger = logging.getLogger(name=__name__)
@@ -9,7 +12,9 @@ logger = logging.getLogger(name=__name__)
 
 class ErrorLoggingMiddleware(MiddlewareMixin):
     def process_exception(self, request, exception):
-        logger.exception(
-            'Exception caught by request middleware; %s, %s', request,
-            exception
-        )
+        if (settings.TESTING and not isinstance(exception, (PermissionDenied, Http404))) or not settings.TESTING:
+            # Don't log non critical exceptions in testing mode.
+            logger.exception(
+                'Exception caught by request middleware; %s, %s', request,
+                exception
+            )
