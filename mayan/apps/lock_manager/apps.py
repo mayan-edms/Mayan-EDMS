648defc05034530a029fc37f471540a9dfb7aba3
Roberto Rosario <roberto.rosario@mayan-edms.com>
2020-04-13 00:22:48 -0400

    Make storage and lock manager Mayan apps
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/lock_manager/apps.py
+++ mayan/apps/lock_manager/apps.py
@@ -1,8 +1,9 @@
-from django import apps
 from django.utils.translation import ugettext_lazy as _
 
+from mayan.apps.common.apps import MayanAppConfig
 
-class LockManagerApp(apps.AppConfig):
+
+class LockManagerApp(MayanAppConfig):
     has_tests = True
     name = 'mayan.apps.lock_manager'
     verbose_name = _('Lock manager')
