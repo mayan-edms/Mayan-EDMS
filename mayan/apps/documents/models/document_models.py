681db217f2b42cc0ea68371906ae2297067bf14a
Roberto Rosario <roberto.rosario@mayan-edms.com>
2020-04-13 04:22:29 -0400

    Add keyword argument
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/documents/models/document_models.py
+++ mayan/apps/documents/models/document_models.py
@@ -236,7 +236,7 @@ class Document(models.Model):
 
             if new_document:
                 if user:
-                    self.add_as_recent_document_for_user(user)
+                    self.add_as_recent_document_for_user(user=user)
                     event_document_create.commit(
                         actor=user, target=self, action_object=self.document_type
                     )
