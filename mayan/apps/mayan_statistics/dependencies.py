4427ca8921d75c5c6da042431f01ace0f60f7b52
Roberto Rosario <roberto.rosario@mayan-edms.com>
2020-05-22 04:02:15 -0400

    Style cleanup
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/mayan_statistics/dependencies.py
+++ mayan/apps/mayan_statistics/dependencies.py
@@ -1,5 +1,3 @@
-from django.utils.translation import ugettext_lazy as _
-
 from mayan.apps.dependencies.classes import (
     JavaScriptDependency, PythonDependency
 )
--- mayan/apps/ocr/migrations/0004_documenttypesettings.py
+++ mayan/apps/ocr/migrations/0004_documenttypesettings.py
@@ -30,7 +30,7 @@ class Migration(migrations.Migration):
             fields=[
                 (
                     'id', models.AutoField(
-                        auto_created=True, primary_key=True,serialize=False,
+                        auto_created=True, primary_key=True, serialize=False,
                         verbose_name='ID'
                     )
                 ),
