911720d0fe61063686beacfd25f6542a0b3f2250
Roberto Rosario <roberto.rosario@mayan-edms.com>
2020-06-14 02:39:23 -0400

    Fix django-debug-toolbar development setting
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/settings/development/ddt.py
+++ mayan/settings/development/ddt.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import, unicode_literals
-
 from .base import *  # NOQA
 
 # Stop debug toolbar patching!
@@ -11,6 +9,6 @@ if 'debug_toolbar' not in INSTALLED_APPS:  # NOQA: F405
         'debug_toolbar',
     )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-) + MIDDLEWARE_CLASSES  # NOQA: F405
+) + MIDDLEWARE  # NOQA: F405
