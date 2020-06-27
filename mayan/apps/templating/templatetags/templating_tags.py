847c2589676c07b0b8f14c15fbf549dd80fcb7d8
Roberto Rosario <roberto.rosario@mayan-edms.com>
2020-06-27 04:31:02 -0400

    Add dict_get and split templatetags
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/templating/templatetags/templating_tags.py
+++ mayan/apps/templating/templatetags/templating_tags.py
@@ -3,6 +3,14 @@ from django.template import Library
 register = Library()
 
 
+@register.filter
+def dict_get(dictionary, key):
+    """
+    Return the value for the given key or '' if not found.
+    """
+    return dictionary.get(key, '')
+
+
 @register.simple_tag(takes_context=True)
 def set(context, name, value):
     """
@@ -10,3 +18,11 @@ def set(context, name, value):
     """
     context[name] = value
     return ''
+
+
+@register.filter
+def split(obj, separator):
+    """
+    Return a list of the words in the string, using sep as the delimiter string.
+    """
+    return obj.split(separator)
