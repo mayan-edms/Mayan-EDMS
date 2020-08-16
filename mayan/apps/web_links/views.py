mayan-edms/Mayan-EDMS

commit 79ff84f7675ba0d78b1802b9f469fc67074433a0

Update more app to use explicit URL parameters
- Add additional tests.
- Increase test coverage.

Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

mayan/apps/web_links/views.py
@@ -36,7 +36,7 @@ class DocumentTypeWebLinksView(AddRemoveView):
     main_object_method_remove = 'web_link_remove'
     main_object_permission = permission_document_type_edit
     main_object_model = DocumentType
-    main_object_pk_url_kwarg = 'pk'
+    main_object_pk_url_kwarg = 'document_type_id'
     secondary_object_model = WebLink
     secondary_object_permission = permission_web_link_edit
     list_available_title = _('Available web links')
@@ -79,7 +79,7 @@ class DocumentTypeWebLinksView(AddRemoveView):
 
 class ResolvedWebLinkView(ExternalObjectMixin, RedirectView):
     external_object_class = Document
-    external_object_pk_url_kwarg = 'document_pk'
+    external_object_pk_url_kwarg = 'document_id'
     external_object_permission = permission_web_link_instance_view
 
     def get_redirect_url(self, *args, **kwargs):
@@ -93,7 +93,7 @@ class ResolvedWebLinkView(ExternalObjectMixin, RedirectView):
 
     def get_web_link(self):
         return get_object_or_404(
-            klass=self.get_web_link_queryset(), pk=self.kwargs['web_link_pk']
+            klass=self.get_web_link_queryset(), pk=self.kwargs['web_link_id']
         )
 
     def get_web_link_queryset(self):
@@ -111,7 +111,7 @@ class WebLinkDocumentTypesView(AddRemoveView):
     main_object_method_remove = 'document_types_remove'
     main_object_permission = permission_web_link_edit
     main_object_model = WebLink
-    main_object_pk_url_kwarg = 'pk'
+    main_object_pk_url_kwarg = 'web_link_id'
     secondary_object_model = DocumentType
     secondary_object_permission = permission_document_type_edit
     list_available_title = _('Available document types')
@@ -131,6 +131,7 @@ class WebLinkDocumentTypesView(AddRemoveView):
 
 
 class WebLinkListView(SingleObjectListView):
+    model = WebLink
     object_permission = permission_web_link_view
 
     def get_extra_context(self):
@@ -152,16 +153,11 @@ class WebLinkListView(SingleObjectListView):
             'title': _('Web links'),
         }
 
-    def get_source_queryset(self):
-        return self.get_web_link_queryset()
-
-    def get_web_link_queryset(self):
-        return WebLink.objects.all()
-
 
 class DocumentWebLinkListView(ExternalObjectMixin, WebLinkListView):
     external_object_class = Document
     external_object_permission = permission_web_link_instance_view
+    external_object_pk_url_kwarg = 'document_id'
     object_permission = permission_web_link_instance_view
 
     def get_extra_context(self):
@@ -203,14 +199,15 @@ class WebLinkCreateView(SingleObjectCreateView):
 class WebLinkDeleteView(SingleObjectDeleteView):
     model = WebLink
     object_permission = permission_web_link_delete
+    pk_url_kwarg = 'web_link_id'
     post_action_redirect = reverse_lazy(
         viewname='web_links:web_link_list'
     )
 
     def get_extra_context(self):
         return {
-            'object': self.get_object(),
-            'title': _('Delete web link: %s') % self.get_object()
+            'object': self.object,
+            'title': _('Delete web link: %s') % self.object
         }
 
 
@@ -218,14 +215,15 @@ class WebLinkEditView(SingleObjectEditView):
     form_class = WebLinkForm
     model = WebLink
     object_permission = permission_web_link_edit
+    pk_url_kwarg = 'web_link_id'
     post_action_redirect = reverse_lazy(
         viewname='web_links:web_link_list'
     )
 
     def get_extra_context(self):
         return {
-            'object': self.get_object(),
-            'title': _('Edit web link: %s') % self.get_object()
+            'object': self.object,
+            'title': _('Edit web link: %s') % self.object
         }
 
     def get_save_extra_data(self):

mayan/apps/dynamic_search/views.py
@@ -64,7 +64,7 @@ class SearchView(SearchModelMixin, SimpleView):
             'form': self.get_form(),
             'form_action': reverse(
                 viewname='search:results', kwargs={
-                    'search_model': self.search_model.get_full_name()
+                    'search_model_name': self.search_model.get_full_name()
                 }
             ),
             'search_model': self.search_model,

mayan/apps/events/views.py
@@ -123,7 +123,9 @@ class NotificationListView(SingleObjectListView):
 
 class NotificationMarkRead(SimpleView):
     def dispatch(self, *args, **kwargs):
-        self.get_queryset().filter(pk=self.kwargs['pk']).update(read=True)
+        self.get_queryset().filter(
+            pk=self.kwargs['notification_id']
+        ).update(read=True)
         return HttpResponseRedirect(
             redirect_to=reverse(viewname='events:user_notifications_list')
         )

mayan/apps/linking/views.py
@@ -13,6 +13,7 @@ from mayan.apps.common.generics import (
     AddRemoveView, SingleObjectCreateView, SingleObjectDeleteView,
     SingleObjectEditView, SingleObjectListView
 )
+from mayan.apps.common.mixins import ExternalObjectMixin
 from mayan.apps.documents.events import event_document_type_edited
 from mayan.apps.documents.models import Document, DocumentType
 from mayan.apps.documents.permissions import (
@@ -38,7 +39,7 @@ class DocumentTypeSmartLinksView(AddRemoveView):
     main_object_method_remove = 'smart_link_remove'
     main_object_permission = permission_document_type_edit
     main_object_model = DocumentType
-    main_object_pk_url_kwarg = 'pk'
+    main_object_pk_url_kwarg = 'document_type_id'
     secondary_object_model = SmartLink
     secondary_object_permission = permission_smart_link_edit
     list_available_title = _('Available smart links')
@@ -82,10 +83,10 @@ class DocumentTypeSmartLinksView(AddRemoveView):
 class ResolvedSmartLinkView(DocumentListView):
     def dispatch(self, request, *args, **kwargs):
         self.document = get_object_or_404(
-            klass=Document, pk=self.kwargs['document_pk']
+            klass=Document, pk=self.kwargs['document_id']
         )
         self.smart_link = get_object_or_404(
-            klass=SmartLink, pk=self.kwargs['smart_link_pk']
+            klass=SmartLink, pk=self.kwargs['smart_link_id']
         )
 
         AccessControlList.objects.check_access(
@@ -152,7 +153,7 @@ class SetupSmartLinkDocumentTypesView(AddRemoveView):
     main_object_method_remove = 'document_types_remove'
     main_object_permission = permission_smart_link_edit
     main_object_model = SmartLink
-    main_object_pk_url_kwarg = 'pk'
+    main_object_pk_url_kwarg = 'smart_link_id'
     secondary_object_model = DocumentType
     secondary_object_permission = permission_document_type_edit
     list_available_title = _('Available document types')
@@ -172,6 +173,7 @@ class SetupSmartLinkDocumentTypesView(AddRemoveView):
 
 
 class SmartLinkListView(SingleObjectListView):
+    model = SmartLink
     object_permission = permission_smart_link_view
 
     def get_extra_context(self):
@@ -195,29 +197,15 @@ class SmartLinkListView(SingleObjectListView):
             'title': _('Smart links'),
         }
 
-    def get_source_queryset(self):
-        return self.get_smart_link_queryset()
 
-    def get_smart_link_queryset(self):
-        return SmartLink.objects.all()
-
-
-class DocumentSmartLinkListView(SmartLinkListView):
-    def dispatch(self, request, *args, **kwargs):
-        self.document = get_object_or_404(klass=Document, pk=self.kwargs['pk'])
-
-        AccessControlList.objects.check_access(
-            obj=self.document, permissions=(permission_document_view,),
-            user=request.user
-        )
-
-        return super(
-            DocumentSmartLinkListView, self
-        ).dispatch(request, *args, **kwargs)
+class DocumentSmartLinkListView(ExternalObjectMixin, SmartLinkListView):
+    external_object_class = Document
+    external_object_permission = permission_document_view
+    external_object_pk_url_kwarg = 'document_id'
 
     def get_extra_context(self):
         return {
-            'document': self.document,
+            'document': self.external_object,
             'hide_link': True,
             'hide_object': True,
             'no_results_icon': icon_smart_link_setup,
@@ -229,12 +217,14 @@ class DocumentSmartLinkListView(SmartLinkListView):
             'no_results_title': _(
                 'There are no smart links for this document'
             ),
-            'object': self.document,
-            'title': _('Smart links for document: %s') % self.document,
+            'object': self.external_object,
+            'title': _('Smart links for document: %s') % self.external_object,
         }
 
     def get_smart_link_queryset(self):
-        return ResolvedSmartLink.objects.get_for(document=self.document)
+        return ResolvedSmartLink.objects.get_for(
+            document=self.external_object
+        )
 
 
 class SmartLinkCreateView(SingleObjectCreateView):
@@ -252,6 +242,7 @@ class SmartLinkCreateView(SingleObjectCreateView):
 class SmartLinkDeleteView(SingleObjectDeleteView):
     model = SmartLink
     object_permission = permission_smart_link_delete
+    pk_url_kwarg = 'smart_link_id'
     post_action_redirect = reverse_lazy(
         viewname='linking:smart_link_list'
     )
@@ -267,6 +258,7 @@ class SmartLinkEditView(SingleObjectEditView):
     form_class = SmartLinkForm
     model = SmartLink
     object_permission = permission_smart_link_edit
+    pk_url_kwarg = 'smart_link_id'
     post_action_redirect = reverse_lazy(
         viewname='linking:smart_link_list'
     )
@@ -281,8 +273,10 @@ class SmartLinkEditView(SingleObjectEditView):
         return {'_user': self.request.user}
 
 
-class SmartLinkConditionListView(SingleObjectListView):
-    object_permission = permission_smart_link_edit
+class SmartLinkConditionListView(ExternalObjectMixin, SingleObjectListView):
+    external_object_class = SmartLink
+    external_object_permission = permission_smart_link_edit
+    external_object_pk_url_kwarg = 'smart_link_id'
 
     def get_extra_context(self):
         return {
@@ -292,7 +286,7 @@ class SmartLinkConditionListView(SingleObjectListView):
             'no_results_main_link': link_smart_link_condition_create.resolve(
                 context=RequestContext(
                     request=self.request, dict_={
-                        'object': self.get_smart_link()
+                        'object': self.external_object
                     }
                 )
             ),
@@ -303,114 +297,86 @@ class SmartLinkConditionListView(SingleObjectListView):
             'no_results_title': _(
                 'There are no conditions for this smart link'
             ),
-            'object': self.get_smart_link(),
+            'object': self.external_object,
             'title': _(
                 'Conditions for smart link: %s'
-            ) % self.get_smart_link(),
+            ) % self.external_object,
         }
 
     def get_source_queryset(self):
-        return self.get_smart_link().conditions.all()
-
-    def get_smart_link(self):
-        return get_object_or_404(klass=SmartLink, pk=self.kwargs['pk'])
+        return self.external_object.conditions.all()
 
 
-class SmartLinkConditionCreateView(SingleObjectCreateView):
+class SmartLinkConditionCreateView(
+    ExternalObjectMixin, SingleObjectCreateView
+):
+    external_object_class = SmartLink
+    external_object_permission = permission_smart_link_edit
+    external_object_pk_url_kwarg = 'smart_link_id'
     form_class = SmartLinkConditionForm
-
-    def dispatch(self, request, *args, **kwargs):
-        AccessControlList.objects.check_access(
-            obj=self.get_smart_link(),
-            permissions=(permission_smart_link_edit,),
-            user=request.user
-        )
-
-        return super(
-            SmartLinkConditionCreateView, self
-        ).dispatch(request, *args, **kwargs)
 
     def get_extra_context(self):
         return {
             'title': _(
                 'Add new conditions to smart link: "%s"'
-            ) % self.get_smart_link(),
-            'object': self.get_smart_link(),
+            ) % self.external_object,
+            'object': self.external_object,
         }
 
     def get_instance_extra_data(self):
-        return {'smart_link': self.get_smart_link()}
+        return {'smart_link': self.external_object}
 
     def get_post_action_redirect(self):
         return reverse(
             viewname='linking:smart_link_condition_list', kwargs={
-                'pk': self.get_smart_link().pk
+                'smart_link_id': self.external_object.pk
             }
         )
 
     def get_queryset(self):
-        return self.get_smart_link().conditions.all()
+        return self.external_object.conditions.all()
 
-    def get_smart_link(self):
-        return get_object_or_404(klass=SmartLink, pk=self.kwargs['pk'])
+
+class SmartLinkConditionDeleteView(SingleObjectDeleteView):
+    model = SmartLinkCondition
+    object_permission = permission_smart_link_edit
+    pk_url_kwarg = 'smart_link_condition_id'
+
+    def get_extra_context(self):
+        return {
+            'condition': self.object,
+            'navigation_object_list': ('object', 'condition'),
+            'object': self.object.smart_link,
+            'title': _(
+                'Delete smart link condition: "%s"?'
+            ) % self.object,
+        }
+
+    def get_post_action_redirect(self):
+        return reverse(
+            viewname='linking:smart_link_condition_list', kwargs={
+                'smart_link_id': self.object.smart_link.pk
+            }
+        )
 
 
 class SmartLinkConditionEditView(SingleObjectEditView):
     form_class = SmartLinkConditionForm
     model = SmartLinkCondition
-
-    def dispatch(self, request, *args, **kwargs):
-        AccessControlList.objects.check_access(
-            obj=self.get_object().smart_link,
-            permissions=(permission_smart_link_edit,), user=request.user
-        )
-
-        return super(
-            SmartLinkConditionEditView, self
-        ).dispatch(request, *args, **kwargs)
+    object_permission = permission_smart_link_edit
+    pk_url_kwarg = 'smart_link_condition_id'
 
     def get_extra_context(self):
         return {
-            'condition': self.get_object(),
+            'condition': self.object,
             'navigation_object_list': ('object', 'condition'),
-            'object': self.get_object().smart_link,
+            'object': self.object.smart_link,
             'title': _('Edit smart link condition'),
         }
 
     def get_post_action_redirect(self):
         return reverse(
             viewname='linking:smart_link_condition_list', kwargs={
-                'pk': self.get_object().smart_link.pk
-            }
-        )
-
-
-class SmartLinkConditionDeleteView(SingleObjectDeleteView):
-    model = SmartLinkCondition
-
-    def dispatch(self, request, *args, **kwargs):
-        AccessControlList.objects.check_access(
-            obj=self.get_object().smart_link,
-            permissions=(permission_smart_link_edit,), user=request.user
-        )
-
-        return super(
-            SmartLinkConditionDeleteView, self
-        ).dispatch(request, *args, **kwargs)
-
-    def get_extra_context(self):
-        return {
-            'condition': self.get_object(),
-            'navigation_object_list': ('object', 'condition'),
-            'object': self.get_object().smart_link,
-            'title': _(
-                'Delete smart link condition: "%s"?'
-            ) % self.get_object(),
-        }
-
-    def get_post_action_redirect(self):
-        return reverse(
-            viewname='linking:smart_link_condition_list', kwargs={
-                'pk': self.get_object().smart_link.pk
+                'smart_link_id': self.object.smart_link.pk
             }
         )

mayan/apps/mailer/views.py
@@ -1,6 +1,5 @@
 from django.contrib import messages
 from django.http import Http404, HttpResponseRedirect
-from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 from django.urls import reverse, reverse_lazy
 from django.utils.translation import ungettext, ugettext_lazy as _
@@ -11,6 +10,7 @@ from mayan.apps.common.generics import (
     SingleObjectDynamicFormCreateView, SingleObjectDynamicFormEditView,
     SingleObjectListView
 )
+from mayan.apps.common.mixins import ExternalObjectMixin
 from mayan.apps.documents.models import Document
 
 from .classes import MailerBackend
@@ -44,7 +44,7 @@ class MailDocumentView(MultipleObjectFormActionView):
     form_class = DocumentMailForm
     model = Document
     object_permission = permission_mailing_send_document
-
+    pk_url_kwarg = 'document_id'
     success_message = _('%(count)d document queued for email delivery')
     success_message_plural = _(
         '%(count)d documents queued for email delivery'
@@ -169,6 +169,7 @@ class UserMailingCreateView(SingleObjectDynamicFormCreateView):
 class UserMailingDeleteView(SingleObjectDeleteView):
     model = UserMailer
     object_permission = permission_user_mailer_delete
+    pk_url_kwarg = 'mailer_id'
     post_action_redirect = reverse_lazy(viewname='mailer:user_mailer_list')
 
     def get_extra_context(self):
@@ -181,6 +182,7 @@ class UserMailingEditView(SingleObjectDynamicFormEditView):
     form_class = UserMailerDynamicForm
     model = UserMailer
     object_permission = permission_user_mailer_edit
+    pk_url_kwarg = 'mailer_id'
 
     def get_extra_context(self):
         return {
@@ -199,22 +201,20 @@ class UserMailingEditView(SingleObjectDynamicFormEditView):
         return result
 
 
-class UserMailerLogEntryListView(SingleObjectListView):
-    model = LogEntry
-    view_permission = permission_user_mailer_view
+class UserMailerLogEntryListView(ExternalObjectMixin, SingleObjectListView):
+    external_object_class = UserMailer
+    external_object_permission = permission_user_mailer_view
+    external_object_pk_url_kwarg = 'mailer_id'
 
     def get_extra_context(self):
         return {
             'hide_object': True,
-            'object': self.get_user_mailer(),
-            'title': _('Error log for: %s') % self.get_user_mailer(),
+            'object': self.external_object,
+            'title': _('Error log for: %s') % self.external_object,
         }
 
     def get_source_queryset(self):
-        return self.get_user_mailer().error_log.all()
-
-    def get_user_mailer(self):
-        return get_object_or_404(klass=UserMailer, pk=self.kwargs['pk'])
+        return self.external_object.error_log.all()
 
 
 class UserMailerListView(SingleObjectListView):
@@ -241,12 +241,14 @@ class UserMailerListView(SingleObjectListView):
         return {'fields': self.get_backend().fields}
 
 
-class UserMailerTestView(FormView):
+class UserMailerTestView(ExternalObjectMixin, FormView):
+    external_object_class = UserMailer
+    external_object_permission = permission_user_mailer_use
+    external_object_pk_url_kwarg = 'mailer_id'
     form_class = UserMailerTestForm
-    object_permission = permission_user_mailer_edit
 
     def form_valid(self, form):
-        self.get_object().test(to=form.cleaned_data['email'])
+        self.external_object.test(to=form.cleaned_data['email'])
         messages.success(
             message=_('Test email sent.'), request=self.request
         )
@@ -255,16 +257,7 @@ class UserMailerTestView(FormView):
     def get_extra_context(self):
         return {
             'hide_object': True,
-            'object': self.get_object(),
+            'object': self.external_object,
             'submit_label': _('Test'),
-            'title': _('Test mailing profile: %s') % self.get_object(),
+            'title': _('Test mailing profile: %s') % self.external_object,
         }
-
-    def get_object(self):
-        user_mailer = get_object_or_404(klass=UserMailer, pk=self.kwargs['pk'])
-        AccessControlList.objects.check_access(
-            obj=user_mailer, permissions=(permission_user_mailer_use,),
-            user=self.request.user
-        )
-
-        return user_mailer

mayan/apps/metadata/views.py
@@ -3,8 +3,6 @@ from furl import furl
 from django.conf import settings
 from django.contrib import messages
 from django.core.exceptions import ValidationError
-from django.db.models import Count
-from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 from django.urls import reverse, reverse_lazy
@@ -37,6 +35,7 @@ from .links import (
     link_metadata_add, link_metadata_multiple_add,
     link_setup_metadata_type_create
 )
+from .mixins import DocumentMetadataSameTypeMixin
 from .models import DocumentMetadata, MetadataType
 from .permissions import (
     permission_document_metadata_add, permission_document_metadata_edit,
@@ -46,34 +45,13 @@ from .permissions import (
 )
 
 
-class DocumentMetadataSameTypeMixin(object):
-    def dispatch(self, request, *args, **kwargs):
-        result = super(DocumentMetadataSameTypeMixin, self).dispatch(
-            request, *args, **kwargs
-        )
-
-        queryset = self.get_object_list()
-
-        for document in queryset:
-            document.add_as_recent_document_for_user(user=request.user)
-
-        if queryset.values('document_type').distinct().aggregate(Count('document_type'))['document_type__count'] > 1:
-            messages.error(
-                message=_(
-                    'Selected documents must be of the same type.'
-                ), request=request
-            )
-            return HttpResponseRedirect(redirect_to=self.previous_url)
-
-        return result
-
-
 class DocumentMetadataAddView(
     DocumentMetadataSameTypeMixin, MultipleObjectFormActionView
 ):
     form_class = DocumentMetadataAddForm
     model = Document
     object_permission = permission_document_metadata_add
+    pk_url_kwarg = 'document_id'
     success_message = _('Metadata add request performed on %(count)d document')
     success_message_plural = _(
         'Metadata add request performed on %(count)d documents'
@@ -134,8 +112,9 @@ class DocumentMetadataAddView(
     def get_post_object_action_url(self):
         if self.action_count == 1:
             return reverse(
-                viewname='metadata:metadata_edit',
-                kwargs={'pk': self.action_id_list[0]}
+                viewname='metadata:metadata_edit', kwargs={
+                    'document_id': self.action_id_list[0]
+                }
             )
 
         elif self.action_count > 1:
@@ -214,6 +193,7 @@ class DocumentMetadataEditView(
     form_class = DocumentMetadataFormSet
     model = Document
     object_permission = permission_document_metadata_edit
+    pk_url_kwarg = 'document_id'
     success_message = _(
         'Metadata edit request performed on %(count)d document'
     )
@@ -226,8 +206,7 @@ class DocumentMetadataEditView(
 
         id_list = ','.join(
             map(
-                force_text,
-                queryset.values_list('pk', flat=True)
+                force_text, queryset.values_list('pk', flat=True)
             )
         )
 
@@ -309,8 +288,9 @@ class DocumentMetadataEditView(
     def get_post_object_action_url(self):
         if self.action_count == 1:
             return reverse(
-                viewname='metadata:metadata_view',
-                kwargs={'pk': self.action_id_list[0]}
+                viewname='metadata:metadata_view', kwargs={
+                    'document_id': self.action_id_list[0]
+                }
             )
         elif self.action_count > 1:
             url = furl(
@@ -370,7 +350,7 @@ class DocumentMetadataEditView(
 class DocumentMetadataListView(ExternalObjectMixin, SingleObjectListView):
     external_object_class = Document
     external_object_permission = permission_document_metadata_view
-    external_object_pk_url_kwarg = 'pk'
+    external_object_pk_url_kwarg = 'document_id'
     object_permission = permission_document_metadata_view
 
     def get_extra_context(self):
@@ -406,6 +386,7 @@ class DocumentMetadataRemoveView(
     form_class = DocumentMetadataRemoveFormSet
     model = Document
     object_permission = permission_document_metadata_remove
+    pk_url_kwarg = 'document_id'
     success_message = _(
         'Metadata remove request performed on %(count)d document'
     )
@@ -474,8 +455,9 @@ class DocumentMetadataRemoveView(
     def get_post_object_action_url(self):
         if self.action_count == 1:
             return reverse(
-                viewname='metadata:metadata_view',
-                kwargs={'pk': self.action_id_list[0]}
+                viewname='metadata:metadata_view', kwargs={
+                    'document_id': self.action_id_list[0]
+                }
             )
 
     def object_action(self, form, instance):
@@ -533,6 +515,7 @@ class MetadataTypeCreateView(SingleObjectCreateView):
 class MetadataTypeDeleteView(SingleObjectDeleteView):
     model = MetadataType
     object_permission = permission_metadata_type_delete
+    pk_url_kwarg = 'metadata_type_id'
     post_action_redirect = reverse_lazy(
         viewname='metadata:setup_metadata_type_list'
     )
@@ -540,8 +523,8 @@ class MetadataTypeDeleteView(SingleObjectDeleteView):
     def get_extra_context(self):
         return {
             'delete_view': True,
-            'object': self.get_object(),
-            'title': _('Delete the metadata type: %s?') % self.get_object(),
+            'object': self.object,
+            'title': _('Delete the metadata type: %s?') % self.object,
         }
 
 
@@ -549,14 +532,15 @@ class MetadataTypeEditView(SingleObjectEditView):
     form_class = MetadataTypeForm
     model = MetadataType
     object_permission = permission_metadata_type_edit
+    pk_url_kwarg = 'metadata_type_id'
     post_action_redirect = reverse_lazy(
         viewname='metadata:setup_metadata_type_list'
     )
 
     def get_extra_context(self):
         return {
-            'object': self.get_object(),
-            'title': _('Edit metadata type: %s') % self.get_object(),
+            'object': self.object,
+            'title': _('Edit metadata type: %s') % self.object,
         }
 
     def get_save_extra_data(self):
@@ -566,6 +550,7 @@ class MetadataTypeEditView(SingleObjectEditView):
 
 
 class MetadataTypeListView(SingleObjectListView):
+    model = MetadataType
     object_permission = permission_metadata_type_view
 
     def get_extra_context(self):
@@ -588,14 +573,12 @@ class MetadataTypeListView(SingleObjectListView):
             'title': _('Metadata types'),
         }
 
-    def get_source_queryset(self):
-        return MetadataType.objects.all()
-
 
 class SetupDocumentTypeMetadataTypes(FormView):
     form_class = DocumentTypeMetadataTypeRelationshipFormSet
     main_model = 'document_type'
     model = DocumentType
+    pk_url_kwarg = 'document_id'
     submodel = MetadataType
 
     def form_valid(self, form):
@@ -654,7 +637,9 @@ class SetupDocumentTypeMetadataTypes(FormView):
         return initial
 
     def get_object(self):
-        obj = get_object_or_404(klass=self.model, pk=self.kwargs['pk'])
+        obj = get_object_or_404(
+            klass=self.model, pk=self.kwargs[self.pk_url_kwarg]
+        )
 
         AccessControlList.objects.check_access(
             obj=obj, permissions=(permission_metadata_type_edit,),
@@ -676,6 +661,7 @@ class SetupDocumentTypeMetadataTypes(FormView):
 class SetupMetadataTypesDocumentTypes(SetupDocumentTypeMetadataTypes):
     main_model = 'metadata_type'
     model = MetadataType
+    pk_url_kwarg = 'metadata_type_id'
     submodel = DocumentType
 
     def get_extra_context(self):

commit fd0927f941b0a50a801cfcc7f87b57fea10ee8b6

Fix issue with resolved web links
Reverse resolved web link issue introduced by the commit
79ff84f7675ba0d78b1802b9f469fc67074433a0. Thanks for forum
user @qra for the report.

Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

mayan/apps/web_links/views.py
@@ -131,7 +131,6 @@ class WebLinkDocumentTypesView(AddRemoveView):
 
 
 class WebLinkListView(SingleObjectListView):
-    model = WebLink
     object_permission = permission_web_link_view
 
     def get_extra_context(self):
@@ -152,6 +151,12 @@ class WebLinkListView(SingleObjectListView):
             ),
             'title': _('Web links'),
         }
+
+    def get_source_queryset(self):
+        return self.get_web_link_queryset()
+
+    def get_web_link_queryset(self):
+        return WebLink.objects.all()
 
 
 class DocumentWebLinkListView(ExternalObjectMixin, WebLinkListView):