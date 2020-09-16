1101b1e300d53f3a08408d0dacf219ed0126e423
Roberto Rosario <roberto.rosario@mayan-edms.com>
2020-09-16 05:31:02 -0400

    Set default value of Match all to False
    Preserve the current behavior.
    
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/dynamic_search/literals.py
+++ mayan/apps/dynamic_search/literals.py
@@ -1,5 +1,5 @@
 DEFAULT_RESULTS_LIMIT = 100
-DEFAULT_SEARCH_MATCH_ALL_DEFAULT_VALUE = 'true'
+DEFAULT_SEARCH_MATCH_ALL_DEFAULT_VALUE = 'false'
 # Duplicated to keep API compatible until version 4.0
 # Merge these two literals and mixins on version 4.0
 SEARCH_MODEL_NAME_API_KWARG = 'search_model'
