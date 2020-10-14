2eaf6d2eb2196621b015de322b420b784dad1d3e
Roberto Rosario <roberto.rosario@mayan-edms.com>
2020-10-14 09:52:03 -0400

    Merge search literals
    Merge SEARCH_MODEL_NAME_KWARG and SEARCH_MODEL_NAME_API_KWARG.
    
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/dynamic_search/literals.py
+++ mayan/apps/dynamic_search/literals.py
@@ -1,7 +1,4 @@
 DEFAULT_RESULTS_LIMIT = 100
 DEFAULT_SEARCH_MATCH_ALL_DEFAULT_VALUE = 'false'
-# Duplicated to keep API compatible until version 4.0
-# Merge these two literals and mixins on version 4.0
-SEARCH_MODEL_NAME_API_KWARG = 'search_model'
 SEARCH_MODEL_NAME_KWARG = 'search_model_name'
 TASK_RETRY_DELAY = 5
--- mayan/apps/dynamic_search/mixins.py
+++ mayan/apps/dynamic_search/mixins.py
@@ -2,7 +2,7 @@ from django.http import Http404
 from django.utils.encoding import force_text
 
 from .classes import SearchModel
-from .literals import SEARCH_MODEL_NAME_API_KWARG, SEARCH_MODEL_NAME_KWARG
+from .literals import SEARCH_MODEL_NAME_KWARG
 
 
 class SearchModelMixin:
@@ -25,9 +25,9 @@ class SearchModelMixin:
 class SearchModelAPIMixin(SearchModelMixin):
     def get_search_model_name(self):
         return self.kwargs.get(
-            SEARCH_MODEL_NAME_API_KWARG, self.request.GET.get(
-                '_{}'.format(SEARCH_MODEL_NAME_API_KWARG), self.request.POST.get(
-                    '_{}'.format(SEARCH_MODEL_NAME_API_KWARG)
+            SEARCH_MODEL_NAME_KWARG, self.request.GET.get(
+                '_{}'.format(SEARCH_MODEL_NAME_KWARG), self.request.POST.get(
+                    '_{}'.format(SEARCH_MODEL_NAME_KWARG)
                 )
             )
         )
