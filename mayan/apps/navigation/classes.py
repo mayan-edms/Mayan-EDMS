4e6b56ecb033baaa8d275c003f39a5a135d16cb9
Roberto Rosario <roberto.rosario@mayan-edms.com>
2020-10-14 03:35:16 -0400

    Fix typo
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/navigation/classes.py
+++ mayan/apps/navigation/classes.py
@@ -744,7 +744,7 @@ class SourceColumn(object):
                 )
             except FieldDoesNotExist as exception:
                 raise ImproperlyConfigured(
-                    '"{}" is not a field or "{}", cannot be used as a '
+                    '"{}" is not a field of "{}", cannot be used as a '
                     'sortable column.'.format(field_name, self.source)
                 ) from exception
 
