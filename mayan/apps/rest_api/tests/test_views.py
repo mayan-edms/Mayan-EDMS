6d37d80806c065272f6b3c518eb5beb2b1f31010
Roberto Rosario <roberto.rosario@mayan-edms.com>
2020-10-23 10:46:53 -0400

    Add REST API view tests
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

---
+++ mayan/apps/rest_api/tests/test_views.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from mayan.apps.common.tests.base import GenericViewTestCase
+
+from .mixins import RESTAPIViewTestMixin
+
+
+class RESTAPIViewTestCase(RESTAPIViewTestMixin, GenericViewTestCase):
+    def test_browser_api_view(self):
+        response = self._request_test_browser_api_view()
+        self.assertEqual(response.status_code, 200)
+
+    def test_redoc_ui_view(self):
+        response = self._request_test_redoc_ui_view()
+        self.assertEqual(response.status_code, 200)
+
+    def test_swagger_ui_view(self):
+        response = self._request_test_swagger_ui_view()
+        self.assertEqual(response.status_code, 200)
+
+    def test_swagger_no_ui_json_view(self):
+        self.expected_content_types = ('application/json',)
+
+        response = self._request_test_swagger_no_ui_json_view()
+        self.assertEqual(response.status_code, 200)
+
+    def test_swagger_no_ui_yaml_view(self):
+        self.expected_content_types = ('application/yaml',)
+
+        response = self._request_test_swagger_no_ui_yaml_view()
+        self.assertEqual(response.status_code, 200)
