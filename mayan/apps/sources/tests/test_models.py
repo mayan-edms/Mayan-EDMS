mayan-edms/Mayan-EDMS

commit 865ae60fcef38e07bbf6d09bd6032017e3603698

Remove transactions when creating documents
This allows document stubs to be accessible from within
signal handlers.

Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

mayan/apps/documents/models/document_type_models.py
@@ -134,16 +134,12 @@ class DocumentType(models.Model):
         )
 
         try:
-            with transaction.atomic():
-                document = Document(
-                    description=description or '', document_type=self,
-                    label=label or file_object.name,
-                    language=language or setting_language.value
-                )
-                document.save(_user=_user)
-
-                document.new_version(file_object=file_object, _user=_user)
-                return document
+            document = Document(
+                description=description or '', document_type=self,
+                label=label or file_object.name,
+                language=language or setting_language.value
+            )
+            document.save(_user=_user)
         except Exception as exception:
             logger.critical(
                 'Unexpected exception while trying to create new document '
@@ -151,6 +147,18 @@ class DocumentType(models.Model):
                 label or file_object.name, self, exception
             )
             raise
+        else:
+            try:
+                document.new_version(file_object=file_object, _user=_user)
+            except Exception as exception:
+                logger.critical(
+                    'Unexpected exception while trying to create initial '
+                    'version for document %s; %s',
+                    label or file_object.name, exception
+                )
+                raise
+            else:
+                return document
 
     def save(self, *args, **kwargs):
         user = kwargs.pop('_user', None)

mayan/apps/documents/models/document_models.py
@@ -261,26 +261,26 @@ class Document(HooksMixin, models.Model):
         user = kwargs.pop('_user', None)
         _commit_events = kwargs.pop('_commit_events', True)
         new_document = not self.pk
-        with transaction.atomic():
-            signal_mayan_pre_save.send(
-                sender=Document, instance=self, user=user
-            )
 
-            super(Document, self).save(*args, **kwargs)
+        signal_mayan_pre_save.send(
+            sender=Document, instance=self, user=user
+        )
 
-            if new_document:
-                if user:
-                    self.add_as_recent_document_for_user(user=user)
-                    event_document_create.commit(
-                        actor=user, target=self, action_object=self.document_type
-                    )
-                else:
-                    event_document_create.commit(
-                        target=self, action_object=self.document_type
-                    )
+        super().save(*args, **kwargs)
+
+        if new_document:
+            if user:
+                self.add_as_recent_document_for_user(user=user)
+                event_document_create.commit(
+                    actor=user, target=self, action_object=self.document_type
+                )
             else:
-                if _commit_events:
-                    event_document_properties_edit.commit(actor=user, target=self)
+                event_document_create.commit(
+                    target=self, action_object=self.document_type
+                )
+        else:
+            if _commit_events:
+                event_document_properties_edit.commit(actor=user, target=self)
 
     def save_to_file(self, *args, **kwargs):
         return self.latest_version.save_to_file(*args, **kwargs)

commit 992524dc5984d91ebce15ab168ce0288c6808291

Fix double document version creation
Fix an issue introduced in 865ae60fcef38e07bbf6d09bd6032017e3603698 when
support for adding document stubs in signal handlers was added.

This issue causes new documents to contain two versions instead of one.
The only consequence of this issue is extra storage usage, no data loss
occurs. The extra document version can be safely deleted by using the
document version revert feature. Doing this will delete the extra
document version from the database and from the storage.

Closes GitLab issue #895. Thanks to Lukas Auer (@lukasauer) for the
report and initial investigation.

Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

mayan/apps/documents/models/document_type_models.py
@@ -149,7 +149,9 @@ class DocumentType(models.Model):
             raise
         else:
             try:
-                document.new_version(file_object=file_object, _user=_user)
+                document_version = document.new_version(
+                    file_object=file_object, _user=_user
+                )
             except Exception as exception:
                 logger.critical(
                     'Unexpected exception while trying to create initial '
@@ -158,7 +160,7 @@ class DocumentType(models.Model):
                 )
                 raise
             else:
-                return document
+                return document, document_version
 
     def save(self, *args, **kwargs):
         user = kwargs.pop('_user', None)

mayan/apps/sources/models/base.py
@@ -105,7 +105,7 @@ class Source(models.Model):
         Upload an individual document
         """
         try:
-            document = document_type.new_document(
+            document, document_version = document_type.new_document(
                 description=description, file_object=file_object, label=label,
                 language=language, _user=user
             )
@@ -118,10 +118,6 @@ class Source(models.Model):
             raise
         else:
             try:
-                document_version = document.new_version(
-                    file_object=file_object, _user=user,
-                )
-
                 if user:
                     document.add_as_recent_document_for_user(user=user)
 

mayan/apps/sources/tests/test_models.py
@@ -12,6 +12,7 @@ from django_celery_beat.models import PeriodicTask
 
 from mayan.apps.common.serialization import yaml_dump
 from mayan.apps.documents.models import Document
+from mayan.apps.documents.storages import storage_document_versions
 from mayan.apps.documents.tests.base import GenericDocumentTestCase
 from mayan.apps.documents.tests.literals import (
     TEST_COMPRESSED_DOCUMENT_PATH, TEST_NON_ASCII_DOCUMENT_FILENAME,
@@ -438,3 +439,30 @@ class WatchFolderTestCase(WatchFolderTestMixin, GenericDocumentTestCase):
             process.join()
 
             self.assertEqual(Document.objects.count(), 0)
+
+
+class SourceModelTestCase(SourceTestMixin, GenericDocumentTestCase):
+    auto_upload_test_document = False
+
+    def setUp(self):
+        super().setUp()
+        self.document_version_storage_instance = storage_document_versions.get_storage_instance()
+
+    def _get_document_version_storage_file_count(self):
+        return len(
+            self.document_version_storage_instance.listdir(path='')[1]
+        )
+
+    def test_single_storage_file_creation(self):
+        document_version_file_count = self._get_document_version_storage_file_count()
+
+        with open(TEST_SMALL_DOCUMENT_PATH, mode='rb') as file_object:
+            self.test_source.handle_upload(
+                file_object=file_object,
+                document_type=self.test_document_type
+            )
+
+        self.assertEqual(
+            self._get_document_version_storage_file_count(),
+            document_version_file_count + 1
+        )