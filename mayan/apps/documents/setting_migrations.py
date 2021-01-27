39311dfe469066fc341f7f646dde6b3f3098aa9d
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-01-27 04:56:59 -0400

    Add document file and version setting migrations
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/documents/setting_migrations.py
+++ mayan/apps/documents/setting_migrations.py
@@ -4,18 +4,53 @@ from mayan.apps.smart_settings.utils import smart_yaml_load
 
 class DocumentsSettingMigration(SettingNamespaceMigration):
     """
-    0001 to 0002: backend arguments are no longer quoted but YAML valid
+    0001 to 0002: Backend arguments are no longer quoted but YAML valid
                   too. Changed in version 3.3.
     0002 to 0003: Setting DOCUMENTS_RECENT_ADDED_COUNT renamed to
                   DOCUMENTS_RECENTLY_CREATED_COUNT,
                   DOCUMENTS_RECENT_ADDED_COUNT renamed to
                   DOCUMENTS_RECENTLY_CREATED_COUNT. Changed in version 4.0.
+    0003 to 0004: New settings for document file storage, file page image
+                  cache and version page image cache added and made to take
+                  their initial settings from existing
+                  DOCUMENTS_CACHE_STORAGE_BACKEND,
+                  DOCUMENTS_CACHE_STORAGE_BACKEND_ARGUMENTS,
+                  DOCUMENTS_STORAGE_BACKEND, and
+                  DOCUMENTS_STORAGE_BACKEND_ARGUMENTS settings.
     """
     def documents_cache_storage_backend_arguments_0001(self, value):
         return smart_yaml_load(value=value)
 
     def documents_storage_backend_arguments_0001(self, value):
         return smart_yaml_load(value=value)
+
+    def documents_file_page_image_cache_storage_backend_0003(self, value):
+        # Get the setting by its new global name
+        setting = Setting.get(global_name='DOCUMENTS_FILE_PAGE_IMAGE_CACHE_STORAGE_BACKEND')
+        # Load the value from the setting's old global name
+        setting.cache_value(global_name='DOCUMENTS_CACHE_STORAGE_BACKEND')
+        return setting.value
+
+    def documents_file_page_image_cache_storage_backend_arguments_0003(self, value):
+        # Get the setting by its new global name
+        setting = Setting.get(global_name='DOCUMENTS_FILE_PAGE_IMAGE_CACHE_STORAGE_BACKEND_ARGUMENTS')
+        # Load the value from the setting's old global name
+        setting.cache_value(global_name='DOCUMENTS_CACHE_STORAGE_BACKEND_ARGUMENTS')
+        return setting.value
+
+    def documents_file_storage_backend_0003(self, value):
+        # Get the setting by its new global name
+        setting = Setting.get(global_name='DOCUMENTS_FILE_STORAGE_BACKEND')
+        # Load the value from the setting's old global name
+        setting.cache_value(global_name='DOCUMENTS_STORAGE_BACKEND')
+        return setting.value
+
+    def documents_file_storage_backend_arguments_0003(self, value):
+        # Get the setting by its new global name
+        setting = Setting.get(global_name='DOCUMENTS_FILE_STORAGE_BACKEND_ARGUMENTS')
+        # Load the value from the setting's old global name
+        setting.cache_value(global_name='DOCUMENTS_STORAGE_BACKEND_ARGUMENTS')
+        return setting.value
 
     def documents_recently_accessed_count_0002(self, value):
         # Get the setting by its new global name
@@ -29,4 +64,18 @@ class DocumentsSettingMigration(SettingNamespaceMigration):
         setting = Setting.get(global_name='DOCUMENTS_RECENTLY_CREATED_COUNT')
         # Load the value from the setting's old global name
         setting.cache_value(global_name='DOCUMENTS_RECENT_ADDED_COUNT')
+        return setting.value
+
+    def documents_version_page_image_cache_storage_backend_0003(self, value):
+        # Get the setting by its new global name
+        setting = Setting.get(global_name='DOCUMENTS_VERSION_PAGE_IMAGE_CACHE_STORAGE_BACKEND')
+        # Load the value from the setting's old global name
+        setting.cache_value(global_name='DOCUMENTS_CACHE_STORAGE_BACKEND')
+        return setting.value
+
+    def documents_version_page_image_cache_storage_backend_arguments_0003(self, value):
+        # Get the setting by its new global name
+        setting = Setting.get(global_name='DOCUMENTS_VERSION_PAGE_IMAGE_CACHE_STORAGE_BACKEND_ARGUMENTS')
+        # Load the value from the setting's old global name
+        setting.cache_value(global_name='DOCUMENTS_CACHE_STORAGE_BACKEND_ARGUMENTS')
         return setting.value
--- mayan/apps/documents/tests/test_setting_migrations.py
+++ mayan/apps/documents/tests/test_setting_migrations.py
@@ -1,69 +1,99 @@
 from django.test import override_settings
 
-from mayan.apps.smart_settings.classes import Setting
 from mayan.apps.smart_settings.tests.mixins import SmartSettingTestMixin
 from mayan.apps.testing.tests.base import BaseTestCase
 
 from ..settings import (
+    setting_document_file_page_image_cache_storage,
     setting_document_file_page_image_cache_storage_arguments,
+    setting_document_file_storage_backend,
     setting_document_file_storage_backend_arguments,
+    setting_documentversionpageimagecache_storage,
+    setting_documentversionpageimagecache_storage_arguments,
     setting_recently_accessed_document_count,
-    setting_recently_created_document_count
+    setting_recently_created_document_count,
 )
 
 from .literals import (
+    TEST_DOCUMENTS_CACHE_STORAGE_BACKEND,
+    TEST_DOCUMENTS_CACHE_STORAGE_BACKEND_ARGUMENTS,
+    TEST_DOCUMENTS_STORAGE_BACKEND, TEST_DOCUMENTS_STORAGE_BACKEND_ARGUMENTS,
     TEST_DOCUMENTS_RECENTLY_ACCESSED_COUNT,
     TEST_DOCUMENTS_RECENTLY_CREATED_COUNT
 )
 
 
 class DocumentSettingMigrationTestCase(SmartSettingTestMixin, BaseTestCase):
-    def test_documents_storage_backend_arguments_0001(self):
-        test_value = {'location': 'test value'}
-        self.test_setting = setting_document_file_page_image_cache_storage_arguments
-        self.test_config_value = '{}'.format(
-            Setting.serialize_value(value=test_value)
-        )
+    @override_settings(DOCUMENTS_CACHE_STORAGE_BACKEND=TEST_DOCUMENTS_CACHE_STORAGE_BACKEND)
+    def test_setting_document_file_page_image_cache_storage_0003(self):
+        test_value = None
+        self.test_setting = setting_document_file_page_image_cache_storage
+        self.test_config_value = test_value
         self._create_test_config_file()
 
         self.assertEqual(
-            setting_document_file_page_image_cache_storage_arguments.value,
-            test_value
+            setting_document_file_page_image_cache_storage.value,
+            TEST_DOCUMENTS_CACHE_STORAGE_BACKEND
         )
 
-    def test_documents_storage_backend_arguments_0001_with_dict(self):
-        test_value = {'location': 'test value'}
+    @override_settings(DOCUMENTS_CACHE_STORAGE_BACKEND_ARGUMENTS=TEST_DOCUMENTS_CACHE_STORAGE_BACKEND_ARGUMENTS)
+    def test_setting_document_file_page_image_cache_storage_arguments_0003(self):
+        test_value = None
         self.test_setting = setting_document_file_page_image_cache_storage_arguments
         self.test_config_value = test_value
         self._create_test_config_file()
 
         self.assertEqual(
             setting_document_file_page_image_cache_storage_arguments.value,
-            test_value
+            TEST_DOCUMENTS_CACHE_STORAGE_BACKEND_ARGUMENTS
         )
 
-    def test_documents_cache_storage_backend_arguments_0001(self):
-        test_value = {'location': 'test value'}
-        self.test_setting = setting_document_file_storage_backend_arguments
-        self.test_config_value = '{}'.format(
-            Setting.serialize_value(value=test_value)
-        )
+    @override_settings(DOCUMENTS_STORAGE_BACKEND=TEST_DOCUMENTS_STORAGE_BACKEND)
+    def test_setting_document_file_storage_backend_0003(self):
+        test_value = None
+        self.test_setting = setting_document_file_storage_backend
+        self.test_config_value = test_value
         self._create_test_config_file()
 
         self.assertEqual(
-            setting_document_file_storage_backend_arguments.value,
-            test_value
+            setting_document_file_storage_backend.value,
+            TEST_DOCUMENTS_STORAGE_BACKEND
         )
 
-    def test_documents_cache_storage_backend_arguments_0001_with_dict(self):
-        test_value = {'location': 'test value'}
+    @override_settings(DOCUMENTS_STORAGE_BACKEND_ARGUMENTS=TEST_DOCUMENTS_STORAGE_BACKEND_ARGUMENTS)
+    def test_setting_document_file_storage_backend_arguments_0003(self):
+        test_value = None
         self.test_setting = setting_document_file_storage_backend_arguments
         self.test_config_value = test_value
         self._create_test_config_file()
 
         self.assertEqual(
             setting_document_file_storage_backend_arguments.value,
-            test_value
+            TEST_DOCUMENTS_STORAGE_BACKEND_ARGUMENTS
+        )
+
+    @override_settings(DOCUMENTS_CACHE_STORAGE_BACKEND=TEST_DOCUMENTS_CACHE_STORAGE_BACKEND)
+    def test_setting_documentversionpageimagecache_storage_0003(self):
+        test_value = None
+        self.test_setting = setting_documentversionpageimagecache_storage
+        self.test_config_value = test_value
+        self._create_test_config_file()
+
+        self.assertEqual(
+            setting_documentversionpageimagecache_storage.value,
+            TEST_DOCUMENTS_CACHE_STORAGE_BACKEND
+        )
+
+    @override_settings(DOCUMENTS_CACHE_STORAGE_BACKEND_ARGUMENTS=TEST_DOCUMENTS_CACHE_STORAGE_BACKEND_ARGUMENTS)
+    def test_setting_documentversionpageimagecache_storage_arguments_0003(self):
+        test_value = None
+        self.test_setting = setting_documentversionpageimagecache_storage_arguments
+        self.test_config_value = test_value
+        self._create_test_config_file()
+
+        self.assertEqual(
+            setting_documentversionpageimagecache_storage_arguments.value,
+            TEST_DOCUMENTS_CACHE_STORAGE_BACKEND_ARGUMENTS
         )
 
     @override_settings(DOCUMENTS_RECENT_ACCESS_COUNT=TEST_DOCUMENTS_RECENTLY_ACCESSED_COUNT)
