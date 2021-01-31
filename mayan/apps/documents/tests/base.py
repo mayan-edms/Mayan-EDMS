956ef4ccc9f173da9301ad889798c59f7d396c4c
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-01-31 05:15:18 -0400

    Add transaction variants of document test cases
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/documents/tests/base.py
+++ mayan/apps/documents/tests/base.py
@@ -1,4 +1,7 @@
-from mayan.apps.testing.tests.base import BaseTestCase, GenericViewTestCase
+from mayan.apps.testing.tests.base import (
+    BaseTestCase, BaseTransactionTestCase, GenericViewTestCase,
+    GenericTransactionViewTestCase
+)
 
 from .mixins.document_mixins import DocumentTestMixin
 
@@ -7,5 +10,17 @@ class GenericDocumentTestCase(DocumentTestMixin, BaseTestCase):
     """Base test case when testing models or classes"""
 
 
+class GenericTransactionDocumentTestCase(
+    DocumentTestMixin, BaseTransactionTestCase
+):
+    """Base test case when testing models or classes with transactions"""
+
+
 class GenericDocumentViewTestCase(DocumentTestMixin, GenericViewTestCase):
     """Base test case when testing views"""
+
+
+class GenericTransactionDocumentViewTestCase(
+    DocumentTestMixin, GenericTransactionViewTestCase
+):
+    """Base test case when testing views with transactions"""
