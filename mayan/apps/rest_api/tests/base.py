b53bcc5906e3dc3037ae5911cf3a037817081fea
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-02-05 03:00:51 -0400

    Add API transaction test case class
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/rest_api/tests/base.py
+++ mayan/apps/rest_api/tests/base.py
@@ -1,13 +1,30 @@
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APITransactionTestCase
 
 from mayan.apps.permissions.classes import Permission
 from mayan.apps.smart_settings.classes import SettingNamespace
-from mayan.apps.testing.tests.base import GenericViewTestCase
+from mayan.apps.testing.tests.base import (
+    GenericViewTestCase, GenericTransactionViewTestCase
+)
 
 
 class BaseAPITestCase(APITestCase, GenericViewTestCase):
     """
-    API test case class that invalidates permissions and smart settings
+    API test case class that invalidates permissions and smart settings.
+    """
+    expected_content_types = None
+
+    def setUp(self):
+        super().setUp()
+        SettingNamespace.invalidate_cache_all()
+        Permission.invalidate_cache()
+
+
+class BaseAPITransactionTestCase(
+    APITransactionTestCase, GenericTransactionViewTestCase
+):
+    """
+    API transaction test case class that invalidates permissions and smart
+    settings.
     """
     expected_content_types = None
 
