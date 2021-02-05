9937a669b3654c1324d37b8d4eac7d04c29976ec
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-02-05 03:01:09 -0400

    Fix document version API test
    Update test to use the API transaction test case class.
    
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/documents/tests/test_document_version_api.py
+++ mayan/apps/documents/tests/test_document_version_api.py
@@ -1,11 +1,14 @@
 from rest_framework import status
 
-from mayan.apps.rest_api.tests.base import BaseAPITestCase
+from mayan.apps.rest_api.tests.base import (
+    BaseAPITestCase, BaseAPITransactionTestCase
+)
+from mayan.apps.storage.events import event_download_file_created
 from mayan.apps.storage.models import DownloadFile
 
 from ..events import (
     event_document_version_created, event_document_version_deleted,
-    event_document_version_edited
+    event_document_version_edited, event_document_version_exported
 )
 from ..permissions import (
     permission_document_version_create, permission_document_version_delete,
@@ -209,6 +212,41 @@ class DocumentVersionAPIViewTestCase(
         self.assertEqual(event.target, self.test_document_version)
         self.assertEqual(event.verb, event_document_version_edited.id)
 
+    def test_document_version_list_api_view_no_permission(self):
+        self._clear_events()
+
+        response = self._request_test_document_version_list_api_view()
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        event = self._get_test_object_event()
+        self.assertEqual(event, None)
+
+    def test_document_version_list_api_view_with_access(self):
+        self.grant_access(
+            obj=self.test_document,
+            permission=permission_document_version_view
+        )
+
+        self._clear_events()
+
+        response = self._request_test_document_version_list_api_view()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(
+            response.data['results'][0]['id'],
+            self.test_document.version_active.id
+        )
+
+        event = self._get_test_object_event()
+        self.assertEqual(event, None)
+
+
+class DocumentVersionExportAPIViewTestCase(
+    DocumentVersionAPIViewTestMixin, DocumentTestMixin,
+    DocumentVersionTestMixin, BaseAPITransactionTestCase
+):
+    _test_event_object_name = 'test_document_version'
+
     def test_document_version_export_api_view_no_permission(self):
         download_file_count = DownloadFile.objects.count()
 
@@ -240,33 +278,16 @@ class DocumentVersionAPIViewTestCase(
             DownloadFile.objects.count(), download_file_count + 1
         )
 
-        event = self._get_test_object_event()
-        self.assertEqual(event, None)
+        test_download_file = DownloadFile.objects.first()
 
-    def test_document_version_list_api_view_no_permission(self):
-        self._clear_events()
+        events = self._get_test_object_events()
 
-        response = self._request_test_document_version_list_api_view()
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(events[0].action_object, self.test_document_version)
+        self.assertEqual(events[0].actor, test_download_file)
+        self.assertEqual(events[0].target, test_download_file)
+        self.assertEqual(events[0].verb, event_download_file_created.id)
 
-        event = self._get_test_object_event()
-        self.assertEqual(event, None)
-
-    def test_document_version_list_api_view_with_access(self):
-        self.grant_access(
-            obj=self.test_document,
-            permission=permission_document_version_view
-        )
-
-        self._clear_events()
-
-        response = self._request_test_document_version_list_api_view()
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        self.assertEqual(
-            response.data['results'][0]['id'],
-            self.test_document.version_active.id
-        )
-
-        event = self._get_test_object_event()
-        self.assertEqual(event, None)
+        self.assertEqual(events[1].action_object, test_download_file)
+        self.assertEqual(events[1].actor, self.test_document_version)
+        self.assertEqual(events[1].target, self.test_document_version)
+        self.assertEqual(events[1].verb, event_document_version_exported.id)
