b930e1e09676284eb54859a0358177b817883f63
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-02-26 00:03:54 -0400

    Drop the indexes from model renames
    Django is unable to rename the named indexes when the
    model is renamed. When another model is created or
    renamed with the name of the original model, there
    is an index name clash.
    
    Directly drop the index prior to its recreation until
    Django bug 23577 is fixed: https://code.djangoproject.com/ticket/23577
    
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/documents/migrations/0057_auto_20200916_1057.py
+++ mayan/apps/documents/migrations/0057_auto_20200916_1057.py
@@ -11,9 +11,27 @@ class Migration(migrations.Migration):
             old_name='DocumentVersion',
             new_name='DocumentFile',
         ),
+        migrations.RunSQL
+        (
+            sql='DROP INDEX "documents_documentversion_timestamp_30bada95";',
+            reverse_sql='CREATE INDEX "documents_documentversion_timestamp_30bada95" '
+            'ON "documents_documentversion" ("timestamp");',
+        ),
+        migrations.RunSQL
+        (
+            sql='DROP INDEX "documents_documentversion_document_id_42757b7a";',
+            reverse_sql='CREATE INDEX "documents_documentversion_document_id_42757b7a" '
+            'ON "documents_documentversion" ("document_id");',
+        ),
         migrations.AlterModelOptions(
             name='documentpageresult',
-            options={'ordering': ('document_file__document', 'page_number'), 'verbose_name': 'Document page', 'verbose_name_plural': 'Document pages'},
+            options={
+                'ordering': (
+                    'document_file__document', 'page_number'
+                ),
+                'verbose_name': 'Document page',
+                'verbose_name_plural': 'Document pages'
+            },
         ),
         migrations.RenameField(
             model_name='documentpage',
