4f7b1d0908ac998d3b9fb08ee002b478dda6ee91
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-02-27 13:29:56 -0400

    Optimize documents migration 0067
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/documents/migrations/0067_auto_20201024_1120.py
+++ mayan/apps/documents/migrations/0067_auto_20201024_1120.py
@@ -2,18 +2,27 @@ from django.db import migrations
 
 
 def operation_document_file_filename_copy(apps, schema_editor):
-    Document = apps.get_model(
-        app_label='documents', model_name='Document'
-    )
-    DocumentFile = apps.get_model(
-        app_label='documents', model_name='DocumentFile'
-    )
-    document_iterator = Document.objects.using(alias=schema_editor.connection.alias).all().only('id', 'label').values_list('id', 'label').iterator()
+    cursor_main = schema_editor.connection.cursor()
+    cursor_document_file = schema_editor.connection.cursor()
+    query_document_file = '''
+        UPDATE "documents_documentfile" SET "filename" = %s WHERE "documents_documentfile"."id" = %s;
+    '''
+    query = '''
+        SELECT
+            "documents_document"."label",
+            "documents_documentfile"."id"
+        FROM "documents_documentfile"
+        INNER JOIN "documents_document" ON (
+            "documents_documentfile"."document_id" = "documents_document"."id"
+        )
+    '''
+    cursor_main.execute(query)
 
-    for document_id, label in document_iterator:
-        DocumentFile.objects.filter(
-            document_id=document_id
-        ).update(filename=label)
+    for row in cursor_main.fetchall():
+
+        cursor_document_file.execute(
+            query_document_file, row
+        )
 
 
 class Migration(migrations.Migration):
