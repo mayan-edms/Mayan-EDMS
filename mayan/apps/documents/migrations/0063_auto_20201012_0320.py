d33041f3084f16e38f6a7238daefd404e5e375b5
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-02-27 09:43:09 -0400

    Futher improve migration 0063
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/documents/migrations/0063_auto_20201012_0320.py
+++ mayan/apps/documents/migrations/0063_auto_20201012_0320.py
@@ -1,47 +1,9 @@
-from django.db import migrations, transaction
-
-
-def pages_reset(
-    document_id, DocumentFile, DocumentFilePage, DocumentVersion,
-    DocumentVersionPage, content_type_id
-):
-    """
-    Remove all page mappings and recreate them to be a 1 to 1 match
-    to the latest document file or the document file supplied.
-    """
-    with transaction.atomic():
-        document_version = DocumentVersion.objects.create(document_id=document_id)
-
-        latest_file_id = DocumentFile.objects.filter(document_id=document_id).only('id').order_by('timestamp').last().id
-
-        if latest_file_id:
-            content_object_id_list = DocumentFilePage.objects.filter(
-                document_file_id=latest_file_id
-            ).only('id').values_list('id', flat=True).iterator()
-
-            document_version_pages = [
-                DocumentVersionPage(
-                    document_version_id=document_version.id,
-                    content_type_id=content_type_id,
-                    object_id=content_object_id,
-                    page_number=page_number
-                ) for page_number, content_object_id in enumerate(
-                    iterable=content_object_id_list, start=1
-                )
-            ]
-
-            document_version.pages.bulk_create(document_version_pages)
+from django.db import migrations
 
 
 def operation_document_version_page_create(apps, schema_editor):
     ContentType = apps.get_model(
         app_label='contenttypes', model_name='ContentType'
-    )
-    Document = apps.get_model(
-        app_label='documents', model_name='Document'
-    )
-    DocumentFile = apps.get_model(
-        app_label='documents', model_name='DocumentFile'
     )
     DocumentFilePage = apps.get_model(
         app_label='documents', model_name='DocumentFilePage'
@@ -49,23 +11,79 @@ def operation_document_version_page_create(apps, schema_editor):
     DocumentVersion = apps.get_model(
         app_label='documents', model_name='DocumentVersion'
     )
-    DocumentVersionPage = apps.get_model(
-        app_label='documents', model_name='DocumentVersionPage'
-    )
 
     content_type = ContentType.objects.get_for_model(model=DocumentFilePage)
+    content_type_id = content_type.id
+    cursor_main = schema_editor.connection.cursor()
+    cursor_document_version_page = schema_editor.connection.cursor()
 
-    document_id_iterator = Document.objects.using(alias=schema_editor.connection.alias).all().only('id').values_list('id', flat=True).iterator()
+    query = '''
+    SELECT
+        "documents_documentfile"."document_id",
+        "documents_documentfilepage"."document_file_id",
+        "documents_documentfilepage"."id"
+    FROM "documents_documentfilepage"
+    INNER JOIN "documents_documentfile" ON (
+        "documents_documentfilepage"."document_file_id" = "documents_documentfile"."id"
+    ) ORDER BY "documents_documentfilepage"."id" ASC
+    '''
+    cursor_main.execute(query)
 
-    for document_id in document_id_iterator:
-        pages_reset(
-            document_id=document_id,
-            DocumentFile=DocumentFile,
-            DocumentFilePage=DocumentFilePage,
-            DocumentVersion=DocumentVersion,
-            DocumentVersionPage=DocumentVersionPage,
-            content_type_id=content_type.id,
+    class DummyDocumentVersion:
+        def save(self):
+            """Does not do anything."""
+
+    document_file_id_last = None
+    document_id_last = None
+    document_version = DummyDocumentVersion()
+    document_version_page_insert_query = '''
+        INSERT INTO documents_documentversionpage (
+            document_version_id,content_type_id,object_id,page_number
+        ) VALUES {};
+    '''
+    document_version_page_values = []
+    page_number = 1
+
+    for row in cursor_main.fetchall():
+        document_id, document_file_id, document_file_page_id = row
+
+        if document_id_last != document_id:
+            document_version.active = True
+            document_version.save()
+            document_id_last = document_id
+
+        if document_file_id_last != document_file_id:
+            document_version = DocumentVersion.objects.create(document_id=document_id)
+            document_version_id = document_version.pk
+            document_file_id_last = document_file_id
+            if document_version_page_values:
+                final_query = document_version_page_insert_query.format(
+                    ','.join(['(%s,%s,%s,%s)'] * (page_number - 1))
+                )
+
+                page_number = 1
+
+                cursor_document_version_page.execute(
+                    final_query, document_version_page_values
+                )
+
+            document_version_page_values = []
+
+        document_version_page_values.extend(
+            (
+                document_version_id, content_type_id, document_file_page_id,
+                page_number
+            )
         )
+
+        page_number = page_number + 1
+
+    final_query = document_version_page_insert_query.format(
+        ','.join(['(%s,%s,%s,%s)'] * (page_number - 1))
+    )
+    cursor_document_version_page.execute(
+        final_query, document_version_page_values
+    )
 
 
 def operation_document_version_page_create_reverse(apps, schema_editor):
