928a912dd84ce904cfdcc94809502799b05f151a
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-03-05 00:42:18 -0400

    Refactor and optimize OCR migrations 0009, 0010
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

---
+++ mayan/apps/ocr/migrations/0010_auto_20210304_1215.py
@@ -0,0 +1,38 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+def code_delete_document_version_page_ocr_errors(apps, schema_editor):
+    DocumentVersionOCRError = apps.get_model(
+        app_label='ocr', model_name='DocumentVersionOCRError'
+    )
+    DocumentVersionOCRError.objects.all().delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('documents', '0075_delete_duplicateddocumentold'),
+        ('ocr', '0009_auto_20210304_0950'),
+    ]
+
+    operations = [
+        migrations.RenameModel(
+            old_name='DocumentTypeSettings',
+            new_name='DocumentTypeOCRSettings',
+        ),
+        migrations.RunPython(
+            code=code_delete_document_version_page_ocr_errors,
+        ),
+        migrations.AlterField(
+            model_name='documentversionocrerror',
+            name='document_version',
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name='ocr_errors', to='documents.DocumentVersion',
+                verbose_name='Document version'
+            ),
+        ),
+        migrations.DeleteModel(
+            name='DocumentPageOCRContent',
+        ),
+    ]
