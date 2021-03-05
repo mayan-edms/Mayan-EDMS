38757b5e97768f1da0b1f9100d3011072ddbc269
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-03-05 00:45:42 -0400

    Style updates
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/duplicates/migrations/0004_storedduplicatebackend.py
+++ mayan/apps/duplicates/migrations/0004_storedduplicatebackend.py
@@ -10,9 +10,23 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='StoredDuplicateBackend',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('backend_path', models.CharField(help_text='The dotted Python path to the backend class.', max_length=128, verbose_name='Backend path')),
-                ('backend_data', models.TextField(blank=True, verbose_name='Backend data')),
+                (
+                    'id', models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False,
+                        verbose_name='ID'
+                    )
+                ),
+                (
+                    'backend_path', models.CharField(
+                        help_text='The dotted Python path to the backend '
+                        'class.', max_length=128, verbose_name='Backend path'
+                    )
+                ),
+                (
+                    'backend_data', models.TextField(
+                        blank=True, verbose_name='Backend data'
+                    )
+                ),
             ],
             options={
                 'verbose_name': 'Stored duplicate backend',
