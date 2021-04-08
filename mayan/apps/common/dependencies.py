89e1045561ea39284abc75a3093c02ced35023a2
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-04-08 06:15:03 -0400

    Update devpi-server from 5.5.0 to 5.5.1
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/common/dependencies.py
+++ mayan/apps/common/dependencies.py
@@ -153,7 +153,7 @@ PythonDependency(
 
 PythonDependency(
     module=__name__, environment=environment_development, name='devpi-server',
-    version_string='==5.5.0'
+    version_string='==5.5.1'
 )
 PythonDependency(
     environment=environment_development, module=__name__,
