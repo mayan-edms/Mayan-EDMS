a8f41654239f4f1688b2615f85926abd09d80647
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-04-26 02:04:27 -0400

    Add method acquire and release lock decorators
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/lock_manager/decorators.py
+++ mayan/apps/lock_manager/decorators.py
@@ -36,3 +36,46 @@ def locked_class_method(func):
                 if _acquire_lock:
                     lock.release()
     return wrapper
+
+
+def acquire_lock_class_method(func):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if inspect.isgeneratorfunction(getattr(func, '__wrapped__', None)):
+            raise ImproperlyConfigured(
+                'This decorator does not support generators.'
+            )
+
+        _acquire_lock = kwargs.pop('_acquire_lock', True)
+        lock_name = self._lock_manager_get_lock_name(*args, **kwargs)
+
+        try:
+            if _acquire_lock:
+                logger.debug('trying to acquire lock: %s', lock_name)
+                self._lock = LockingBackend.get_instance().acquire_lock(name=lock_name)
+                logger.debug('acquired lock: %s', lock_name)
+        except LockError:
+            logger.debug('unable to obtain lock: %s' % lock_name)
+            raise
+        else:
+            return func(self, *args, **kwargs)
+
+    return wrapper
+
+
+def release_lock_class_method(func):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if inspect.isgeneratorfunction(getattr(func, '__wrapped__', None)):
+            raise ImproperlyConfigured(
+                'This decorator does not support generators.'
+            )
+
+        _acquire_lock = kwargs.pop('_acquire_lock', True)
+
+        try:
+            return func(self, *args, **kwargs)
+        finally:
+            if _acquire_lock:
+                self._lock.release()
+    return wrapper
