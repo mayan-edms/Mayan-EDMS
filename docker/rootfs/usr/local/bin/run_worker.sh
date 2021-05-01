90ba0bd687dd07b819749900dd11baef45456499
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-05-01 04:48:21 -0400

    Support nice levels for the `run_worker` command
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- docker/rootfs/usr/local/bin/run_worker.sh
+++ docker/rootfs/usr/local/bin/run_worker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export MAYAN_WORKER_NAME=${MAYAN_WORKER_NAME:-$1}
+export MAYAN_WORKER_NICE_LEVEL=${MAYAN_WORKER_NICE_LEVEL:-0}
 
 if [ ! "${MAYAN_QUEUE_LIST}" ]; then
     if [ ! "$MAYAN_WORKER_NAME" ]; then
@@ -14,4 +15,4 @@ fi
 # Use -A and not --app. Both are the same but behave differently
 # -A can be located before the command while --app cannot.
 # Pass ${@:2} to allow overriding the defaults arguments
-su mayan -c "${MAYAN_PYTHON_BIN_DIR}celery -A mayan worker -Ofair -l ERROR -Q ${MAYAN_QUEUE_LIST} ${@:2}"
+su mayan -c "nice -n ${MAYAN_WORKER_NICE_LEVEL} ${MAYAN_PYTHON_BIN_DIR}celery -A mayan worker -Ofair -l ERROR -Q ${MAYAN_QUEUE_LIST} ${@:2}"
