56393e4a4b0154895edcb0d21b7c888b85811739
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-06-11 05:37:58 -0400

    Unify release note exporters
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- contrib/scripts/export_release_notes.py
+++ contrib/scripts/export_release_notes.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
+import optparse
 import os
 from pathlib import Path
 import sys
 
+import django
+from django.conf import settings
 from docutils import core
+from html2bbcode.parser import HTML2BBCode
 from lxml import etree, html
 
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(1, os.path.abspath('.'))
-
-from mayan.settings import BASE_DIR
-
-ignore_ids_list = ('upgrading-process',)
+VERSION = '2.0'
+ignore_ids_list = ('upgrade-process', 'troubleshooting')
 
 
 class ReleaseNoteExporter:
@@ -21,62 +21,58 @@ class ReleaseNoteExporter:
         result = []
 
         for element in tree:
-            if element.tag == 'div':
-                if not element.attrib.get('id') in ignore_ids_list:
-                    result.extend(
-                        ReleaseNoteExporter.filter_elements(tree=element)
-                    )
-            else:
-                if not element.attrib.get('id') in ignore_ids_list:
-                    result.append(
-                        etree.tostring(element).replace(b'\n', b' ')
-                    )
+            if element.attrib.get('id') not in ignore_ids_list:
+
+                if element.tag == 'div':
+                    if not element.attrib.get('id') in ignore_ids_list:
+                        result.extend(
+                            ReleaseNoteExporter.filter_elements(tree=element)
+                        )
+                else:
+                    if not element.attrib.get('id') in ignore_ids_list:
+                        result.append(
+                            etree.tostring(element).replace(b'\n', b' ')
+                        )
 
         return result
 
-    def __init__(self, version):
-        self.version = version
+    def __init__(self):
+        sys.path.insert(0, os.path.abspath('..'))
+        sys.path.insert(1, os.path.abspath('.'))
+
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mayan.settings')
+
+        self.parser = optparse.OptionParser(
+            usage='%prog [version number]', version='%prog {}'.format(VERSION)
+        )
+        self.parser.add_option(
+            '-f', '--format', help='specify the output format',
+            dest='output_format',
+            action='store', metavar='output_format'
+        )
+
+        (self.options, args) = self.parser.parse_args()
+
+        if len(args) != 1:
+            self.parser.error('version argument is missing')
+
+        django.setup()
+
+        self.version = args[0]
 
     def export(self):
         path_documentation = Path(
-            BASE_DIR
+            settings.BASE_DIR
         ) / '..' / 'docs' / 'releases' / '{}.txt'.format(self.version)
 
         with path_documentation.open(mode='r') as file_object:
             content = []
 
-            content.append('New version of Mayan EDMS available\n')
-            content.append('===================================\n\n\n\n\n\n')
-
-            content.append(
-                'Please read the release notes before upgrading: '
-                'https://docs.mayan-edms.com/releases/{}.html\n\n'.format(
-                    self.version
-                )
-            )
-
-            content.append('Package locations\n')
-            content.append('=================\n\n\n\n\n\n\n\n')
-
-            content.append(
-                'Docker image available at: '
-                'https://hub.docker.com/r/mayanedms/mayanedms\n\n'
-            )
-            content.append(
-                'Python packages available at: '
-                'https://pypi.org/project/mayan-edms/{}/ and '
-                'installed via:\n\n'.format(self.version)
-            )
-
-            content.append(
-                '``pip install mayan-edms=={}``\n\n'.format(self.version)
-            )
-            file_object.readline()
-            file_object.readline()
-            file_object.readline()
-            file_object.readline()
             for line in file_object:
-                if ':gitlab-issue:' in line:
+                if line.startswith('.. include'):
+                    # Skip
+                    pass
+                elif ':gitlab-issue:' in line:
                     line_parts = line.split('`')
 
                     result = (
@@ -100,16 +96,45 @@ class ReleaseNoteExporter:
         result = ReleaseNoteExporter.filter_elements(
             tree=html.fromstring(html_fragment)
         )
-        return b''.join(result)
+
+        if self.options.output_format == 'bb':
+            result = result[1:]
+            html_output = str(b''.join(result))
+
+            html_replace_list = (
+                ('<tt', '<code'),
+                ('</tt>', '</code>'),
+            )
+
+            for html_replace_item in html_replace_list:
+                html_output = html_output.replace(*html_replace_item)
+
+            parser = HTML2BBCode()
+
+            result = str(parser.feed(html_output))
+
+            bbcode_replace_list = (
+                ('[h1]', '\n[size=150]'),
+                ('[/h1]', '[/size]\n'),
+                ('[h2]', '\n[size=150]'),
+                ('[/h2]', '[/size]\n'),
+                ('[h3]', '\n[b]'),
+                ('[/h3]', '[/b]\n'),
+                ('[li]', '\n[*]'),
+                ('[/li]', ''),
+                ('[code]', '[b][i]'),
+                ('[/code]', '[/i][/b]'),
+            )
+
+            for bbcode_replace_item in bbcode_replace_list:
+                result = result.replace(*bbcode_replace_item)
+
+            return result
+        else:
+            return b''.join(result)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print(
-            'usage: export_release_notes <version>'
-        )
-        exit(0)
-
-    print(
-        ReleaseNoteExporter(version=sys.argv[1]).export()
-    )
+    message_processor = ReleaseNoteExporter()
+    result = message_processor.export()
+    print(result)
