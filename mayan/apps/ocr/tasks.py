f3607444cc99da11b512a1a11576b2836c191ca4
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-06-17 21:14:30 -0400

    Use `retry_backoff` for the OCR tasks.
    Remove the `TASK_DOCUMENT_VERSION_PAGE_OCR_RETRY_DELAY` setting.
    
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/ocr/tasks.py
+++ mayan/apps/ocr/tasks.py
@@ -10,13 +10,12 @@ from mayan.apps.lock_manager.exceptions import LockError
 from mayan.celery import app
 
 from .events import event_ocr_document_version_finish
-from .literals import TASK_DOCUMENT_VERSION_PAGE_OCR_RETRY_DELAY
 from .signals import signal_post_document_version_ocr
 
 logger = logging.getLogger(name=__name__)
 
 
-@app.task(bind=True, ignore_result=True)
+@app.task(bind=True, ignore_result=True, retry_backoff=True)
 def task_document_version_ocr_process(self, document_version_id, user_id=None):
     logger.info(
         'Starting OCR for document version page ID: %s', document_version_id
@@ -48,9 +47,7 @@ def task_document_version_ocr_process(self, document_version_id, user_id=None):
         raise
 
 
-@app.task(
-    bind=True, default_retry_delay=TASK_DOCUMENT_VERSION_PAGE_OCR_RETRY_DELAY
-)
+@app.task(bind=True, retry_backoff=True)
 def task_document_version_page_ocr_process(
     self, document_version_page_id, user_id=None
 ):
@@ -92,7 +89,9 @@ def task_document_version_page_ocr_process(
 
 
 @app.task(bind=True, ignore_result=True)
-def task_document_version_ocr_finished(self, results, document_version_id, user_id=None):
+def task_document_version_ocr_finished(
+    self, results, document_version_id, user_id=None
+):
     logger.info(
         'OCR complete for document version ID: %s', document_version_id
     )
