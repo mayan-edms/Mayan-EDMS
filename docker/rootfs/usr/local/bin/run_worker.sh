027f28891bb3f476f37f05b61e4844cf5c3eed80
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-06-20 02:21:39 -0400

    Fix worker command argument
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- docker/rootfs/usr/local/bin/run_worker.sh
+++ docker/rootfs/usr/local/bin/run_worker.sh
@@ -15,4 +15,4 @@ fi
 # Use -A and not --app. Both are the same but behave differently
 # -A can be located before the command while --app cannot.
 # Pass ${@:2} to allow overriding the defaults arguments
-su mayan -c "nice -n ${MAYAN_WORKER_NICE_LEVEL} ${MAYAN_PYTHON_BIN_DIR}celery -A mayan worker -loglevel=ERROR -Ofair --queues=${MAYAN_QUEUE_LIST} --without-gossip --without-heartbeat --without-mingle ${@:2}"
+su mayan -c "nice -n ${MAYAN_WORKER_NICE_LEVEL} ${MAYAN_PYTHON_BIN_DIR}celery -A mayan worker --loglevel=ERROR -Ofair --queues=${MAYAN_QUEUE_LIST} --without-gossip --without-heartbeat --without-mingle ${@:2}"
