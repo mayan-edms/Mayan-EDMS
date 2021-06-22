1bf1609be0b04699a0dd77d7470def2b1a708ab3
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-06-22 12:44:01 -0400

    Add support for MIME type detection backends
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/mimetype/api.py
+++ mayan/apps/mimetype/api.py
@@ -1,39 +1,9 @@
-from shutil import copyfileobj
-
-import magic
-
-from mayan.apps.storage.utils import NamedTemporaryFile
+from .classes import MIMETypeBackend
 
 
 def get_mimetype(file_object, mimetype_only=False):
-    """
-    Determine a file's mimetype by calling the system's libmagic
-    library via python-magic.
-    """
-    file_mimetype = None
-    file_mime_encoding = None
+    mime_type_backend = MIMETypeBackend.get_backend_instance()
 
-    temporary_file_object = NamedTemporaryFile()
-    file_object.seek(0)
-    copyfileobj(fsrc=file_object, fdst=temporary_file_object)
-    file_object.seek(0)
-    temporary_file_object.seek(0)
-
-    kwargs = {'mime': True}
-
-    if not mimetype_only:
-        kwargs['mime_encoding'] = True
-
-    try:
-        mime = magic.Magic(**kwargs)
-
-        if mimetype_only:
-            file_mimetype = mime.from_file(filename=temporary_file_object.name)
-        else:
-            file_mimetype, file_mime_encoding = mime.from_file(
-                filename=temporary_file_object.name
-            ).split('; charset=')
-    finally:
-        temporary_file_object.close()
-
-    return file_mimetype, file_mime_encoding
+    return mime_type_backend.get_mimetype(
+        file_object=file_object, mimetype_only=mimetype_only
+    )
---
+++ mayan/apps/mimetype/literals.py
@@ -0,0 +1,2 @@
+DEFAULT_MIME_TYPE_BACKEND = 'mayan.apps.mimetype.backends.python_magic.MIMETypeBackendPythonMagic'
+DEFAULT_MIME_TYPE_BACKEND_ARGUMENTS = {}
