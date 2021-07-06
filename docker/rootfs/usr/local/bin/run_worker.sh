c2864943cc560283d54d8b7d63c11a123365556b
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-07-06 02:45:58 -0400

    Remove remaining bash features
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- docker/rootfs/usr/local/bin/run_worker.sh
+++ docker/rootfs/usr/local/bin/run_worker.sh
@@ -14,5 +14,5 @@ fi
 
 # Use -A and not --app. Both are the same but behave differently
 # -A can be located before the command while --app cannot.
-# Pass ${@:2} to allow overriding the defaults arguments
-su mayan -c "nice -n ${MAYAN_WORKER_NICE_LEVEL} ${MAYAN_PYTHON_BIN_DIR}celery -A mayan worker -Ofair -l ERROR -Q ${MAYAN_QUEUE_LIST} ${@:2}"
+shift
+su mayan -c "nice -n ${MAYAN_WORKER_NICE_LEVEL} ${MAYAN_PYTHON_BIN_DIR}celery -A mayan worker -Ofair -l ERROR -Q ${MAYAN_QUEUE_LIST} ${@}"
