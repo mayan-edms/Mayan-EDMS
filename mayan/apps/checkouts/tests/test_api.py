d6163b2de3659d9e817e96fd13170caec9c32c00
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-07-19 22:58:18 -0400

    Checkout test updates
    - Silence debug output of tests.
    - Speed up tests using document stubs.
    
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/checkouts/tests/test_api.py
+++ mayan/apps/checkouts/tests/test_api.py
@@ -20,6 +20,12 @@ class CheckoutsAPITestCase(
     DocumentCheckoutsAPIViewTestMixin, DocumentCheckoutTestMixin,
     DocumentTestMixin, BaseAPITestCase
 ):
+    auto_upload_test_document = False
+
+    def setUp(self):
+        super().setUp()
+        self._create_test_document_stub()
+
     def test_document_check_out_create_api_view_no_permission(self):
         response = self._request_test_document_check_out_create_api_view()
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
