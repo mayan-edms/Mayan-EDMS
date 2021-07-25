b2dbf3b243b246cf2a1925267071902ea9c2a96c
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-07-25 02:53:00 -0400

    Fix OCR version event texts
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/ocr/events.py
+++ mayan/apps/ocr/events.py
@@ -5,14 +5,14 @@ from mayan.apps.events.classes import EventTypeNamespace
 namespace = EventTypeNamespace(label=_('OCR'), name='ocr')
 
 event_ocr_document_version_content_deleted = namespace.add_event_type(
-    label=_('Document OCR content deleted'),
+    label=_('Document version OCR content deleted'),
     name='document_content_deleted'
 )
 event_ocr_document_version_submitted = namespace.add_event_type(
-    label=_('Document file submitted for OCR'),
+    label=_('Document version submitted for OCR'),
     name='document_version_submit'
 )
 event_ocr_document_version_finished = namespace.add_event_type(
-    label=_('Document file OCR finished'),
+    label=_('Document version OCR finished'),
     name='document_version_finish'
 )
