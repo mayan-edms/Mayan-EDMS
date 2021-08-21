a5a187dc0f1f6b08b57c40a7ebba20ab89e7465f
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-08-21 04:17:03 -0400

    Add detail for file partitions
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/file_caching/urls.py
+++ mayan/apps/file_caching/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
 from .views import (
-    CacheDetailView, CacheListView, CachePartitionPurgeView, CachePurgeView
+    CacheDetailView, CacheListView, CachePartitionDetailView,
+    CachePartitionPurgeView, CachePurgeView
 )
 
 urlpatterns = [
@@ -19,6 +20,11 @@ urlpatterns = [
     url(
         regex=r'^caches/multiple/purge/$', name='cache_multiple_purge',
         view=CachePurgeView.as_view()
+    ),
+    url(
+        regex=r'^caches/(?P<cache_partition_id>\d+)',
+        name='cache_partition_detail',
+        view=CachePartitionDetailView.as_view()
     ),
     url(
         regex=r'^apps/(?P<app_label>[-\w]+)/models/(?P<model_name>[-\w]+)/objects/(?P<object_id>\d+)/cache_partitions/purge/$',
