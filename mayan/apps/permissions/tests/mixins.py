e28be9b8e111ce14f3f098d0735741a1af07e972
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-08-25 03:13:15 -0400

    Remove unused permission test code
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/permissions/tests/mixins.py
+++ mayan/apps/permissions/tests/mixins.py
@@ -4,10 +4,9 @@ from ..classes import Permission, PermissionNamespace
 from ..models import Role
 
 from .literals import (
-    TEST_CASE_ROLE_LABEL, TEST_PERMISSION_LABEL, TEST_PERMISSION_LABEL_2,
-    TEST_PERMISSION_NAME, TEST_PERMISSION_NAME_2, TEST_PERMISSION_NAMESPACE_LABEL,
-    TEST_PERMISSION_NAMESPACE_LABEL_2, TEST_PERMISSION_NAMESPACE_NAME,
-    TEST_PERMISSION_NAMESPACE_NAME_2, TEST_ROLE_LABEL, TEST_ROLE_LABEL_EDITED
+    TEST_CASE_ROLE_LABEL, TEST_PERMISSION_LABEL, TEST_PERMISSION_NAME,
+    TEST_PERMISSION_NAMESPACE_LABEL, TEST_PERMISSION_NAMESPACE_NAME,
+    TEST_ROLE_LABEL, TEST_ROLE_LABEL_EDITED
 )
 
 
@@ -54,16 +53,6 @@ class PermissionTestMixin:
         self.test_permission = self.test_permission_namespace.add_permission(
             label=TEST_PERMISSION_LABEL,
             name=TEST_PERMISSION_NAME
-        )
-
-    def _create_test_permission_2(self):
-        self.test_permission_namespace_2 = PermissionNamespace(
-            label=TEST_PERMISSION_NAMESPACE_LABEL_2,
-            name=TEST_PERMISSION_NAMESPACE_NAME_2
-        )
-        self.test_permission_2 = self.test_permission_namespace_2.add_permission(
-            label=TEST_PERMISSION_LABEL_2,
-            name=TEST_PERMISSION_NAME_2
         )
 
 
