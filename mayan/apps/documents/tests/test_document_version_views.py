aead9df30fccc44f97f1db4380715236d4e33d9b
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-08-25 05:43:31 -0400

    Track user when setting a version active
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/documents/tests/test_document_version_views.py
+++ mayan/apps/documents/tests/test_document_version_views.py
@@ -67,7 +67,7 @@ class DocumentVersionViewTestCase(
         self.assertEqual(events.count(), 1)
 
         self.assertEqual(events[0].action_object, self.test_document)
-        self.assertEqual(events[0].actor, self.test_document_version)
+        self.assertEqual(events[0].actor, self._test_case_user)
         self.assertEqual(events[0].target, self.test_document_version)
         self.assertEqual(events[0].verb, event_document_version_edited.id)
 
