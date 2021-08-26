b441099497c5e1d867f770d8de7ebf7db286fbad
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-08-26 09:21:21 -0400

    Remove redundant the mixin of the signatures API
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/document_signatures/api_views.py
+++ mayan/apps/document_signatures/api_views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import get_object_or_404
-
-from mayan.apps.acls.models import AccessControlList
-from mayan.apps.documents.models import Document
+from mayan.apps.documents.api_views.mixins import ParentObjectDocumentFileAPIViewMixin
 from mayan.apps.rest_api import generics
 
 from .models import DetachedSignature, EmbeddedSignature
@@ -19,37 +16,14 @@ from .serializers import (
 )
 
 
-class APIDocumentSignatureViewMixin:
-    def get_document(self):
-        return get_object_or_404(
-            klass=self.get_document_queryset(), pk=self.kwargs['document_id']
-        )
-
-    def get_document_queryset(self):
-        return AccessControlList.objects.restrict_queryset(
-            permission=self._document_view_permissions[self.request.method],
-            queryset=Document.valid.all(),
-            user=self.request.user
-        )
-
-    def get_document_file(self):
-        return get_object_or_404(
-            klass=self.get_document_file_queryset(),
-            pk=self.kwargs['document_file_id']
-        )
-
-    def get_document_file_queryset(self):
-        return self.get_document().files.all()
-
-
 class APIDocumentFileSignDetachedView(
-    APIDocumentSignatureViewMixin, generics.ObjectActionAPIView
+    ParentObjectDocumentFileAPIViewMixin, generics.ObjectActionAPIView
 ):
     """
     post: Sign a document file with a detached signature.
     """
-    _document_view_permissions = {
-        'POST': permission_document_file_sign_detached,
+    mayan_external_object_permissions = {
+        'POST': (permission_document_file_sign_detached,)
     }
     lookup_url_kwarg = 'document_file_id'
     serializer_class = SignDetachedSerializer
@@ -70,13 +44,13 @@ class APIDocumentFileSignDetachedView(
 
 
 class APIDocumentFileSignEmbeddedView(
-    APIDocumentSignatureViewMixin, generics.ObjectActionAPIView
+    ParentObjectDocumentFileAPIViewMixin, generics.ObjectActionAPIView
 ):
     """
     post: Sign a document file with an embedded signature.
     """
-    _document_view_permissions = {
-        'POST': permission_document_file_sign_embedded
+    mayan_external_object_permissions = {
+        'POST': (permission_document_file_sign_embedded,)
     }
     lookup_url_kwarg = 'document_file_id'
     serializer_class = SignEmbeddedSerializer
@@ -97,14 +71,14 @@ class APIDocumentFileSignEmbeddedView(
 
 
 class APIDocumentFileDetachedSignatureListView(
-    APIDocumentSignatureViewMixin, generics.ListAPIView
+    ParentObjectDocumentFileAPIViewMixin, generics.ListAPIView
 ):
     """
     get: Returns a list of all the detached signatures of a document file.
     post: Create a detached signature for a document file.
     """
-    _document_view_permissions = {
-        'GET': permission_document_file_signature_view,
+    mayan_external_object_permissions = {
+        'GET': (permission_document_file_signature_view,)
     }
     serializer_class = DetachedSignatureSerializer
 
@@ -113,32 +87,18 @@ class APIDocumentFileDetachedSignatureListView(
             document_file=self.get_document_file()
         )
 
-    def get_serializer_context(self):
-        """
-        Extra context provided to the serializer class.
-        """
-        context = super().get_serializer_context()
-        if self.kwargs:
-            context.update(
-                {
-                    'document_file': self.get_document_file(),
-                }
-            )
-
-        return context
-
 
 class APIDocumentFileDetachedSignatureDetailView(
-    APIDocumentSignatureViewMixin, generics.RetrieveDestroyAPIView
+    ParentObjectDocumentFileAPIViewMixin, generics.RetrieveDestroyAPIView
 ):
     """
     delete: Delete an detached signature of the selected document.
     get: Returns the details of the selected detached signature.
     """
-    _document_view_permissions = {
-        'DELETE': permission_document_file_signature_delete,
-        'GET': permission_document_file_signature_view,
-        'POST': permission_document_file_signature_view
+    mayan_external_object_permissions = {
+        'DELETE': (permission_document_file_signature_delete,),
+        'GET': (permission_document_file_signature_view,),
+        'POST': (permission_document_file_signature_view)
     }
     lookup_url_kwarg = 'detached_signature_id'
     serializer_class = DetachedSignatureSerializer
@@ -148,29 +108,15 @@ class APIDocumentFileDetachedSignatureDetailView(
             document_file=self.get_document_file()
         )
 
-    def get_serializer_context(self):
-        """
-        Extra context provided to the serializer class.
-        """
-        context = super().get_serializer_context()
-        if self.kwargs:
-            context.update(
-                {
-                    'document': self.get_document(),
-                }
-            )
-
-        return context
-
 
 class APIDocumentFileDetachedSignatureUploadView(
-    APIDocumentSignatureViewMixin, generics.CreateAPIView
+    ParentObjectDocumentFileAPIViewMixin, generics.CreateAPIView
 ):
     """
     post: Upload a detached signature file for a document file.
     """
-    _document_view_permissions = {
-        'POST': permission_document_file_signature_upload
+    mayan_external_object_permissions = {
+        'POST': (permission_document_file_signature_upload,)
     }
     lookup_url_kwarg = 'document_file_id'
     serializer_class = DetachedSignatureUploadSerializer
@@ -186,14 +132,14 @@ class APIDocumentFileDetachedSignatureUploadView(
 
 
 class APIDocumentFileEmbeddedSignatureListView(
-    APIDocumentSignatureViewMixin, generics.ListAPIView
+    ParentObjectDocumentFileAPIViewMixin, generics.ListAPIView
 ):
     """
     get: Returns a list of all the embedded signatures of a document file.
     """
     serializer_class = EmbeddedSignatureSerializer
-    _document_view_permissions = {
-        'GET': permission_document_file_signature_view
+    mayan_external_object_permissions = {
+        'GET': (permission_document_file_signature_view,)
     }
 
     def get_queryset(self):
@@ -201,35 +147,15 @@ class APIDocumentFileEmbeddedSignatureListView(
             document_file=self.get_document_file()
         )
 
-    def get_serializer(self, *args, **kwargs):
-        if not self.request:
-            return None
-
-        return super().get_serializer(*args, **kwargs)
-
-    def get_serializer_context(self):
-        """
-        Extra context provided to the serializer class.
-        """
-        context = super().get_serializer_context()
-        if self.kwargs:
-            context.update(
-                {
-                    'document_file': self.get_document_file(),
-                }
-            )
-
-        return context
-
 
 class APIDocumentFileEmbeddedSignatureDetailView(
-    APIDocumentSignatureViewMixin, generics.RetrieveAPIView
+    ParentObjectDocumentFileAPIViewMixin, generics.RetrieveAPIView
 ):
     """
     get: Returns the details of the selected embedded signature.
     """
-    _document_view_permissions = {
-        'GET': permission_document_file_signature_view,
+    mayan_external_object_permissions = {
+        'GET': (permission_document_file_signature_view,)
     }
     lookup_url_kwarg = 'embedded_signature_id'
     serializer_class = EmbeddedSignatureSerializer
@@ -238,17 +164,3 @@ class APIDocumentFileEmbeddedSignatureDetailView(
         return EmbeddedSignature.objects.filter(
             document_file=self.get_document_file()
         )
-
-    def get_serializer_context(self):
-        """
-        Extra context provided to the serializer class.
-        """
-        context = super().get_serializer_context()
-        if self.kwargs:
-            context.update(
-                {
-                    'document': self.get_document(),
-                }
-            )
-
-        return context
