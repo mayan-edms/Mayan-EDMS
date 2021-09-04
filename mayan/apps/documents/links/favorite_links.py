1814a1d791eb8e2783fa6277d2a77970b0c061d1
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-09-04 19:28:25 -0400

    Add conditions to the favorite document links
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/documents/links/favorite_links.py
+++ mayan/apps/documents/links/favorite_links.py
@@ -9,16 +9,26 @@ from ..icons import (
 from ..permissions import permission_document_view
 
 
+def condition_is_in_favorites(context):
+    return context['resolved_object'].favorites.exists()
+
+
+def condition_not_is_in_favorites(context):
+    return not context['resolved_object'].favorites.exists()
+
+
 link_document_list_favorites = Link(
     icon=icon_favorite_document_list, text=_('Favorites'),
     view='documents:document_favorite_list'
 )
 link_document_favorites_add = Link(
+    condition=condition_not_is_in_favorites,
     args='resolved_object.id', icon=icon_favorite_document_add,
     permissions=(permission_document_view,), text=_('Add to favorites'),
     view='documents:document_favorite_add'
 )
 link_document_favorites_remove = Link(
+    condition=condition_is_in_favorites,
     args='resolved_object.id', icon=icon_favorite_document_remove,
     permissions=(permission_document_view,), text=_('Remove from favorites'),
     view='documents:document_favorite_remove'
