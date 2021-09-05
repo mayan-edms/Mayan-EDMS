ee6857300afc3c498199cce08111f02f3eba3cc9
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-09-05 04:06:56 -0400

    Remove unused imports
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/redactions/tests/literals.py
+++ mayan/apps/redactions/tests/literals.py
@@ -1,6 +1,2 @@
-import os
-
-from django.conf import settings
-
 TEST_REDACTION_ARGUMENT = "{'left': 10, 'top': 10, 'right': 10, 'bottom': 10}"
 TEST_REDACTION_ARGUMENT_EDITED = "{'left': 20, 'top': 20, 'right': 20, 'bottom': 20}"
