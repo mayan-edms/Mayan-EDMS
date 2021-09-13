1071a3a9f3244fcc03a767656ce5a65e3b197171
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-09-13 06:00:14 -0400

    Add `GenericDocumentAPIViewTestCase`
    This is the base API test case with the document test mixin.
    
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/documents/tests/base.py
+++ mayan/apps/documents/tests/base.py
@@ -1,26 +1,35 @@
+from mayan.apps.rest_api.tests.base import BaseAPITestCase
 from mayan.apps.testing.tests.base import (
     BaseTestCase, BaseTransactionTestCase, GenericViewTestCase,
     GenericTransactionViewTestCase
 )
 
+
 from .mixins.document_mixins import DocumentTestMixin
 
 
 class GenericDocumentTestCase(DocumentTestMixin, BaseTestCase):
-    """Base test case when testing models or classes."""
+    """Base test case when testing document models or classes."""
+
+
+class GenericDocumentAPIViewTestCase(DocumentTestMixin, BaseAPITestCase):
+    """Base test case when testing document API views."""
+
+
+class GenericDocumentViewTestCase(DocumentTestMixin, GenericViewTestCase):
+    """Base test case when testing document views."""
 
 
 class GenericTransactionDocumentTestCase(
     DocumentTestMixin, BaseTransactionTestCase
 ):
-    """Base test case when testing models or classes with transactions."""
-
-
-class GenericDocumentViewTestCase(DocumentTestMixin, GenericViewTestCase):
-    """Base test case when testing views."""
-
+    """
+    Base test case when testing document models or classes with transactions.
+    """
 
 class GenericTransactionDocumentViewTestCase(
     DocumentTestMixin, GenericTransactionViewTestCase
 ):
-    """Base test case when testing views with transactions."""
+    """Base test case when testing document views with transactions."""
+
+
