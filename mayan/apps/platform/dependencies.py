a5c259a4b0fc39d30eeff86dd4d932b7a7977f2a
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-09-25 02:47:45 -0400

    Add Sentry.io service client backend
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/platform/dependencies.py
+++ mayan/apps/platform/dependencies.py
@@ -7,5 +7,8 @@ PythonDependency(
     module=__name__, name='gunicorn', version_string='==20.1.0'
 )
 PythonDependency(
+    module=__name__, name='sentry-sdk', version_string='==1.4.1'
+)
+PythonDependency(
     module=__name__, name='whitenoise', version_string='==5.3.0'
 )
