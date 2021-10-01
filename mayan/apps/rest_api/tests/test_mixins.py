fe638272f91fba0e17889feebbd2de01f2c8b72e
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-10-01 04:25:01 -0400

    Improve 404 API error management
    Make sure a 404 error on a external object referenced by
    a child queryset, while using the browseable API, succeeds
    in allowing serializer instantiation to allow error display.
    
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/rest_api/tests/test_mixins.py
+++ mayan/apps/rest_api/tests/test_mixins.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers, status
 
 from ..api_view_mixins import ExternalObjectAPIViewMixin
-from ..generics import RetrieveAPIView
+from ..generics import ListCreateAPIView, RetrieveAPIView
 
 from .base import BaseAPITestCase
 from .mixins import APIUserTestCaseMixin, TestAPIViewTestCaseMixin
@@ -55,4 +55,37 @@ class ExternalObjectAPIViewMixinTestCase(
     def test_mixin_using_token_authentication_no_permission(self):
         response = self._request_test_api_view()
 
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class ChildExternalObjectAPIViewMixinTestCase(
+    APIUserTestCaseMixin, TestAPIViewTestCaseMixin, BaseAPITestCase
+):
+    auto_add_test_view = True
+    auto_create_test_object = True
+
+    test_view_url = r'^test-view-url/(?P<test_object_id>\d+)/$'
+
+    def _test_view_factory(self, test_object=None):
+        TestModel = self.TestModel
+
+        class TestModelSerializer(serializers.Serializer):
+            """Empty serializer."""
+
+        class TestView(ExternalObjectAPIViewMixin, ListCreateAPIView):
+            external_object_queryset = TestModel.objects.all()
+            external_object_pk_url_kwarg = 'test_object_id'
+            serializer_class = TestModelSerializer
+
+        return TestView.as_view()
+
+    def _request_test_api_view(self):
+        return self.get(
+            viewname='rest_api:{}'.format(self.test_view_name), kwargs={
+                'test_object_id': self.test_object.pk + 1,
+            }, query={'format': 'api'}
+        )
+
+    def test_mixin_with_non_existent_object_and_browseable_view(self):
+        response = self._request_test_api_view()
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
