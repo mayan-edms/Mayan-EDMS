92389c7a68d6b4a21d5769a25760ac112b6319f7
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-10-07 02:45:45 -0400

    Add `databases` app to `INSTALLED_APPS`
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/settings/base.py
+++ mayan/settings/base.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = (
     'mayan.apps.common',
     'mayan.apps.converter',
     'mayan.apps.dashboards',
+    'mayan.apps.databases',
     'mayan.apps.dependencies',
     'mayan.apps.django_gpg',
     'mayan.apps.dynamic_search',
