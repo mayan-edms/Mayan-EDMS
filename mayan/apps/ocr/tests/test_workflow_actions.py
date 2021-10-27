570f9e809c89385c0f73a5f5d134cee4940ead05
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-10-27 06:28:57 -0400

    Fix OCR test
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/ocr/tests/test_workflow_actions.py
+++ mayan/apps/ocr/tests/test_workflow_actions.py
@@ -130,7 +130,7 @@ class UpdateDocumentPageOCRActionViewTestCase(
         self._create_test_workflow_template()
         self._create_test_workflow_template_state()
 
-    def test_update_document_version_page_ocr_workflow_state_action_create_post_view_no_permission(self):
+    def test_update_document_version_page_ocr_workflow_state_action_create_get_view_no_permission(self):
         action_count = self.test_workflow_template_state.actions.count()
 
         response = self._request_test_workflow_template_state_action_create_get_view(
@@ -143,7 +143,7 @@ class UpdateDocumentPageOCRActionViewTestCase(
             self.test_workflow_template_state.actions.count(), action_count
         )
 
-    def test_update_document_version_page_ocr_workflow_state_action_create_post_view_with_access(self):
+    def test_update_document_version_page_ocr_workflow_state_action_create_get_view_with_access(self):
         self.grant_access(
             obj=self.test_workflow_template,
             permission=permission_workflow_template_edit
@@ -153,9 +153,9 @@ class UpdateDocumentPageOCRActionViewTestCase(
         response = self._request_test_workflow_template_state_action_create_get_view(
             class_path='mayan.apps.ocr.workflow_actions.UpdateDocumentPageOCRAction',
         )
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
 
         self.test_workflow_template_state.refresh_from_db()
         self.assertEqual(
-            self.test_workflow_template_state.actions.count(), action_count + 1
+            self.test_workflow_template_state.actions.count(), action_count
         )
