111a46bd7f4a325f7cc1fcd8f5435ef25f97c135
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-10-27 04:45:52 -0400

    Add OCR update workflow action view tests
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/ocr/tests/test_workflow_actions.py
+++ mayan/apps/ocr/tests/test_workflow_actions.py
@@ -1,8 +1,13 @@
 import json
 
 from mayan.apps.document_states.literals import WORKFLOW_ACTION_ON_ENTRY
+from mayan.apps.document_states.permissions import permission_workflow_template_edit
 from mayan.apps.document_states.tests.mixins.workflow_template_mixins import WorkflowTemplateTestMixin
+from mayan.apps.document_states.tests.mixins.workflow_template_state_mixins import (
+    WorkflowTemplateStateActionViewTestMixin
+)
 from mayan.apps.documents.tests.base import GenericDocumentViewTestCase
+from mayan.apps.testing.tests.base import GenericViewTestCase
 
 from ..models import DocumentVersionPageOCRContent
 from ..workflow_actions import UpdateDocumentPageOCRAction
@@ -113,4 +118,44 @@ class UpdateDocumentPageOCRActionTestCase(
 
         self.assertEqual(
             ''.join(self.test_document.ocr_content()), self.test_document.label
+        )
+
+
+class UpdateDocumentPageOCRActionViewTestCase(
+    WorkflowTemplateTestMixin, WorkflowTemplateStateActionViewTestMixin,
+    GenericViewTestCase
+):
+    def setUp(self):
+        super().setUp()
+        self._create_test_workflow_template()
+        self._create_test_workflow_template_state()
+
+    def test_update_document_version_page_ocr_workflow_state_action_create_post_view_no_permission(self):
+        action_count = self.test_workflow_template_state.actions.count()
+
+        response = self._request_test_workflow_template_state_action_create_get_view(
+            class_path='mayan.apps.ocr.workflow_actions.UpdateDocumentPageOCRAction',
+        )
+        self.assertEqual(response.status_code, 404)
+
+        self.test_workflow_template_state.refresh_from_db()
+        self.assertEqual(
+            self.test_workflow_template_state.actions.count(), action_count
+        )
+
+    def test_update_document_version_page_ocr_workflow_state_action_create_post_view_with_access(self):
+        self.grant_access(
+            obj=self.test_workflow_template,
+            permission=permission_workflow_template_edit
+        )
+        action_count = self.test_workflow_template_state.actions.count()
+
+        response = self._request_test_workflow_template_state_action_create_get_view(
+            class_path='mayan.apps.ocr.workflow_actions.UpdateDocumentPageOCRAction',
+        )
+        self.assertEqual(response.status_code, 302)
+
+        self.test_workflow_template_state.refresh_from_db()
+        self.assertEqual(
+            self.test_workflow_template_state.actions.count(), action_count + 1
         )
