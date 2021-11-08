dc2a8cbfa2d438efc379e2258cbe4f8bc07f986c
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-11-08 03:22:58 -0400

    Bump version to 4.0.20
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/__init__.py
+++ mayan/__init__.py
@@ -1,7 +1,7 @@
 __title__ = 'Mayan EDMS'
-__version__ = '4.0.19'
-__build__ = 0x040019
-__build_string__ = 'v4.0.19_Wed Oct 27 05:33:28 2021 -0400'
+__version__ = '4.0.20'
+__build__ = 0x040020
+__build_string__ = 'v4.0.19-11-gcc3260c18c_Mon Nov 8 03:22:15 2021 -0400'
 __django_version__ = '2.2'
 __author__ = 'Roberto Rosario'
 __author_email__ = 'roberto.rosario@mayan-edms.com'
