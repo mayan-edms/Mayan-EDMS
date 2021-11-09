bae35fffff68bf4d57bf881f110d53feacfeb072
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-11-09 05:08:54 -0400

    Fix authentication view tests
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/authentication/urls.py
+++ mayan/apps/authentication/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path
 
 from .views.authentication_views import (
     MayanLoginView, MayanLogoutView, MayanPasswordChangeDoneView,
@@ -32,8 +33,8 @@ urlpatterns_password = [
         name='password_reset_complete_view',
         view=MayanPasswordResetCompleteView.as_view()
     ),
-    url(
-        regex=r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    path(
+        'password/reset/confirm/<uidb64>/<token>/',
         name='password_reset_confirm_view',
         view=MayanPasswordResetConfirmView.as_view()
     ),
