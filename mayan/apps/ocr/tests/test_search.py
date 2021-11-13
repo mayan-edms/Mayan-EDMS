64b82337795342040c6a7fa5151ef9f9d198b24f
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-11-13 06:45:23 -0400

    Update OCR search tests
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/ocr/tests/test_search.py
+++ mayan/apps/ocr/tests/test_search.py
@@ -1,24 +1,170 @@
 from mayan.apps.documents.tests.base import GenericDocumentViewTestCase
-from mayan.apps.documents.tests.mixins.document_mixins import DocumentSearchTestMixin
-from mayan.apps.documents.permissions import permission_document_view
+from mayan.apps.documents.permissions import (
+    permission_document_version_view, permission_document_view
+)
+from mayan.apps.documents.search import (
+    document_version_page_search, document_search, document_version_search
+)
+from mayan.apps.dynamic_search.tests.mixins import SearchTestMixin
 
 from .mixins import DocumentVersionOCRTestMixin
 
 
-class DocumentVersionOCRSearchTestCase(
-    DocumentSearchTestMixin, DocumentVersionOCRTestMixin,
-    GenericDocumentViewTestCase
+class DocumentOCRSearchTestCase(
+    DocumentVersionOCRTestMixin, SearchTestMixin, GenericDocumentViewTestCase
 ):
-    def test_document_search_with_access(self):
-        self._create_test_document_version_ocr_content()
+    _test_search_index_object_name = 'test_document'
+    _test_search_model = document_search
+    auto_create_test_document_version_ocr_content = True
 
-        self.grant_access(
-            obj=self.test_document, permission=permission_document_view
-        )
-
-        queryset = self._perform_document_search(
+    def _do_test_search(self):
+        return self.do_test_search(
             query={
                 'versions__version_pages__ocr_content__content': self.test_document_version_page.ocr_content.content
             }
         )
+
+    def test_document_search_no_permission(self):
+        self._clear_events()
+
+        queryset = self._do_test_search()
+        self.assertTrue(self.test_document not in queryset)
+
+        events = self._get_test_events()
+        self.assertEqual(events.count(), 0)
+
+    def test_document_search_with_access(self):
+        self.grant_access(
+            obj=self.test_document, permission=permission_document_view
+        )
+
+        self._clear_events()
+
+        queryset = self._do_test_search()
         self.assertTrue(self.test_document in queryset)
+
+        events = self._get_test_events()
+        self.assertEqual(events.count(), 0)
+
+    def test_trashed_document_search_with_access(self):
+        self.grant_access(
+            obj=self.test_document, permission=permission_document_view
+        )
+
+        self.test_document.delete()
+
+        self._clear_events()
+
+        queryset = self._do_test_search()
+        self.assertTrue(self.test_document not in queryset)
+
+        events = self._get_test_events()
+        self.assertEqual(events.count(), 0)
+
+
+class DocumentVersionOCRSearchTestCase(
+    DocumentVersionOCRTestMixin, SearchTestMixin, GenericDocumentViewTestCase
+):
+    _test_search_index_object_name = 'test_document_version'
+    _test_search_model = document_version_search
+    auto_create_test_document_version_ocr_content = True
+
+    def _do_test_search(self):
+        return self.do_test_search(
+            query={
+                'version_pages__ocr_content__content': self.test_document_version_page.ocr_content.content
+            }
+        )
+
+    def test_document_version_search_no_permission(self):
+        self._clear_events()
+
+        queryset = self._do_test_search()
+        self.assertTrue(self.test_document_version not in queryset)
+
+        events = self._get_test_events()
+        self.assertEqual(events.count(), 0)
+
+    def test_document_version_search_with_access(self):
+        self.grant_access(
+            obj=self.test_document,
+            permission=permission_document_version_view
+        )
+
+        self._clear_events()
+
+        queryset = self._do_test_search()
+        self.assertTrue(self.test_document_version in queryset)
+
+        events = self._get_test_events()
+        self.assertEqual(events.count(), 0)
+
+    def test_trashed_document_version_search_with_access(self):
+        self.grant_access(
+            obj=self.test_document,
+            permission=permission_document_version_view
+        )
+
+        self.test_document.delete()
+
+        self._clear_events()
+
+        queryset = self._do_test_search()
+        self.assertTrue(self.test_document_version not in queryset)
+
+        events = self._get_test_events()
+        self.assertEqual(events.count(), 0)
+
+
+class DocumentVersionPageOCRSearchTestCase(
+    DocumentVersionOCRTestMixin, SearchTestMixin, GenericDocumentViewTestCase
+):
+    _test_search_index_object_name = 'test_document_version_page'
+    _test_search_model = document_version_page_search
+    auto_create_test_document_version_ocr_content = True
+
+    def _do_test_search(self):
+        return self.do_test_search(
+            query={
+                'ocr_content__content': self.test_document_version_page.ocr_content.content
+            }
+        )
+
+    def test_document_version_search_no_permission(self):
+        self._clear_events()
+
+        queryset = self._do_test_search()
+        self.assertTrue(self.test_document_version_page not in queryset)
+
+        events = self._get_test_events()
+        self.assertEqual(events.count(), 0)
+
+    def test_document_version_search_with_access(self):
+        self.grant_access(
+            obj=self.test_document,
+            permission=permission_document_version_view
+        )
+
+        self._clear_events()
+
+        queryset = self._do_test_search()
+        self.assertTrue(self.test_document_version_page in queryset)
+
+        events = self._get_test_events()
+        self.assertEqual(events.count(), 0)
+
+    def test_trashed_document_version_search_with_access(self):
+        self.grant_access(
+            obj=self.test_document,
+            permission=permission_document_version_view
+        )
+
+        self.test_document.delete()
+
+        self._clear_events()
+
+        queryset = self._do_test_search()
+        self.assertTrue(self.test_document_version_page not in queryset)
+
+        events = self._get_test_events()
+        self.assertEqual(events.count(), 0)
