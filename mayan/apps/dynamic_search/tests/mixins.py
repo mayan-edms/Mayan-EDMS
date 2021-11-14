1b7b1cafe257926a06d0609a91c8767704e67a98
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-11-14 07:29:59 -0400

    Unify the search test mixins
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/dynamic_search/tests/mixins.py
+++ mayan/apps/dynamic_search/tests/mixins.py
@@ -2,8 +2,28 @@ from mayan.apps.documents.search import document_search
 
 from ..classes import SearchBackend
 
+from .mocks import TestSearchBackend
 
-class SearchAPIViewTestMixin:
+
+class SearchTestMixin:
+    _test_search_index_object_name = None
+    _test_search_model = None
+
+    def _deindex_instance(self, instance):
+        self.search_backend.deindex_instance(instance=instance)
+
+    def _index_instance(self, instance):
+        self.search_backend.index_instance(instance=instance)
+
+    def setUp(self):
+        super().setUp()
+        # Monkeypatch the search class so that the test behavior is only
+        # enabled when called from a search test.
+        TestSearchBackend._test_view = self
+        self.search_backend = SearchBackend.get_instance()
+
+
+class SearchAPIViewTestMixin(SearchTestMixin):
     def _request_search_view(self):
         query = {'q': self.test_document.label}
         return self.get(
@@ -22,49 +42,12 @@ class SearchAPIViewTestMixin:
         )
 
 
-class SearchTestMixin:
-    _test_search_index_object_name = None
-    _test_search_model = None
-
-    def _deindex_instance(self, instance):
-        self.search_backend.deindex_instance(instance=instance)
-
-    def _index_instance(self, instance):
-        self.search_backend.index_instance(instance=instance)
-
-    def do_test_search(self, terms=None, query=None):
-        if self._test_search_index_object_name:
-            self.search_backend.index_instance(
-                instance=getattr(self, self._test_search_index_object_name)
-            )
-
-        query = query or {'q': terms}
-
-        return self.search_backend.search(
-            search_model=self._test_search_model, query=query,
-            user=self._test_case_user
-        )
-
-    def setUp(self):
-        super().setUp()
-
-        self.search_backend = SearchBackend.get_instance(
-            extra_kwargs={'_search_test': True}
-        )
-
-    def tearDown(self):
-        if hasattr(self.search_backend, '_cleanup'):
-            self.search_backend._cleanup()
-
-        super().tearDown()
-
-
 class SearchToolsViewTestMixin:
     def _request_search_backend_reindex_view(self):
         return self.post(viewname='search:search_backend_reindex')
 
 
-class SearchViewTestMixin:
+class SearchViewTestMixin(SearchTestMixin):
     def _request_search_results_view(self, data, kwargs=None, query=None):
         return self.get(
             viewname='search:results', kwargs=kwargs, data=data, query=query
