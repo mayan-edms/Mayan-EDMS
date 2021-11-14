ae30fc85fface9638a213b0a6ee25abb69c80d88
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-11-14 07:28:30 -0400

    Use `TemporaryDirectory` for test search backend
    Do automatic clean up of the temporary index directory.
    
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/dynamic_search/tests/mocks.py
+++ mayan/apps/dynamic_search/tests/mocks.py
@@ -1,6 +1,6 @@
 from django.utils.module_loading import import_string
 
-from mayan.apps.storage.utils import fs_cleanup, mkdtemp
+from mayan.apps.storage.utils import TemporaryDirectory
 
 from ..backends.whoosh import WhooshSearchBackend
 from ..literals import DEFAULT_SEARCH_BACKEND
@@ -10,27 +10,49 @@ default_search_backend = import_string(dotted_path=DEFAULT_SEARCH_BACKEND)
 
 
 class TestSearchBackend(default_search_backend):
-    def __init__(self, *args, **kwargs):
-        self._search_test = kwargs.pop('_search_test', False)
+    """
+    Test search backend dynamically generated from the default search
+    backend. If the default backend is the Whoosh backend, a temporary
+    folder will be created to store the search index. The temporary directory
+    is automatically deleted by Python when all references are removed.
 
+    This test search backend will only perform instance indexing and
+    deindexing when called from a search test. This allows using the backend
+    for search tests but avoids the indexing penalty for non search tests.
+
+    It will also automatically insert the test object into the index
+    before performing a call to the `search` method.
+    """
+    _test_view = None
+
+    def __init__(self, *args, **kwargs):
         kwargs = setting_backend_arguments.value.copy()
 
-        if self._search_test:
+        if self._test_view:
             if issubclass(default_search_backend, WhooshSearchBackend):
-                self._test_index_path = mkdtemp()
-                kwargs['index_path'] = self._test_index_path
+                self._temporary_directory = TemporaryDirectory()
+                kwargs['index_path'] = self._temporary_directory.name
 
         super().__init__(*args, **kwargs)
 
-    def _cleanup(self):
-        if self._search_test:
-            if issubclass(default_search_backend, WhooshSearchBackend):
-                fs_cleanup(filename=self._test_index_path)
-
     def deindex_instance(self, instance):
-        if self._search_test:
+        if self._test_view:
             super().deindex_instance(instance=instance)
 
     def index_instance(self, instance):
-        if self._search_test:
+        if self._test_view:
             super().index_instance(instance=instance)
+
+    def search(self, *args, **kwargs):
+        if self._test_view:
+            # if running as part of a search test. Index the test object
+            # before executing the search method.
+            if self._test_view._test_search_index_object_name:
+                self.index_instance(
+                    instance=getattr(
+                        self._test_view,
+                        self._test_view._test_search_index_object_name
+                    )
+                )
+
+        return super().search(*args, **kwargs)
