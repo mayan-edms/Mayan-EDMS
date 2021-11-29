aad1b64d03ab37a6d749bb83dde2aeccf76fd5ca
Roberto Rosario <roberto.rosario@mayan-edms.com>
2021-11-29 08:00:31 -0400

    Consolidate test model creation
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/common/tests/test_classes.py
+++ mayan/apps/common/tests/test_classes.py
@@ -1,11 +1,28 @@
-from django.db import models
+from django.db import models, transaction
 
-from mayan.apps.testing.tests.base import BaseTestCase
+from mayan.apps.testing.tests.base import BaseTestCase, BaseTransactionTestCase
 
 from ..classes import QuerysetParametersSerializer
 
 
 class QuerysetParametersSerializerTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.TestModelParent = self._create_test_model(
+            model_name='TestModelParent'
+        )
+        self.TestModelChild = self._create_test_model(
+            fields={
+                'parent': models.ForeignKey(
+                    on_delete=models.CASCADE, related_name='children',
+                    to='TestModelParent'
+                )
+            }, model_name='TestModelChild'
+        )
+
+        self._test_object_parent = self.TestModelParent.objects.create()
+        self.TestModelChild.objects.create(parent_id=self._test_object_parent.pk)
+
     def _assertQuerysetEqual(self):
         rebuilt_items = list(map(repr, self.queryset_rebuilt))
 
@@ -14,12 +31,10 @@ class QuerysetParametersSerializerTestCase(BaseTestCase):
         )
 
     def test_without_kwargs(self):
-        self._create_test_object()
-
-        self.queryset_original = self.TestModel.objects.all()
+        self.queryset_original = self.TestModelParent.objects.all()
 
         decomposed_queryset = QuerysetParametersSerializer.decompose(
-            _model=self.TestModel, _method_name='all'
+            _model=self.TestModelParent, _method_name='all'
         )
 
         self.queryset_rebuilt = QuerysetParametersSerializer.rebuild(
@@ -29,25 +44,11 @@ class QuerysetParametersSerializerTestCase(BaseTestCase):
         self._assertQuerysetEqual()
 
     def test_foreign_key_model(self):
-        self.TestModelParent = self._create_test_model(
-            model_name='TestModelParent'
-        )
-        self.TestModelChild = self._create_test_model(
-            fields={
-                'parent': models.ForeignKey(
-                    on_delete=models.CASCADE, related_name='children',
-                    to='TestModelParent',
-                )
-            }, model_name='TestModelChild'
-        )
-
-        parent = self.TestModelParent.objects.create()
-        self.TestModelChild.objects.create(parent=parent)
-
         self.queryset_original = self.TestModelChild.objects.all()
 
         decomposed_queryset = QuerysetParametersSerializer.decompose(
-            _model=self.TestModelChild, _method_name='filter', parent=parent
+            _model=self.TestModelChild, _method_name='filter',
+            parent=self._test_object_parent
         )
 
         self.queryset_rebuilt = QuerysetParametersSerializer.rebuild(
@@ -57,26 +58,11 @@ class QuerysetParametersSerializerTestCase(BaseTestCase):
         self._assertQuerysetEqual()
 
     def test_foreign_key_model_id_query(self):
-        self.TestModelParent = self._create_test_model(
-            model_name='TestModelParent'
-        )
-        self.TestModelChild = self._create_test_model(
-            fields={
-                'parent': models.ForeignKey(
-                    on_delete=models.CASCADE, related_name='children',
-                    to='TestModelParent',
-                )
-            }, model_name='TestModelChild'
-        )
-
-        parent = self.TestModelParent.objects.create()
-        self.TestModelChild.objects.create(parent_id=parent.pk)
-
         self.queryset_original = self.TestModelChild.objects.all()
 
         decomposed_queryset = QuerysetParametersSerializer.decompose(
             _model=self.TestModelChild, _method_name='filter',
-            parent_id=parent.pk
+            parent_id=self._test_object_parent.pk
         )
 
         self.queryset_rebuilt = QuerysetParametersSerializer.rebuild(
