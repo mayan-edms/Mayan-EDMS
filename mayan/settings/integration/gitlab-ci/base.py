ea433ad8b0fa485e652db03508c257d3ca3dac13
Roberto Rosario <roberto.rosario@mayan-edms.com>
2022-02-08 05:54:19 -0400

    Add `TESTING` flag to the GitLab settings
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/settings/integration/gitlab-ci/base.py
+++ mayan/settings/integration/gitlab-ci/base.py
@@ -3,3 +3,5 @@ from ..base import *  # NOQA
 SIGNATURES_GPG_PATH = '/usr/bin/gpg1'
 
 TESTS_SELENIUM_SKIP = True
+
+TESTING = True  # Silence the error logger for non critical HTTP404 and PermissionDenied
