820edfc4824155399cd92f7ad490776f1c5905bd
Roberto Rosario <roberto.rosario@mayan-edms.com>
2022-06-05 16:06:06 -0400

    Add packaging library to the build environment
    Signed-off-by: Roberto Rosario <roberto.rosario@mayan-edms.com>

--- mayan/apps/dependencies/dependencies.py
+++ mayan/apps/dependencies/dependencies.py
@@ -1,8 +1,11 @@
+from mayan.apps.dependencies.environments import environment_build
+
 from .classes import PythonDependency
 
 PythonDependency(
     module=__name__, name='node-semver', version_string='==0.8.1'
 )
 PythonDependency(
-    module=__name__, name='packaging', version_string='==21.3'
+    environment=environment_build, module=__name__, name='packaging',
+    version_string='==21.3'
 )
